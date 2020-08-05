luxumbra/TheGame

get3BoxProfile handler

packages/backend/src/handlers/actions/routes.ts
@@ -2,9 +2,12 @@ import express from 'express';
 
 import { asyncHandlerWrapper } from '../../lib/apiHelpers';
 
+import getBoxProfile from './getBoxProfile/handler';
 import updateBoxProfileHandler from './updateBoxProfile/handler';
 
 const router = express.Router();
+
+router.post('/getBoxProfile', asyncHandlerWrapper(getBoxProfile));
 
 router.post('/updateBoxProfile', asyncHandlerWrapper(updateBoxProfileHandler));
 

appcalipse/meetwith

packages/backend/src/handlers/actions/routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import { asyncHandlerWrapper } from '../../lib/apiHelpers';
+
+import getBoxProfile from './getBoxProfile/handler';
 import { updateBoxProfileHandler } from './updateBoxProfile/handler';
 
 export const actionRoutes = express.Router();
 
-actionRoutes.post(
-  '/updateBoxProfile',
-  asyncHandlerWrapper(updateBoxProfileHandler),
-);
+actionRoutes.post('/getBoxProfile', asyncHandlerWrapper(getBoxProfile));
+
+actionRoutes.post('/updateBoxProfile', asyncHandlerWrapper(updateBoxProfileHandler));
+