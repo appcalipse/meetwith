7fbad31dd449b623fa07d3ed4817e7d8a4b54c87
Ramon Canales <emaildoramon@gmail.com>
2021-12-01 12:26:31 +0000

    favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,7 +31,15 @@ export default function MyApp({Component, pageProps}: AppProps) {
           <CookiesProvider>
       <AccountProvider>
         <Head>
+          <title>Meet with wallet</title>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f35826" />
+          <meta name="msapplication-TileColor" content="#1a202c" />
+          <meta name="theme-color" content="#f35826"></meta>
         </Head>
         {loading ? (
           <div>Loading...</div>
