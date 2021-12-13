3aec216375ea620090c4c82484f51eeecfcd5120
Ramon Canales <emaildoramon@gmail.com>
2021-12-13 14:57:33 +0000

    almost there

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
-const apiUrl = process.env.NEXT_PUBLIC_ENV !== 'production' ? (process.env.NEXT_PUBLIC_ENV === 'development' ? 'http://localhost:3000/api' : 'https://meet-with-wallet-git-develop-appcalipse.vercel.app/') : 'https://meetwithwallet.xyz/api';
+const apiUrl = process.env.NEXT_PUBLIC_ENV !== 'production' ? (process.env.NEXT_PUBLIC_ENV !== 'development' ? 'http://localhost:3000/api' : 'https://meet-with-wallet-git-develop-appcalipse.vercel.app/') : 'https://meetwithwallet.xyz/api';
 const isProduction = process.env.NEXT_PUBLIC_ENV === 'production';
 const DEFAULT_MESSAGE = `Welcome to Meet with Wallet! Please sign this message to make your experience safe.`
 
 console.log(process.env.NEXT_PUBLIC_ENV) 
-export { apiUrl, isProduction, DEFAULT_MESSAGE }
+export { apiUrl, isProduction, DEFAULT_MESSAGE }]
