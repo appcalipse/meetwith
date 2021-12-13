316a87c51e537349b112f9f75bde152f76c062ed
Ramon Canales <emaildoramon@gmail.com>
2021-12-13 15:05:36 +0000

    kill me now again

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
-const apiUrl = process.env.NEXT_PUBLIC_ENV !== 'production' ? (process.env.NEXT_PUBLIC_ENV !== 'development' ? 'http://localhost:3000/api' : 'https://meet-with-wallet-git-develop-appcalipse.vercel.app/') : 'https://meetwithwallet.xyz/api';
+const apiUrl = process.env.NEXT_PUBLIC_ENV !== 'production' ? (process.env.NEXT_PUBLIC_ENV !== 'development' ? 'http://localhost:3000/api' : 'https://meet-with-wallet-git-develop-appcalipse.vercel.app/api') : 'https://meetwithwallet.xyz/api';
 const isProduction = process.env.NEXT_PUBLIC_ENV === 'production';
 const DEFAULT_MESSAGE = `Welcome to Meet with Wallet! Please sign this message to make your experience safe.`
 
