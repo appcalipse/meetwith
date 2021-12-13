6f16dbdd18e7d6f26de24b4b6e32ec17789c439d
Ramon Canales <emaildoramon@gmail.com>
2021-12-13 14:32:19 +0000

    console logging

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,4 +2,5 @@ const apiUrl = process.env.NODE_ENV !== 'production' ? (process.env.NODE_ENV ===
 const isProduction = process.env.NODE_ENV === 'production';
 const DEFAULT_MESSAGE = `Welcome to Meet with Wallet! Please sign this message to make your experience safe.`
 
+console.log(process.env.NODE_ENV)) 
 export { apiUrl, isProduction, DEFAULT_MESSAGE }
