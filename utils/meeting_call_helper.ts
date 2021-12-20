2e639ec87b1f44e96fd633823a765638e25b1c5b
Ramon Canales <emaildoramon@gmail.com>
2021-12-20 13:22:40 +0000

    changing meeting platform to huddle (basic approach)

--- utils/meeting_call_helper.ts
+++ utils/meeting_call_helper.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const generateMeetingUrl = (): string => {
-    return `https://meet.jit.si/${uuidv4()}`;
+    return `https://app.huddle01.com/room?roomId=${uuidv4()}`;
 }
 
 export { generateMeetingUrl }
