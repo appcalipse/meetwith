a0a11660aad0a0d5ec113a59183286257ca9a310
Ramon Canales <emaildoramon@gmail.com>
2021-12-22 17:37:05 +0000

    go biuld

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import React from 'react'
 import Hero from '../components/landing/Hero'
 import Features from '../components/landing/Features'
-import Subscribe from '../components/landing/Subscribe'
 import Pricing from '../components/landing/Pricing'
 import FAQ from '../components/landing/FAQ'
 
