bc64ac40977ad66eb341af7fa22b1c9b16e24687
Eric Pereira <git@ericpereira.dev>
2022-01-07 11:31:12 +0000

    Lint and ts fix

---
+++ src/utils/storage.ts
@@ -0,0 +1,22 @@
+import { Account } from '../types/Account'
+
+const SIGNATURE_KEY = 'current_user_sig'
+const ACCOUNT = 'current_account'
+
+const saveSignature = (account_address: string, signature: string) => {
+  window.localStorage.setItem(`${SIGNATURE_KEY}:${account_address}`, signature)
+}
+
+const getSignature = (account_address: string): string | null => {
+  return window.localStorage.getItem(`${SIGNATURE_KEY}:${account_address}`)
+}
+
+const storeCurrentAccount = (account: Account) => {
+  window.localStorage.setItem(ACCOUNT, account.address)
+}
+
+const getCurrentAccount = (): string => {
+  return window.localStorage.getItem(ACCOUNT) as string
+}
+
+export { saveSignature, getSignature, storeCurrentAccount, getCurrentAccount }
