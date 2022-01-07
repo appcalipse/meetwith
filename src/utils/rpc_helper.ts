969e3b4cdffaae4f1d9763f67c1e012dd6ce7b87
Ramon Canales <emaildoramon@gmail.com>
2022-01-07 11:43:52 +0000

    linting

--- src/utils/rpc_helper.ts
+++ src/utils/rpc_helper.ts
@@ -1,46 +1,51 @@
-import { ethers } from "ethers";
-import { JsonRpcProvider } from "@ethersproject/providers";
+import { ethers } from 'ethers'
+import { JsonRpcProvider } from '@ethersproject/providers'
 
 interface AccountExtraProps {
-    name: string,
-    avatar?: string,
-} 
-
-export const resolveExtraInfo = async (address: string): Promise<AccountExtraProps | undefined> => {
-    return await resolveENS(address)
+  name: string
+  avatar?: string
 }
 
-const resolveENS = async (address: string): Promise<AccountExtraProps | undefined> => {
+export const resolveExtraInfo = async (
+  address: string
+): Promise<AccountExtraProps | undefined> => {
+  return await resolveENS(address)
+}
 
-    let provider: JsonRpcProvider
-    
-    if(window.ethereum && window.ethereum.chainId === "0x1") {
-        provider = new ethers.providers.Web3Provider(window.ethereum);
-    } else {
-        provider = new ethers.providers.InfuraProvider("homestead", process.env.NEXT_PUBLIC_INFURA_RPC_PROJECT_ID);
-    }
+const resolveENS = async (
+  address: string
+): Promise<AccountExtraProps | undefined> => {
+  let provider: JsonRpcProvider
 
-    const name = await provider.lookupAddress(address)
+  if (window.ethereum && window.ethereum.chainId === '0x1') {
+    provider = new ethers.providers.Web3Provider(window.ethereum)
+  } else {
+    provider = new ethers.providers.InfuraProvider(
+      'homestead',
+      process.env.NEXT_PUBLIC_INFURA_RPC_PROJECT_ID
+    )
+  }
 
-    if(!name) {
-        return undefined
-    }
+  const name = await provider.lookupAddress(address)
 
-    const resolver = await provider.getResolver(name);
+  if (!name) {
+    return undefined
+  }
 
-    const validatedAddress = await resolver?.getAddress()
+  const resolver = await provider.getResolver(name)
 
-    // Check to be sure the reverse record is correct.
-    if(address.toLowerCase() !== validatedAddress?.toLowerCase()) {
-        return undefined
-    }
+  const validatedAddress = await resolver?.getAddress()
 
-    const avatarInfo = await resolver?.getText("avatar")
-    const avatar = avatarInfo ? (await resolver?.getAvatar())?.url : undefined
+  // Check to be sure the reverse record is correct.
+  if (address.toLowerCase() !== validatedAddress?.toLowerCase()) {
+    return undefined
+  }
 
-    return {
-        name,
-        avatar
-    }
+  const avatarInfo = await resolver?.getText('avatar')
+  const avatar = avatarInfo ? (await resolver?.getAvatar())?.url : undefined
 
+  return {
+    name,
+    avatar,
+  }
 }
