8784f7dd91af6f7d8846da5f6ed81b9659d7220c
falleco <israel@zenklub.com>
2022-01-19 11:23:27 +0000

    chore: adding date and time picker

---
+++ src/components/input-time-picker/index.tsx
@@ -0,0 +1,69 @@
+import { Box, Button, HStack, Icon, Input, InputGroup, InputLeftElement, Link, Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverTrigger, Radio, RadioGroup, Select, useBoolean, useColorModeValue, VStack } from "@chakra-ui/react"
+import React from "react"
+import { FaClock } from "react-icons/fa"
+import dayjs from 'dayjs';
+
+export interface InputTimePickerProps {
+    onChange: (time: string) => void,
+    value: string,
+}
+
+const generateTimes = () => {
+    const start = dayjs().startOf('day');
+    const end = dayjs(start).add(1, 'day').startOf('day');
+    const interval = 15;
+
+    const response = [];
+    for(let cur = start; cur.isBefore(end); cur = dayjs(cur).add(interval, 'minutes')) {
+        response.push(cur.format('HH:mm'))
+    }
+
+    return response;
+}
+
+export const InputTimePicker: React.FC<InputTimePickerProps> = ({onChange, value}) => {
+    const [isEditing, setIsEditing] = useBoolean()
+    const iconColor = useColorModeValue('gray.500', 'gray.200')
+    const times = generateTimes();
+
+    return (
+        <InputGroup>
+        <InputLeftElement
+                        pointerEvents="none"
+                        children={
+                            <Icon
+                            fontSize="16"
+                            color={iconColor}
+                            _groupHover={{
+                                color: iconColor,
+                            }}
+                            as={FaClock}
+                            />
+                        }
+                        />
+        <Popover
+            isOpen={isEditing}
+            onOpen={setIsEditing.on}
+            onClose={setIsEditing.off}
+            closeOnBlur={false}
+            isLazy
+            lazyBehavior='keepMounted'
+            >
+            <HStack>
+                <PopoverTrigger>
+                    <Input id="time" sx={{paddingLeft: '36px'}} placeholder="Time" type="text" value={value} onChange={(ev) => { onChange(ev.target.value); setIsEditing.off();}} />
+                </PopoverTrigger>
+            </HStack>
+
+            <PopoverContent>
+                <PopoverArrow />
+                <PopoverBody>
+                    <VStack sx={{maxHeight: "300px", overflowY: "scroll"}}>
+                        {times.map((it, idx) => (<Link key={it} onClick={() => { onChange(it); setIsEditing.off();}}><Box>{it}</Box></Link>))}
+                    </VStack>
+                </PopoverBody>
+            </PopoverContent>
+            </Popover>
+        </InputGroup>
+    )
+}
