9aa524565ff94cd89321fed5cbbd72c6de5bb423
Ramon Canales <emaildoramon@gmail.com>
2022-01-23 20:29:16 +0000

    linting & removing unused code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -206,11 +206,9 @@ const MobileNav = () => {
   )
 }
 
-const MobileNavItem = ({ label, children, href }: NavItem) => {
-  const { isOpen, onToggle } = useDisclosure()
-
+const MobileNavItem = ({ label, href }: NavItem) => {
   return (
-    <Stack spacing={4} onClick={children && onToggle}>
+    <Stack spacing={4}>
       <Flex
         py={2}
         as={Link}
@@ -227,34 +225,7 @@ const MobileNavItem = ({ label, children, href }: NavItem) => {
         >
           {label}
         </Text>
-        {children && (
-          <Icon
-            as={ChevronDownIcon}
-            transition={'all .25s ease-in-out'}
-            transform={isOpen ? 'rotate(180deg)' : ''}
-            w={6}
-            h={6}
-          />
-        )}
       </Flex>
-
-      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-        <Stack
-          mt={2}
-          pl={4}
-          borderLeft={1}
-          borderStyle={'solid'}
-          borderColor={useColorModeValue('gray.200', 'gray.700')}
-          align={'start'}
-        >
-          {children &&
-            children.map(child => (
-              <NextLink key={child.label} href={child.href!} passHref>
-                <Link py={2}>{child.label}</Link>
-              </NextLink>
-            ))}
-        </Stack>
-      </Collapse>
     </Stack>
   )
 }
