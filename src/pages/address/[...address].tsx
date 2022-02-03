b2f0dbb7d1c8b03c5dbceaabf83e8555024f8579
yummyweb <antriksh.timepass@gmail.com>
2022-02-03 23:11:57 +0530

    fix: add navbar and footer to user calendar

--- src/pages/address/[...address].tsx
+++ src/pages/address/[...address].tsx
@@ -1,10 +1,18 @@
 import { NextPage } from 'next'
 import React from 'react'
 
+import Footer from '../../components/Footer'
+import { Navbar } from '../../components/Navbar'
 import PublicCalendar from '../../components/public-calendar'
 import { forceAuthenticationCheck } from '../../session/forceAuthenticationCheck'
 
-const Schedule: NextPage = () => <PublicCalendar />
+const Schedule: NextPage = () => (
+  <>
+    <Navbar />
+    <PublicCalendar />
+    <Footer />
+  </>
+)
 
 const EnhancedSchedule = forceAuthenticationCheck(Schedule)
 
