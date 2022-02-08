3f126676058127c8d233cc147ba55cec590f3264
Ramon Canales <emaildoramon@gmail.com>
2022-02-08 16:53:58 +0000

    adding endpoints for external account creation and verification

---
+++ src/pages/api/auth/signature/index.ts
@@ -0,0 +1,25 @@
+import { withSentry } from '@sentry/nextjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { DEFAULT_MESSAGE } from '../../../../utils/constants'
+import { getAccountFromDB } from '../../../../utils/database'
+
+const getDefaultSignature = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (req.method === 'GET') {
+    const { identifier } = req.query
+    try {
+      const account = await getAccountFromDB(identifier as string)
+      res
+        .status(200)
+        .json({ message: DEFAULT_MESSAGE(account.nonce), nonce: account.nonce })
+    } catch (e) {
+      const nonce = Number(Math.random().toString(8).substring(2, 10))
+      res.status(200).send({ message: DEFAULT_MESSAGE(nonce), nonce })
+    }
+  }
+}
+
+export default withSentry(getDefaultSignature)
