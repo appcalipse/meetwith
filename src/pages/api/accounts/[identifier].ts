7a229dcd6b30cb25361e016770857faa0ac20693
Ramon Canales <emaildoramon@gmail.com>
2022-02-22 08:33:03 +0000

    integration ready

--- src/pages/api/accounts/[identifier].ts
+++ src/pages/api/accounts/[identifier].ts
@@ -3,15 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import { withSessionRoute } from '../../../utils/auth/withSessionApiRoute'
 import { getAccountFromDB } from '../../../utils/database'
-import { sendEPNSNotification } from '../../../utils/epns_helper'
 
 const getAccount = async (req: NextApiRequest, res: NextApiResponse) => {
-  sendEPNSNotification(
-    ['0xe5b06bfd663C94005B8b159Cd320Fd7976549f9b'],
-    'title normal',
-    'message normal'
-  )
-
   if (req.method === 'GET') {
     const { identifier } = req.query
     try {
