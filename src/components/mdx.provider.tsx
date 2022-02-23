bda98b6d51744f174bdff97f8a39365f79603022
falleco <israel@zenklub.com>
2022-02-23 10:59:22 +0000

    adding terms and privacy policies mechanics

---
+++ src/components/mdx.provider.tsx
@@ -0,0 +1,28 @@
+import { Heading, ListItem, Text, UnorderedList } from '@chakra-ui/react'
+import { MDXProvider } from '@mdx-js/react'
+import React from 'react'
+
+const components = {
+  h1: ({ children }: any) => (
+    <Heading as="h1" size="2xl" mt="1rem">
+      {children}
+    </Heading>
+  ),
+  h2: ({ children }: any) => (
+    <Heading as="h2" size="xl" mt="1rem">
+      {children}
+    </Heading>
+  ),
+  h3: ({ children }: any) => (
+    <Heading as="h3" size="lg" mt="3rem">
+      {children}
+    </Heading>
+  ),
+  p: ({ children }: any) => <Text fontSize="md">{children}</Text>,
+  ul: UnorderedList,
+  li: ({ children }: any) => <ListItem>{children}</ListItem>,
+}
+
+export const ChakraMDXProvider: React.FC = ({ children }) => {
+  return <MDXProvider components={components}>{children}</MDXProvider>
+}
