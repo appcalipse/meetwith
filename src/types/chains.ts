7f6b241d9ef7b35117cf342c51042e329e306bc0
Ramon Canales <emaildoramon@gmail.com>
2022-02-23 17:57:30 +0000

    adding other testnets info

--- src/types/chains.ts
+++ src/types/chains.ts
@@ -56,46 +56,61 @@ export const supportedChains: ChainInfo[] = [
       },
     ],
   },
+  {
+    chain: SupportedChain.HARMONY_TESTNET,
+    id: 1,
+    name: 'Harmon Testnet',
+    rpcUrl: 'https://api.s0.b.hmny.io',
+    testnet: true,
+    nativeTokenSymbol: 'ONE',
+    subscriptionContractAddess: '0x16B589300B32C967CA3fd4386Ae904F93FC2B53e',
+    registerContractAddress: '0xF134578d326479FD3aA7c9f861AA8F84FD1f4A82',
+    acceptableTokens: [
+      {
+        token: AcceptedToken.DAI,
+        contractAddress: '0xD3F1eE2b69ffCF8AcD20ef79Fd6697Ceb99Ae024',
+      },
+    ],
+  },
+
+  {
+    chain: SupportedChain.METIS_STARTDUST,
+    id: 1,
+    name: 'Metis Stardust',
+    rpcUrl: 'https://stardust.metis.io/?owner=588',
+    testnet: true,
+    nativeTokenSymbol: 'METIS',
+    subscriptionContractAddess: '0x2809e5Cf4776640D0Da184605B0c82F803e97EFc',
+    registerContractAddress: '0xcc7f7D0Dd776a5ea17683eF6253DF8aCD3CBFA63',
+    acceptableTokens: [
+      {
+        token: AcceptedToken.DAI,
+        contractAddress: '0x2B1a67268BD808781bf5Eb761f1c43987dfa8E33',
+      },
+    ],
+  },
   //   {
   //     chain: SupportedChain.HARMONY,
   //     id: 1,
   //     name: 'Harmony',
-  //     rpcUrl: 'https://api.polygon.io/v2',
+  //     rpcUrl: 'https://api.harmony.one',
   //     testnet: false,
   //     nativeTokenSymbol: 'ONE',
   //     subscriptionContractAddess: '0x9c8c6f9f8e8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f',
   //     registerContractAddress: '0x9c8c6f9f8e8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f',
   //   },
-  //   {
-  //     chain: SupportedChain.HARMONY_TESTNET,
-  //     id: 1,
-  //     name: 'Harmon Testnet',
-  //     rpcUrl: 'https://api.polygon.io/v2',
-  //     testnet: true,
-  //     nativeTokenSymbol: 'ONE',
-  //     subscriptionContractAddess: '0x9c8c6f9f8e8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f',
-  //     registerContractAddress: '0x9c8c6f9f8e8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f',
-  //   },
+  //
   //   {
   //     chain: SupportedChain.METIS_ANDROMEDA,
   //     id: 1,
   //     name: 'Metis Andromeda',
-  //     rpcUrl: 'https://api.polygon.io/v2',
+  //     rpcUrl: 'https://andromeda.metis.io/?owner=1088',
   //     testnet: false,
   //     nativeTokenSymbol: 'METIS',
   //     subscriptionContractAddess: '0x9c8c6f9f8e8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f',
   //     registerContractAddress: '0x9c8c6f9f8e8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f',
   //   },
-  //   {
-  //     chain: SupportedChain.METIS_STARTDUST,
-  //     id: 1,
-  //     name: 'Metis Stardust',
-  //     rpcUrl: 'https://api.polygon.io/v2',
-  //     testnet: true,
-  //     nativeTokenSymbol: 'METIS',
-  //     subscriptionContractAddess: '0x9c8c6f9f8e8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f',
-  //     registerContractAddress: '0x9c8c6f9f8e8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f',
-  //   },
+  //
 ]
 
 export const getTestnetChains = (): ChainInfo[] => {
