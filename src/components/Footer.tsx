1e470e3f6d7fbf9bb8279e39e05a6c408c6db4b3
falleco <israel@zenklub.com>
2022-03-01 20:29:58 +0000

    chore: fixing pr comments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -147,7 +147,6 @@ export default function SmallWithLogoLeft() {
       <Container
         as={Stack}
         maxW={'6xl'}
-        // py={4}
         direction={{ base: 'column', md: 'row' }}
         spacing={4}
         justify={{ base: 'center', md: 'space-between' }}
