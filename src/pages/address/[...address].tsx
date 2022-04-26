018190bf193a4bc9d23e8e18fee295850877ce4f
Alex Ignez <rx.rossi@gmail.com>
2022-04-26 22:57:43 +0100

    Add custom Head component and use custom meta tags on the public calendar page

--- src/pages/address/[...address].tsx
+++ src/pages/address/[...address].tsx
@@ -4,8 +4,21 @@ import React from 'react'
 import PublicCalendar from '../../components/public-calendar'
 import { forceAuthenticationCheck } from '../../session/forceAuthenticationCheck'
 
-const Schedule: NextPage = () => <PublicCalendar />
+interface ScheduleProps {
+  currentUrl?: string
+}
 
-const EnhancedSchedule = forceAuthenticationCheck(Schedule)
+const Schedule: NextPage<ScheduleProps> = props => (
+  <PublicCalendar url={props.currentUrl} />
+)
+
+const EnhancedSchedule: NextPage<ScheduleProps> =
+  forceAuthenticationCheck(Schedule)
+
+EnhancedSchedule.getInitialProps = async ctx => {
+  const host = ctx.req?.headers.host
+
+  return { currentUrl: host && ctx.asPath ? host + ctx.asPath : undefined }
+}
 
 export default EnhancedSchedule
