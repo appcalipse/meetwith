1ec5cc0282558fd825c85f37dc0699fadb57ec8a
Alex Ignez <rx.rossi@gmail.com>
2022-04-29 16:03:40 +0100

    Move some logic from PublicCalendar to the NextPage component that uses it

--- src/pages/address/[...address].tsx
+++ src/pages/address/[...address].tsx
@@ -1,37 +1,64 @@
+import * as Sentry from '@sentry/browser'
 import { NextPage } from 'next'
+import Router from 'next/router'
 import React from 'react'
 
-import PublicCalendar from '../../components/public-calendar'
+import PublicCalendar, {
+  PublicCalendarProps,
+} from '../../components/public-calendar'
 import { forceAuthenticationCheck } from '../../session/forceAuthenticationCheck'
-import { Account } from '../../types/Account'
 import { getAccount } from '../../utils/api_helper'
+import { AccountNotFoundError } from '../../utils/errors'
+import { isProAccount } from '../../utils/subscription_manager'
 import { isValidEVMAddress } from '../../utils/validations'
 
-interface ScheduleProps {
-  currentUrl: string
-  account: Account
-}
-
-const Schedule: NextPage<ScheduleProps> = props => (
-  <PublicCalendar url={props.currentUrl} account={props.account} />
+const Schedule: NextPage<PublicCalendarProps> = props => (
+  <PublicCalendar {...props} />
 )
 
 const EnhancedSchedule: NextPage = forceAuthenticationCheck(Schedule)
 
 EnhancedSchedule.getInitialProps = async ctx => {
+  const serverSide = Boolean(ctx.res)
+
+  const redirectTo404 = () => {
+    if (serverSide) {
+      ctx.res!.writeHead(302, {
+        Location: '/404',
+      })
+      ctx.res!.end()
+    } else {
+      Router.replace('404')
+    }
+  }
+
   const address = ctx.query.address
 
-  if (address && address[0] && isValidEVMAddress(address[0])) {
-    //TODO: handle when this fails
+  if (!address || !address[0] || !isValidEVMAddress(address[0])) {
+    return redirectTo404()
+  }
+
+  try {
     const account = await getAccount(address[0])
+
+    if (account.is_invited) {
+      return redirectTo404()
+    }
+    if (!isValidEVMAddress(address[0]) && !isProAccount(account)) {
+      return redirectTo404()
+    }
 
     const host = ctx.req?.headers.host
     const currentUrl = host && ctx.asPath ? host + ctx.asPath : ''
 
-    return { currentUrl, account }
-  } else {
-    //TODO: handle this
-    throw Error('Unexpected behaviour')
+    return { currentUrl, account, serverSideRender: serverSide }
+  } catch (e) {
+    if (!(e instanceof AccountNotFoundError) && !serverSide) {
+      // does sentry work on the server side?
+      Sentry.captureException(e)
+    }
+
+    return redirectTo404()
   }
 }
 
