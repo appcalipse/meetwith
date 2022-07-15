652d0e166a87aeb92d087aab64c7b30808dab325
Ramon Canales <emaildoramon@gmail.com>
2022-07-15 11:17:09 +0100

    adding sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+// Check options to change config in https://www.npmjs.com/package/next-sitemap
+const config = {
+  siteUrl: process.env.NEXT_PUBLIC_HOSTED_AT || 'https://meetwithwallet.xyz',
+  generateRobotsTxt: true,
+
+  // ...other options
+}
+
+module.exports = config
