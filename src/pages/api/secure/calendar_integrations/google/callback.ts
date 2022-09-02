fd7e8d7ae3f21069ca51e89f0f98f111689425d3
Ramon Canales <emaildoramon@gmail.com>
2022-09-02 16:34:05 +0100

    done

--- src/pages/api/secure/calendar_integrations/google/callback.ts
+++ src/pages/api/secure/calendar_integrations/google/callback.ts
@@ -1,4 +1,4 @@
-import * as Sentry from '@sentry/nextjs'
+import * as Sentry from '@sentry/node'
 import { Auth, google } from 'googleapis'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
