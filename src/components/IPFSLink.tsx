92d5c0b20744ed90a77d36aad97ae9e302e61fa4
Ramon Canales <emaildoramon@gmail.com>
2022-09-14 10:48:55 +0100

    to finish

--- src/components/IPFSLink.tsx
+++ src/components/IPFSLink.tsx
@@ -25,7 +25,7 @@ const IPFSLink: React.FC<IPFSLinkProps> = ({ ipfsHash, title }) => {
 
       <HStack mt={0}>
         <Link
-          href={`https://ipfs.io/ipfs/${ipfsHash}?utm_source=meetwithwallet`}
+          href={`https://mww.infura-ipfs.io/ipfs/${ipfsHash}`}
           isExternal
           target="_blank"
           rel="noopener noreferrer"
