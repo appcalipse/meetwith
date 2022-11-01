a0c9d5442e5415864e38aef71066cf807092272f
Ramon Canales <emaildoramon@gmail.com>
2022-11-01 13:27:25 +0000

    removing unused files

--- src/pages/api/accounts/script.ts
+++
@@ -1,9 +0,0 @@
-import { withSentry } from '@sentry/nextjs'
-import { NextApiRequest, NextApiResponse } from 'next'
-
-import { fixCalendarConnectionIfNeeded } from '@/scripts/calendarsFix'
-
-export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
-  fixCalendarConnectionIfNeeded()
-  res.send({})
-})
