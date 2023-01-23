bbf15dd318caabd2ee748450a7d021cb95788e40
Derek Werbowy <derek.werbowy@gmail.com>
2023-01-23 15:08:59 -0500

    fix scheduling a meeting with no display name

--- src/components/schedule/base-dialog.tsx
+++ src/components/schedule/base-dialog.tsx
@@ -292,7 +292,7 @@ export const BaseMeetingDialog: React.FC<BaseMeetingDialogProps> = ({
       (participants.length === 1 &&
         (participants[0]?.account_address?.toLowerCase() ===
           currentAccount!.address.toLowerCase() ||
-          participants[0]?.name === currentAccount!.preferences?.name))
+          participants[0]?.name === (currentAccount!.preferences?.name || '')))
     ) {
       toast({
         title: 'Missing participants',
