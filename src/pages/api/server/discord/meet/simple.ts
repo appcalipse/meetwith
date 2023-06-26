6a74ffca6db2fd83a1f07a7e62b200c1d972c00a
Ramon "9Tails" Canales <emaildoramon@gmail.com>
2023-06-26 14:48:00 +0100

    Update return object

--- src/pages/api/server/discord/meet/simple.ts
+++ src/pages/api/server/discord/meet/simple.ts
@@ -7,12 +7,18 @@ export default async function simpleDiscordMeet(
   if (req.method === 'POST') {
     return res.status(200).json({
       meeting_info: {
-        DBSlotEnhanced: {
+        meeting_info_encrypted: {
           iv: 'string',
           ephemPublicKey: 'string',
           ciphertext: 'string',
           mac: 'string',
         },
+        id: 'some_uuid',
+        created_at: new Date(),
+        meeting_info_file_path: 'string',
+        version: 1,
+        source: 'mww',
+        account_address: 'the scheduler 0xAddress'
       },
       participants: ['string1', 'string2'],
       participants_not_available: ['string3', 'string4'],
