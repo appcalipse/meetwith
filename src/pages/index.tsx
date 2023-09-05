fb580c6e4d9971b53ba799f58ba76d9f524222f8
Ramon "9Tails" Canales <emaildoramon@gmail.com>
2023-09-05 17:11:10 +0100

    Merge pull request #237 from appcalipse/fix/20230905_calendarUpdateSync
    Fix/20230905 calendar update sync

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import { Box, DarkMode } from '@chakra-ui/react'
+import { getIronSession } from 'iron-session'
 import type { NextPage } from 'next'
 import React from 'react'
+
+import { sessionOptions } from '@/middleware'
+import redirectTo from '@/utils/redirect'
 
 import { Faq } from '../components/landing/FAQ'
 import { Features } from '../components/landing/Features'
 import { Hero } from '../components/landing/Hero'
 import { Plans } from '../components/landing/Plans'
 import { Why } from '../components/landing/Why'
-import { forceAuthenticationCheck } from '../session/forceAuthenticationCheck'
 
 const Home: NextPage = () => {
   return (
@@ -48,4 +51,15 @@ const Home: NextPage = () => {
   )
 }
 
-export default forceAuthenticationCheck(Home)
+Home.getInitialProps = async ctx => {
+  if (!!ctx.req) {
+    const session = await getIronSession(ctx.req, ctx.res!, sessionOptions)
+    if (session.account) {
+      return redirectTo(`/dashboard/meetings`, 302, ctx)
+    }
+  }
+
+  return {}
+}
+
+export default Home
