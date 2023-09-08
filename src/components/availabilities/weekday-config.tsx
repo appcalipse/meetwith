8a7662361f6802ffe8520d1d2a23c47fc62719a2
Ramon Canales <emaildoramon@gmail.com>
2023-09-08 15:38:14 +0100

    fixing availability config

--- src/components/availabilities/weekday-config.tsx
+++ src/components/availabilities/weekday-config.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Checkbox,
   HStack,
   IconButton,
@@ -8,7 +9,6 @@ import {
   VStack,
 } from '@chakra-ui/react'
 import { format, setDay } from 'date-fns'
-import { useEffect, useState } from 'react'
 import { FaPlus, FaTrash } from 'react-icons/fa'
 
 import { DayAvailability, TimeRange } from '../../types/Account'
@@ -17,7 +17,7 @@ import { TimeSelector } from './time-selector'
 
 type WeekdayConfigProps = {
   dayAvailability: DayAvailability
-  onChange: (weekday: number, times: TimeRange[]) => void
+  onChange: (weekday: number, times: TimeRange[] | null) => void
 }
 
 const getNewSlotEnd = (start: string) => {
@@ -31,30 +31,16 @@ const getNewSlotEnd = (start: string) => {
 }
 
 export const WeekdayConfig: React.FC<WeekdayConfigProps> = props => {
-  const [isSelected, setSelected] = useState(
-    props.dayAvailability.ranges.length > 0
-  )
-
-  const [error, setError] = useState([])
-
-  const [times, setTimes] = useState(
-    props.dayAvailability.ranges.sort(
-      (a, b) =>
-        Number(a.start.split(':').join('')) -
-        Number(b.start.split(':').join(''))
-    )
-  )
-
   const handleChangeTime = (index: number, start: string, end: string) => {
     const newTimes = [...times]
     newTimes[index] = { start, end }
-    setTimes(newTimes)
+    props.onChange(props.dayAvailability.weekday, newTimes)
   }
 
   const handleAddSlotClick = () => {
     const start = times[times.length - 1].end
 
-    setTimes([
+    props.onChange(props.dayAvailability.weekday, [
       ...times,
       {
         start,
@@ -66,88 +52,86 @@ export const WeekdayConfig: React.FC<WeekdayConfigProps> = props => {
   const handleTimeRemoveClick = (index: number) => {
     const newTimes = [...times]
     newTimes.splice(index, 1)
-    setTimes(newTimes)
-    if (newTimes.length === 0) {
-      setSelected(false)
-    }
+    props.onChange(props.dayAvailability.weekday, newTimes)
   }
 
-  useEffect(() => {
-    if (isSelected && times.length === 0) {
-      setTimes([defaultTimeRange()])
-    } else if (!isSelected) {
-      setTimes([])
-    }
-  }, [isSelected])
-
-  useEffect(() => {
-    props.onChange(props.dayAvailability.weekday, times)
-  }, [times])
-
   const iconColor = useColorModeValue('gray.500', 'gray.200')
+  const borderColor = useColorModeValue('gray.300', 'gray.700')
+
+  const isSelected = props.dayAvailability.ranges.length > 0
+  const times = [...props.dayAvailability.ranges].sort(
+    (a, b) =>
+      Number(a.start.split(':').join('')) - Number(b.start.split(':').join(''))
+  )
 
   return (
-    <HStack
-      alignItems="start"
-      minH="40px"
-      width="100%"
-      flexDirection={{ base: 'column', md: 'row' }}
-      gridGap={2}
-    >
-      <Checkbox
-        pt={2}
-        colorScheme="primary"
-        isChecked={isSelected}
-        onChange={e => setSelected(e.target.checked)}
-        minW="140px"
+    <Box py={2} width="100%" borderBottom="1px solid" borderColor={borderColor}>
+      <HStack
+        alignItems="start"
+        minH="40px"
+        width="100%"
+        flexDirection={{ base: 'column', md: 'row' }}
+        gridGap={2}
       >
-        <strong>
-          {format(setDay(new Date(), props.dayAvailability.weekday), 'cccc')}
-        </strong>
-      </Checkbox>
-      <Spacer />
-      {isSelected ? (
-        <VStack alignItems="start">
-          {times.map((time, index) => (
-            <VStack alignItems="start" key={index}>
-              <HStack>
-                <TimeSelector
-                  onChange={handleChangeTime}
-                  index={index}
-                  start={time.start}
-                  end={time.end}
-                  {...(index > 0 && { previousEnd: times[index - 1].end })}
-                  {...(index < times.length - 1 && {
-                    nextStart: times[index + 1].start,
-                  })}
-                />
-                <IconButton
-                  color={iconColor}
-                  aria-label="remove"
-                  icon={<FaTrash size={18} />}
-                  onClick={() => handleTimeRemoveClick(index)}
-                />
-              </HStack>
-              {error[index] && <Text>{error[index]}</Text>}
-            </VStack>
-          ))}
-        </VStack>
-      ) : (
-        <Text ml={2} pt={2}>
-          Not available
-        </Text>
-      )}
-      <Spacer flex={1} />
+        <Checkbox
+          pt={2}
+          colorScheme="primary"
+          isChecked={isSelected}
+          onChange={e => {
+            props.onChange(
+              props.dayAvailability.weekday,
+              e.target.checked ? [defaultTimeRange()] : null
+            )
+          }}
+          minW="140px"
+        >
+          <strong>
+            {format(setDay(new Date(), props.dayAvailability.weekday), 'cccc')}
+          </strong>
+        </Checkbox>
+        <Spacer />
+        {isSelected ? (
+          <VStack alignItems="start">
+            {times.map((time, index) => (
+              <VStack alignItems="start" key={index}>
+                <HStack>
+                  <TimeSelector
+                    onChange={handleChangeTime}
+                    index={index}
+                    start={time.start}
+                    end={time.end}
+                    {...(index > 0 && { previousEnd: times[index - 1].end })}
+                    {...(index < times.length - 1 && {
+                      nextStart: times[index + 1].start,
+                    })}
+                  />
+                  <IconButton
+                    color={iconColor}
+                    aria-label="remove"
+                    icon={<FaTrash size={18} />}
+                    onClick={() => handleTimeRemoveClick(index)}
+                  />
+                </HStack>
+              </VStack>
+            ))}
+          </VStack>
+        ) : (
+          <Text ml={2} pt={2}>
+            Not available
+          </Text>
+        )}
+        <Spacer flex={1} />
 
-      {isSelected && !times.some(time => time.end === '24:00') && (
-        <IconButton
-          margin="0"
-          color={iconColor}
-          aria-label="add"
-          icon={<FaPlus size={18} />}
-          onClick={handleAddSlotClick}
-        />
-      )}
-    </HStack>
+        {isSelected && !times.some(time => time.end === '24:00') && (
+          <IconButton
+            margin="0"
+            color={iconColor}
+            aria-label="add"
+            icon={<FaPlus size={18} />}
+            onClick={handleAddSlotClick}
+          />
+        )}
+      </HStack>
+    </Box>
   )
 }
