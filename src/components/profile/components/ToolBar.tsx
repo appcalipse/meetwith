d2350bc01a85f538e7e3174c7774f048f33c504d
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-02-08 00:29:18 +0100

    Feat: enhancements
    - added tooltip to display extra info
    - added new focus color for icons
    - updated orientation of popups (top)
    - added fill for buttons
    - implemented link check
    - added helper function for testing links validity
    - re-ordered positions of actions

--- src/components/profile/components/ToolBar.tsx
+++ src/components/profile/components/ToolBar.tsx
@@ -8,9 +8,11 @@ import {
   Popover,
   PopoverArrow,
   PopoverBody,
+  PopoverCloseButton,
   PopoverContent,
   PopoverTrigger,
   Text,
+  Tooltip,
   useDisclosure,
 } from '@chakra-ui/react'
 import * as TogglePrimitive from '@radix-ui/react-toggle'
@@ -25,7 +27,11 @@ import {
   BiListOl,
   BiListUl,
   BiStrikethrough,
+  BiUnderline,
+  BiX,
 } from 'react-icons/bi'
+
+import { isValidUrl } from '../../../utils/validations'
 const Toggle = TogglePrimitive.Root
 
 interface ToolBarProps {
@@ -34,9 +40,6 @@ interface ToolBarProps {
 
 const ToolBar: React.FC<ToolBarProps> = ({ editor }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const urlRegex = new RegExp(
-    /((http|https):\/\/[a-zA-Z0-9\-\.\_]+\.[a-zA-Z]{2,3}(\/\S*)?)/
-  )
   const [url, setUrl] = React.useState('')
   const getSelectedText = React.useCallback(() => {
     if (!editor) return ''
@@ -45,6 +48,18 @@ const ToolBar: React.FC<ToolBarProps> = ({ editor }) => {
     return editor.state.doc.textBetween(from, to, ' ')
   }, [editor])
   if (!editor) return null
+  const handleUrlSave = () => {
+    if (!isValidUrl(url)) return
+    editor
+      .chain()
+      .focus()
+      .setLink({
+        href: url,
+      })
+      .run()
+    onClose()
+    setUrl('')
+  }
   return (
     <>
       <Box
@@ -55,86 +70,154 @@ const ToolBar: React.FC<ToolBarProps> = ({ editor }) => {
         padding="10px"
       >
         <HStack justifyContent="space-between">
-          <Toggle
-            onPressedChange={() =>
-              editor.chain().focus().toggleHeading({ level: 1 }).run()
-            }
-            pressed={editor.isActive('heading', { level: 1 })}
-            asChild
+          <Tooltip
+            hasArrow
+            placement="top"
+            label="Bold"
+            aria-label="A tooltip for bold"
           >
-            <IconButton
-              backgroundColor={
-                editor.isActive('heading', { level: 1 })
-                  ? 'blue.500'
-                  : 'whiteAlpha.300'
-              }
-              aria-label="Toggle Heading"
-              icon={<BiHeading />}
-            />
-          </Toggle>
-          <Toggle
-            asChild
-            onPressedChange={() => editor.chain().focus().toggleBold().run()}
-            pressed={editor.isActive('bold')}
+            <Toggle
+              asChild
+              onPressedChange={() => editor.chain().focus().toggleBold().run()}
+              pressed={editor.isActive('bold')}
+            >
+              <IconButton
+                backgroundColor={
+                  editor.isActive('bold') ? 'primary.400' : 'whiteAlpha.300'
+                }
+                aria-label="Toggle Bold"
+                icon={<BiBold />}
+              />
+            </Toggle>
+          </Tooltip>
+          <Tooltip
+            hasArrow
+            placement="top"
+            label="Italics"
+            aria-label="A tooltip for the link"
           >
-            <IconButton
-              backgroundColor={
-                editor.isActive('bold') ? 'blue.500' : 'whiteAlpha.300'
+            <Toggle
+              asChild
+              onPressedChange={() =>
+                editor.chain().focus().toggleItalic().run()
               }
-              aria-label="Toggle Bold"
-              icon={<BiBold />}
-            />
-          </Toggle>
+              pressed={editor.isActive('italic')}
+            >
+              <IconButton
+                aria-label="Toggle Italics"
+                icon={<BiItalic />}
+                backgroundColor={
+                  editor.isActive('italic') ? 'primary.400' : 'whiteAlpha.300'
+                }
+              />
+            </Toggle>
+          </Tooltip>
+          <Tooltip
+            hasArrow
+            placement="top"
+            label="Underline"
+            aria-label="A tooltip"
+          >
+            <Toggle
+              onPressedChange={() =>
+                editor.chain().focus().toggleUnderline().run()
+              }
+              pressed={editor.isActive('underline')}
+              asChild
+            >
+              <IconButton
+                backgroundColor={
+                  editor.isActive('underline')
+                    ? 'primary.400'
+                    : 'whiteAlpha.300'
+                }
+                aria-label="Toggle Underline"
+                icon={<BiUnderline />}
+              />
+            </Toggle>
+          </Tooltip>
+          <Tooltip
+            hasArrow
+            placement="top"
+            label="Strikethrough"
+            aria-label="A tooltip for the strikethrough"
+          >
+            <Toggle
+              asChild
+              onPressedChange={() =>
+                editor.chain().focus().toggleStrike().run()
+              }
+              pressed={editor.isActive('strike')}
+            >
+              <IconButton
+                aria-label="Toggle Strikethrough"
+                backgroundColor={
+                  editor.isActive('strike') ? 'primary.400' : 'whiteAlpha.300'
+                }
+                icon={<BiStrikethrough />}
+              />
+            </Toggle>
+          </Tooltip>
           <Popover
             returnFocusOnClose={false}
             isOpen={isOpen}
             onClose={onClose}
-            placement="right"
+            placement="top-start"
             closeOnBlur={false}
           >
             <PopoverTrigger>
-              <Toggle
-                asChild
-                onPressedChange={() => {
-                  onOpen()
-                }}
-                pressed={editor.isActive('link')}
+              <Tooltip
+                hasArrow
+                placement="top"
+                label="Create A Link"
+                aria-label="A tooltip for the link"
               >
-                <IconButton
-                  backgroundColor={
-                    editor.isActive('link') ? 'blue.500' : 'whiteAlpha.300'
-                  }
-                  aria-label="Toggle Link"
-                  icon={<BiLink />}
-                />
-              </Toggle>
+                <Toggle
+                  asChild
+                  onPressedChange={() => {
+                    onOpen()
+                  }}
+                  pressed={editor.isActive('link')}
+                >
+                  <IconButton
+                    backgroundColor={
+                      editor.isActive('link') ? 'primary.400' : 'whiteAlpha.300'
+                    }
+                    aria-label="Toggle Link"
+                    icon={<BiLink />}
+                  />
+                </Toggle>
+              </Tooltip>
             </PopoverTrigger>
             <PopoverContent zIndex={99}>
               <PopoverArrow />
               <PopoverBody>
-                <Flex>
+                <Flex alignItems="center" gap={4}>
+                  <BiX cursor="pointer" size={'2em'} onClick={onClose} />
                   <Input
                     value={url}
                     onChange={e => setUrl(e.target.value)}
                     type="text"
                     placeholder="Placeholder Url"
                     aria-label="Url"
+                    onKeyDown={e => {
+                      if (e.key === 'Enter') {
+                        handleUrlSave()
+                      }
+                    }}
+                    onKeyUp={e => {
+                      if (e.key === 'Escape') {
+                        onClose()
+                      }
+                    }}
+                    width="100%"
                   />
                   <Button
-                    variant="text"
-                    disabled={!urlRegex.test(url)}
-                    onClick={() => {
-                      editor
-                        .chain()
-                        .focus()
-                        .setLink({
-                          href: url,
-                        })
-                        .run()
-                      onClose()
-                      setUrl('')
-                    }}
+                    variant="outline"
+                    disabled={!isValidUrl(url)}
+                    onClick={handleUrlSave}
                     aria-label="Save"
+                    backgroundColor="primary.400"
                   >
                     Save
                   </Button>
@@ -142,68 +225,66 @@ const ToolBar: React.FC<ToolBarProps> = ({ editor }) => {
               </PopoverBody>
             </PopoverContent>
           </Popover>
-          <Toggle
-            asChild
-            onPressedChange={() => editor.chain().focus().toggleItalic().run()}
-            pressed={editor.isActive('italic')}
-          >
-            <IconButton
-              aria-label="Toggle Italics"
-              icon={<BiItalic />}
-              backgroundColor={
-                editor.isActive('italic') ? 'blue.500' : 'whiteAlpha.300'
-              }
-            />
-          </Toggle>
-          <Toggle
-            asChild
-            onPressedChange={() => editor.chain().focus().toggleStrike().run()}
-            pressed={editor.isActive('strike')}
-          >
-            <IconButton
-              aria-label="Toggle Strikethrough"
-              backgroundColor={
-                editor.isActive('strike') ? 'blue.500' : 'whiteAlpha.300'
-              }
-              icon={<BiStrikethrough />}
-            />
-          </Toggle>
 
-          <Toggle
-            asChild
-            onPressedChange={() =>
-              editor.chain().focus().toggleBulletList().run()
-            }
-            pressed={editor.isActive('bulletList')}
+          <Tooltip
+            hasArrow
+            placement="top"
+            label="Create an unordered list"
+            aria-label="A tooltip for the link"
           >
-            <IconButton
-              aria-label="Toggle Unordered List"
-              backgroundColor={
-                editor.isActive('bulletList') ? 'blue.500' : 'whiteAlpha.300'
+            <Toggle
+              asChild
+              onPressedChange={() =>
+                editor.chain().focus().toggleBulletList().run()
               }
-              icon={<BiListUl />}
-            />
-          </Toggle>
-          <Toggle
-            asChild
-            onPressedChange={() =>
-              editor.chain().focus().toggleOrderedList().run()
-            }
-            pressed={editor.isActive('orderedList')}
+              pressed={editor.isActive('bulletList')}
+            >
+              <IconButton
+                aria-label="Toggle Unordered List"
+                backgroundColor={
+                  editor.isActive('bulletList')
+                    ? 'primary.400'
+                    : 'whiteAlpha.300'
+                }
+                icon={<BiListUl />}
+              />
+            </Toggle>
+          </Tooltip>
+          <Tooltip
+            hasArrow
+            placement="top"
+            label="Ordered list"
+            aria-label="A tooltip for the link"
           >
-            <IconButton
-              aria-label="Toggle List"
-              backgroundColor={
-                editor.isActive('orderedList') ? 'blue.500' : 'whiteAlpha.300'
+            <Toggle
+              asChild
+              onPressedChange={() =>
+                editor.chain().focus().toggleOrderedList().run()
               }
-              icon={<BiListOl />}
-            />
-          </Toggle>
+              pressed={editor.isActive('orderedList')}
+            >
+              <IconButton
+                aria-label="Toggle List"
+                backgroundColor={
+                  editor.isActive('orderedList')
+                    ? 'primary.400'
+                    : 'whiteAlpha.300'
+                }
+                icon={<BiListOl />}
+              />
+            </Toggle>
+          </Tooltip>
         </HStack>
       </Box>
       <BubbleMenu
         editor={editor as Editor}
-        tippyOptions={{ duration: 100, trigger: 'manual' }}
+        tippyOptions={{
+          duration: 100,
+          trigger: 'manual',
+          theme: 'material',
+          placement: 'top-start',
+          animateFill: true,
+        }}
         shouldShow={editor => editor.editor.isActive('link')}
         className="floating-menu"
       >
