fb54726149c1d83ec4255c8f87a54b0c853782eb
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-04-09 02:13:04 +0100

    feat: moved render treee to content and implemenmted invite navigation

--- src/components/profile/Group.tsx
+++ src/components/profile/Group.tsx
@@ -17,12 +17,13 @@ import {
   useColorModeValue,
   VStack,
 } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import React, { ReactNode, useEffect, useState } from 'react'
 import { FaCaretDown, FaPlus, FaSearch } from 'react-icons/fa'
 
 import { Account } from '@/types/Account'
 import { GetGroupsResponse } from '@/types/Group'
-import { getGroups } from '@/utils/api_helper'
+import { getGroups, getGroupsEmpty } from '@/utils/api_helper'
 
 import GroupCard from '../group/GroupCard'
 
@@ -35,14 +36,11 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
   const [loading, setLoading] = useState(true)
   const [noMoreFetch, setNoMoreFetch] = useState(false)
   const [firstFetch, setFirstFetch] = useState(true)
-  const bgColor = useColorModeValue('white', '#1F2933')
+  const { invite } = useRouter().query
   const fetchMeetings = async (reset?: boolean) => {
     const PAGE_SIZE = 5
     setLoading(true)
-    const newGroups = (await getGroups(
-      PAGE_SIZE,
-      groups.length
-    )) as Array<GetGroupsResponse>
+    const newGroups = await getGroups(PAGE_SIZE, groups.length)
     if (newGroups.length < PAGE_SIZE) {
       setNoMoreFetch(true)
     }
@@ -59,10 +57,13 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
   useEffect(() => {
     resetState()
   }, [currentAccount?.address])
+  useEffect(() => {
+    // Add modal logic here
+  }, [invite])
   let content: ReactNode
   if (firstFetch) {
     content = (
-      <VStack alignItems="center" mb={8}>
+      <VStack alignItems="center" mb={6}>
         <Image src="/assets/schedule.svg" height="200px" alt="Loading..." />
         <HStack pt={8}>
           <Spinner />
@@ -72,7 +73,7 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
     )
   } else if (groups.length === 0) {
     content = (
-      <VStack alignItems="center" mt={8} gap={4}>
+      <VStack alignItems="center" mt={6} gap={4}>
         <Image src="/assets/no_group.svg" height="200px" alt="Loading..." />
         <Text fontSize="lg">
           You will see your Groups here once you created a new Group.
@@ -94,13 +95,8 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
     )
   } else {
     content = (
-      <VStack mb={8}>
-        <Accordion
-          allowMultiple
-          bgColor={bgColor}
-          width="100%"
-          borderRadius="lg"
-        >
+      <VStack my={6}>
+        <Accordion allowMultiple width="100%">
           {groups.map(group => (
             <GroupCard
               key={group.id}
@@ -154,41 +150,6 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
         >
           Create new group
         </Button>
-      </HStack>
-      <HStack justifyContent="space-between" mt={2}>
-        <HStack>
-          <Button>Filters</Button>
-          <Menu>
-            <MenuButton as={Button} rightIcon={<FaCaretDown />}>
-              Sort
-            </MenuButton>
-            <MenuList>
-              <MenuItem>Ascending</MenuItem>
-              <MenuItem>Descending</MenuItem>
-            </MenuList>
-          </Menu>
-        </HStack>
-        <Box width="300px" position="relative">
-          <Input
-            type="text"
-            placeholder="Search"
-            borderColor={useColorModeValue('gray.300', 'whiteAlpha.300')}
-            paddingRight={45}
-          />
-          <Box
-            background="none"
-            position="absolute"
-            right={5}
-            borderRadius={10}
-            color={useColorModeValue('gray.300', 'whiteAlpha.300')}
-            insetY={0}
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <FaSearch />
-          </Box>
-        </Box>
       </HStack>
       {content}
     </Flex>
