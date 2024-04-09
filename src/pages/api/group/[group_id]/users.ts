dd9a6ce447a2350b093987086d5e4d433115fa90
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-04-09 02:08:47 +0100

    feat: reworked response mapping to suite new api query

--- src/pages/api/group/[group_id]/users.ts
+++ src/pages/api/group/[group_id]/users.ts
@@ -22,20 +22,20 @@ const handle = async (req: NextApiRequest, res: NextApiResponse) => {
       )
       const responseJson: Array<GroupMember> = groups.map(group => ({
         displayName: group.preferences.name,
-        address: group.address,
-        role: group.role,
-        invitePending: group.invite_pending,
-        calendarConnected: group.calendars.calendars?.length > 0,
+        address: group.group_members[0]?.address,
+        role: group.group_members[0].role,
+        invitePending: group.group_members[0].invite_pending,
+        calendarConnected: group.calendars[0].calendars.length > 0,
       }))
       return res.status(200).json(responseJson)
-    } catch (error) {
+    } catch (error: any) {
       if (error instanceof NotGroupMemberError) {
         return res.status(403).json({ error: error.message })
       }
       if (error instanceof GroupNotExistsError) {
         return res.status(404).json({ error: error.message })
       }
-      return res.status(500).send(error)
+      return res.status(500).send(error.message)
     }
   }
 }
