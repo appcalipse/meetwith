e0aec1367e6fe44b5029df12108f6921f794c03b
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-04-14 23:34:53 +0100

    feat: added logic for new group_invites table

--- src/pages/api/secure/group/[group_id]/users.ts
+++ src/pages/api/secure/group/[group_id]/users.ts
@@ -20,15 +20,31 @@ const handle = async (req: NextApiRequest, res: NextApiResponse) => {
         Number(req.query.limit as string),
         Number(req.query.offset as string)
       )
-      const responseJson: Array<GroupMember> = groups.map(group => ({
-        displayName: group.preferences.name,
-        address: group.group_members[0]?.address,
-        role: group.group_members[0].role,
-        invitePending: group.group_members[0].invite_pending,
-        calendarConnected: group.calendars[0].calendars.length > 0,
-      }))
+      const responseJson: Array<GroupMember> = groups
+        .map(group => {
+          if (group.group_members.length > 0) {
+            return {
+              displayName: group.preferences?.name,
+              address: group.group_members?.[0]?.member_id as string,
+              role: group.group_members?.[0].role,
+              invitePending: false,
+              calendarConnected: group.calendars[0].calendars.length > 0,
+            }
+          } else {
+            return {
+              displayName: group.preferences?.name,
+              address: (group.group_invites?.[0]?.user_id ||
+                group.group_invites?.[0]?.email) as string,
+              role: group.group_invites?.[0].role,
+              invitePending: true,
+              calendarConnected: group.calendars[0].calendars.length > 0,
+            }
+          }
+        })
+        .filter(Boolean)
       return res.status(200).json(responseJson)
     } catch (error: any) {
+      console.log(error)
       if (error instanceof NotGroupMemberError) {
         return res.status(403).json({ error: error.message })
       }
