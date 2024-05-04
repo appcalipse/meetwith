a507ac746df46425b4996c727e6f53d86f829952
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-05-04 20:54:11 +0100

    feat: added globals

--- jest.config.js
+++ jest.config.js
@@ -9,12 +9,15 @@ const createJestConfig = nextJest({
 const esModules = ['@wagmi'].join('|')
 
 // Add any custom config to be passed to Jest
+
+/** @type {import('jest').Config} */
 const customJestConfig = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', __dirname],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   collectCoverageFrom: ['./src/**'],
   verbose: true,
@@ -37,34 +40,4 @@ const customJestConfig = {
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = async () => ({
-  /**
-   * Using ...(await createJestConfig(customJestConfig)()) to override transformIgnorePatterns
-   * provided byt next/jest.
-   *
-   * @link https://github.com/vercel/next.js/issues/36077#issuecomment-1096635363
-   */
-  ...(await createJestConfig(customJestConfig)()),
-  /**
-   * Swiper use ECMAScript Modules (ESM) and Jest provides some experimental support for it
-   * but "node_modules" are not transpiled by next/jest yet.
-   *
-   * The "transformIgnorePatterns" on "jest.config.js" prevents the Swiper files from being
-   * transformed by Jest but it affects the CSS files that are provided by this package.
-   * Mocking these CSS files is the solution that demands the smallest configuration.
-   *
-   * @link https://github.com/vercel/next.js/issues/36077#issuecomment-1096698456
-   * @link https://jestjs.io/docs/ecmascript-modules
-   */
-  collectCoverageFrom: [
-    'pages/**/*.{js,jsx,ts,tsx}',
-    'features/**/*.{js,jsx,ts,tsx}',
-    'components/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/reportWebVitals.ts',
-    '!**/node_modules/**',
-  ],
-  verbose: true,
-  testPathIgnorePatterns: ['<rootDir>/src/__tests__/tofix'],
-  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`],
-})
+module.exports = createJestConfig(customJestConfig)
