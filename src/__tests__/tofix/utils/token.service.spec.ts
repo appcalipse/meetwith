1e8213be88a8578dafca171b14e1eaaa51ab38c2
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-05-07 00:00:12 +0100

    chore: added mocks for test

--- src/__tests__/tofix/utils/token.service.spec.ts
+++ src/__tests__/tofix/utils/token.service.spec.ts
@@ -1,4 +1,5 @@
 import { ethers } from 'ethers'
+import * as thirdWeb from 'thirdweb'
 
 import { getTokenBalance, getTokenInfo } from '@/utils/token.service'
 
@@ -8,70 +9,73 @@ import {
   USDC_ELEMENT,
   USDT_ELEMENT,
 } from '../../../testing/mocks'
-
+jest.mock('thirdweb')
 describe('get balance for tokens', () => {
-  beforeAll(() => {
-    jest
-      .spyOn(ethers, 'Contract')
-      .mockImplementation((addressOrName: string) => {
-        return {
-          name: async () => {
-            switch (addressOrName) {
-              case USDT_ELEMENT.itemId:
-                return Promise.resolve(USDT_ELEMENT.itemName)
-              case NFT_ELEMENT.itemId:
-                return Promise.resolve(NFT_ELEMENT.itemName)
-            }
-          },
-          symbol: async () => {
-            switch (addressOrName) {
-              case USDT_ELEMENT.itemId:
-                return Promise.resolve(USDT_ELEMENT.itemSymbol)
-              case NFT_ELEMENT.itemId:
-                return Promise.resolve(NFT_ELEMENT.itemSymbol)
-            }
-          },
-          decimals: async () => {
-            switch (addressOrName) {
-              case USDT_ELEMENT.itemId:
-                return Promise.resolve(18n)
-              case NFT_ELEMENT.itemId:
-                return Promise.resolve(0)
-            }
-          },
-          baseURI: async () => {
-            switch (addressOrName) {
-              case USDT_ELEMENT.itemId:
-                throw new Error('Not a NFT')
-              case NFT_ELEMENT.itemId:
-                return Promise.resolve('fakeBaseURi')
-            }
-          },
-          balanceOf: async () => {
-            switch (addressOrName) {
-              case DAI_ELEMENT.itemId:
-                return Promise.resolve(BigInt(1e18))
-              case USDT_ELEMENT.itemId:
-                return Promise.resolve(0n)
-              case USDC_ELEMENT.itemId:
-                return Promise.resolve(0n)
-              case NFT_ELEMENT.itemId:
-                return Promise.resolve(1n)
-              default:
-                return Promise.resolve(0n)
-            }
-          },
-        } as any
-      })
-  })
+  // beforeAll(() => {
+  //   jest.spyOn(thirdWeb, 'readContract').mockImplementation(({ params }) => {
+  //     const addressOrName = params?.[0]
+  //     console.log(addressOrName)
+  //     return {
+  //       name: async () => {
+  //         switch (addressOrName) {
+  //           case USDT_ELEMENT.itemId:
+  //             return Promise.resolve(USDT_ELEMENT.itemName)
+  //           case NFT_ELEMENT.itemId:
+  //             return Promise.resolve(NFT_ELEMENT.itemName)
+  //         }
+  //       },
+  //       symbol: async () => {
+  //         switch (addressOrName) {
+  //           case USDT_ELEMENT.itemId:
+  //             return Promise.resolve(USDT_ELEMENT.itemSymbol)
+  //           case NFT_ELEMENT.itemId:
+  //             return Promise.resolve(NFT_ELEMENT.itemSymbol)
+  //         }
+  //       },
+  //       decimals: async () => {
+  //         switch (addressOrName) {
+  //           case USDT_ELEMENT.itemId:
+  //             return Promise.resolve(18n)
+  //           case NFT_ELEMENT.itemId:
+  //             return Promise.resolve(0)
+  //         }
+  //       },
+  //       baseURI: async () => {
+  //         switch (addressOrName) {
+  //           case USDT_ELEMENT.itemId:
+  //             throw new Error('Not a NFT')
+  //           case NFT_ELEMENT.itemId:
+  //             return Promise.resolve('fakeBaseURi')
+  //         }
+  //       },
+  //       balanceOf: async () => {
+  //         switch (addressOrName) {
+  //           case DAI_ELEMENT.itemId:
+  //             return Promise.resolve(BigInt(1e18))
+  //           case USDT_ELEMENT.itemId:
+  //             return Promise.resolve(0n)
+  //           case USDC_ELEMENT.itemId:
+  //             return Promise.resolve(0n)
+  //           case NFT_ELEMENT.itemId:
+  //             return Promise.resolve(1n)
+  //           default:
+  //             return Promise.resolve(0n)
+  //         }
+  //       },
+  //     } as any
+  //   })
+  // })
 
   afterAll(() => {
-    jest.unmock('ethers')
+    jest.unmock('thirdweb')
   })
 
   const WALLET_ADDRESS = '0x4F834fbb8b10F2cCbCBcA08D183aF3b9bdfCb2be'
 
   it('returns 0 because wallet has no balance of Mumbai USDT', async () => {
+    jest
+      .spyOn(thirdWeb, 'readContract')
+      .mockResolvedValue(Promise.resolve(BigInt(0n) as unknown as unknown[]))
     const balance = await getTokenBalance(
       WALLET_ADDRESS,
       USDT_ELEMENT.itemId as `0x${string}`,
@@ -81,6 +85,9 @@ describe('get balance for tokens', () => {
   })
 
   it('returns more than zero cause wallet has balance of a mock DAI', async () => {
+    jest
+      .spyOn(thirdWeb, 'readContract')
+      .mockResolvedValue(Promise.resolve(BigInt(1e18) as unknown as unknown[]))
     const balance = await getTokenBalance(
       WALLET_ADDRESS,
       DAI_ELEMENT.itemId as `0x${string}`,
@@ -90,6 +97,9 @@ describe('get balance for tokens', () => {
   })
 
   it('returns one cause wallet holds NFT', async () => {
+    jest
+      .spyOn(thirdWeb, 'readContract')
+      .mockResolvedValue(Promise.resolve(BigInt(1n) as unknown as unknown[]))
     const balance = await getTokenBalance(
       WALLET_ADDRESS,
       NFT_ELEMENT.itemId as `0x${string}`,
@@ -99,11 +109,24 @@ describe('get balance for tokens', () => {
   })
 
   it('returns token info for NFT', async () => {
+    jest.spyOn(thirdWeb, 'readContract').mockImplementation(({ method }) => {
+      switch (method) {
+        case 'name':
+          return Promise.resolve(NFT_ELEMENT.itemName as unknown as unknown[])
+        case 'symbol':
+          return Promise.resolve(NFT_ELEMENT.itemSymbol as unknown as unknown[])
+        case 'decimals':
+          return Promise.resolve(0 as unknown as unknown[])
+        case 'function baseURI()':
+          return Promise.resolve('fakeBaseURi' as unknown as unknown[])
+        default:
+          return Promise.resolve(0n as unknown as unknown[])
+      }
+    })
     const tokenInfo = await getTokenInfo(
       NFT_ELEMENT.itemId as `0x${string}`,
       NFT_ELEMENT.chain!
     )
-
     expect(tokenInfo?.itemName).toEqual(NFT_ELEMENT.itemName)
     expect(tokenInfo?.itemSymbol).toEqual(NFT_ELEMENT.itemSymbol)
     expect(tokenInfo?.type).toEqual(NFT_ELEMENT.type)
@@ -112,6 +135,22 @@ describe('get balance for tokens', () => {
   })
 
   it('returns token info for ERC20', async () => {
+    jest.spyOn(thirdWeb, 'readContract').mockImplementation(({ method }) => {
+      switch (method) {
+        case 'name':
+          return Promise.resolve(USDT_ELEMENT.itemName as unknown as unknown[])
+        case 'symbol':
+          return Promise.resolve(
+            USDT_ELEMENT.itemSymbol as unknown as unknown[]
+          )
+        case 'decimals':
+          return Promise.resolve(0 as unknown as unknown[])
+        case 'function baseURI()':
+          return Promise.resolve(0 as unknown as unknown[])
+        default:
+          return Promise.resolve(0n as unknown as unknown[])
+      }
+    })
     const tokenInfo = await getTokenInfo(
       USDT_ELEMENT.itemId as `0x${string}`,
       USDT_ELEMENT.chain!
