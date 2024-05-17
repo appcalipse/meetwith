8797ce8df4505971148819549a30df6d6f79d85d
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-05-18 00:25:22 +0100

    feat(invitation): created join and reject endpoints

---
+++ src/pages/api/secure/group/[group_id]/join.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { withSessionRoute } from '@/ironAuth/withSessionApiRoute'
+import { acceptGroupInvite, initDB } from '@/utils/database'
+
+const handle = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    initDB()
+    const account_address = req.session.account!.address
+    if (!account_address) {
+      return res.status(401).send('Unauthorized')
+    }
+    try {
+      const group_id = req.query.group_id as string
+      await acceptGroupInvite(group_id, account_address)
+      return res.status(200).json({
+        success: true,
+      })
+    } catch (e) {
+      return res.status(500).send(e)
+    }
+  }
+  return res.status(405).send('Method not allowed')
+}
+export default withSessionRoute(handle)
