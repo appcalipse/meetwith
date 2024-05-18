8fdf56ad6d72800d51936e43196d73ff28ea7c3b
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-05-18 17:58:45 +0100

    improvements(types): created types for notification requests

--- src/types/Requests.ts
+++ src/types/Requests.ts
@@ -3,6 +3,7 @@ import { Encrypted } from 'eth-crypto'
 import { Account } from './Account'
 import {
   ConferenceMeeting,
+  GroupNotificationType,
   MeetingDecrypted,
   NotBefore,
   ParticipantMappingType,
@@ -72,7 +73,11 @@ export interface MeetingCreationSyncRequest extends MeetingSyncRequest {
   title?: string
   content?: string
 }
-
+export interface GroupInviteNotifyRequest {
+  group_id: string
+  accountsToNotify: string[]
+  notifyType: GroupNotificationType
+}
 export interface MeetingCancelSyncRequest extends MeetingSyncRequest {
   addressesToRemove: string[]
   guestsToRemove: ParticipantInfo[]
