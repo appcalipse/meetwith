885012ca1f76a2cad5b552f088886f677872c287
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-06-15 18:13:08 +0100

    feat: created a handler for redirections'

---
+++ src/components/redirect/index.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router'
+import React, { FC, useEffect } from 'react'
+
+const RedirectHandler: FC = () => {
+  const { query, push, prefetch } = useRouter()
+  const { state, redirect, intent } = query
+  console.log(redirect)
+  useEffect(() => {
+    if (state) {
+      return
+    }
+    if (redirect) {
+      prefetch(redirect as string)
+      push(`${redirect as string}&intent=${intent}`)
+    }
+  }, [state, redirect])
+  return null
+}
+
+export default RedirectHandler
