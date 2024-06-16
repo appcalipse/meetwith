9068cca0523f0d8bc472a58f83a13c7791bd2dcc
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-06-16 23:25:24 +0100

    improvements: implemented new ui

--- src/components/MeetSlotPicker/calendar/Root.jsx
+++ src/components/MeetSlotPicker/calendar/Root.jsx
@@ -1,4 +1,12 @@
-import { Center, Flex, HStack } from '@chakra-ui/react'
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
 import { addMonths, format, isSameMonth, isToday, subMonths } from 'date-fns'
 import PropTypes from 'prop-types'
 import React, { useState } from 'react'
@@ -74,109 +82,123 @@ function Root({
   }
 
   return (
-    <Grid>
-      <MonthYear>
-        <HStack>
-          <PrevMonth disabled={isAnimating} onClick={handlePrevMonth}>
-            <FaChevronLeft />
-          </PrevMonth>
+    <VStack flex={1.5} alignItems="flex-start">
+      <Heading size="md" mr="auto">
+        Select Time
+      </Heading>
+      <Grid>
+        <MonthYear>
+          <HStack>
+            <PrevMonth disabled={isAnimating} onClick={handlePrevMonth}>
+              <FaChevronLeft />
+            </PrevMonth>
 
-          <Wrapper>
-            <CurrentMonth animation={animation}>
-              {format(selectedMonth, 'MMMM yyyy')}
-            </CurrentMonth>
+            <Wrapper>
+              <CurrentMonth animation={animation}>
+                {format(selectedMonth, 'MMMM yyyy')}
+              </CurrentMonth>
 
-            <FakeCurrentMonth animation={animation}>
-              {format(fakeMonth, 'MMMM yyy')}
-            </FakeCurrentMonth>
-          </Wrapper>
+              <FakeCurrentMonth animation={animation}>
+                {format(fakeMonth, 'MMMM yyy')}
+              </FakeCurrentMonth>
+            </Wrapper>
 
-          <NextMonth disabled={isAnimating} onClick={handleNextMonth}>
-            <Flex justifyContent="flex-end">
-              <FaChevronRight />
-            </Flex>
-          </NextMonth>
-        </HStack>
-      </MonthYear>
+            <NextMonth disabled={isAnimating} onClick={handleNextMonth}>
+              <Flex justifyContent="flex-end">
+                <FaChevronRight />
+              </Flex>
+            </NextMonth>
+          </HStack>
+        </MonthYear>
 
-      <Wrapper>
-        {!!loading ? (
-          <Center minH="455px">
-            <Loading label="Checking availability" />
-          </Center>
-        ) : (
-          <Wrapper>
-            <Calendar animation={animation} onAnimationEnd={handleAnimationEnd}>
-              <DaysOfWeek>
-                <WeekDays>
-                  {WEEK_DAYS.map(weekDay => {
-                    return <WeekDay key={weekDay}>{weekDay}</WeekDay>
-                  })}
-                </WeekDays>
-              </DaysOfWeek>
+        <Wrapper>
+          {!!loading ? (
+            <Center minH="455px">
+              <Loading label="Checking availability" />
+            </Center>
+          ) : (
+            <Wrapper>
+              <Calendar
+                animation={animation}
+                onAnimationEnd={handleAnimationEnd}
+              >
+                <DaysOfWeek>
+                  <WeekDays>
+                    {WEEK_DAYS.map(weekDay => {
+                      return (
+                        <Text fontWeight="500" color="white" key={weekDay}>
+                          {weekDay}
+                        </Text>
+                      )
+                    })}
+                  </WeekDays>
+                </DaysOfWeek>
 
-              <MonthDays>
-                {days.map(day => {
-                  const _isSameMonth = isSameMonth(day, startDay)
-                  if (!_isSameMonth) {
-                    return <MonthDay key={day} />
-                  }
-
-                  const formatted = format(day, 'd')
-                  const _isToday = isToday(day)
-                  const isValid = validator ? validator(day) : true
-                  return (
-                    <MonthDay
-                      key={day}
-                      isValid={isValid}
-                      isToday={_isToday}
-                      onClick={() => isValid && handlePickDay(day)}
-                    >
-                      {formatted}
-                    </MonthDay>
-                  )
-                })}
-              </MonthDays>
-            </Calendar>
-
-            <FakeCalendar animation={animation}>
-              <DaysOfWeek>
-                <WeekDays>
-                  {WEEK_DAYS.map(weekDay => {
-                    return <WeekDay key={weekDay}>{weekDay}</WeekDay>
-                  })}
-                </WeekDays>
-              </DaysOfWeek>
-
-              <DaysOfMonth>
                 <MonthDays>
-                  {fakeDays.map(fakeDay => {
-                    const _isSameMonth = isSameMonth(fakeDay, fakeStartDay)
+                  {days.map(day => {
+                    const _isSameMonth = isSameMonth(day, startDay)
                     if (!_isSameMonth) {
-                      return <MonthDay key={fakeDay} />
+                      return <MonthDay key={day} />
                     }
 
-                    const formatted = format(fakeDay, 'd')
-                    const _isToday = isToday(fakeDay)
-                    const isValid = validator ? validator(fakeDay) : true
+                    const formatted = format(day, 'd')
+                    const _isToday = isToday(day)
+                    const _isWeekend = [0, 6].includes(day.getDay())
+                    const isValid = validator ? validator(day) : true
                     return (
                       <MonthDay
-                        key={fakeDay}
-                        disabled={!_isSameMonth}
+                        key={day}
                         isValid={isValid}
                         isToday={_isToday}
+                        isWeekend={_isWeekend}
+                        onClick={() => isValid && handlePickDay(day)}
                       >
                         {formatted}
                       </MonthDay>
                     )
                   })}
                 </MonthDays>
-              </DaysOfMonth>
-            </FakeCalendar>
-          </Wrapper>
-        )}
-      </Wrapper>
-    </Grid>
+              </Calendar>
+
+              <FakeCalendar animation={animation}>
+                <DaysOfWeek>
+                  <WeekDays>
+                    {WEEK_DAYS.map(weekDay => {
+                      return <WeekDay key={weekDay}>{weekDay}</WeekDay>
+                    })}
+                  </WeekDays>
+                </DaysOfWeek>
+
+                <DaysOfMonth>
+                  <MonthDays>
+                    {fakeDays.map(fakeDay => {
+                      const _isSameMonth = isSameMonth(fakeDay, fakeStartDay)
+                      if (!_isSameMonth) {
+                        return <MonthDay key={fakeDay} />
+                      }
+
+                      const formatted = format(fakeDay, 'd')
+                      const _isToday = isToday(fakeDay)
+                      const isValid = validator ? validator(fakeDay) : true
+                      return (
+                        <MonthDay
+                          key={fakeDay}
+                          disabled={!_isSameMonth}
+                          isValid={isValid}
+                          isToday={_isToday}
+                        >
+                          {formatted}
+                        </MonthDay>
+                      )
+                    })}
+                  </MonthDays>
+                </DaysOfMonth>
+              </FakeCalendar>
+            </Wrapper>
+          )}
+        </Wrapper>
+      </Grid>
+    </VStack>
   )
 }
 
