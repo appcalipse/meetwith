725b034f1956854185b3b3d55a15424df0d97649
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-06-19 00:06:15 +0100

    improvements: implemented new ui and implemented logic for the token gate check

--- src/components/token-gate/TokenGateValidation.tsx
+++ src/components/token-gate/TokenGateValidation.tsx
@@ -1,10 +1,20 @@
-import { Box, Text } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import { Box, HStack, Image, Text } from '@chakra-ui/react'
+import { bg } from 'date-fns/locale'
+import React, { useContext, useEffect, useState } from 'react'
 
+import Loading from '@/components/Loading'
+import { getTokenIcon } from '@/components/profile/SubscriptionDialog'
+import { OnboardingModalContext } from '@/providers/OnboardingModalProvider'
+import { useLogin } from '@/session/login'
 import { Account } from '@/types/Account'
+import { AcceptedToken } from '@/types/chains'
 import { GateCondition } from '@/types/TokenGating'
 import { getGateCondition } from '@/utils/api_helper'
-import { isConditionValid } from '@/utils/token.gate.service'
+import { formatUnits } from '@/utils/generic_utils'
+import {
+  ConditionValidation,
+  isConditionValid,
+} from '@/utils/token.gate.service'
 
 import HumanReadableGate from './HumanReadableGate'
 
@@ -15,23 +25,43 @@ interface TokenGateValidationProps {
   setIsGateValid: (isValid: boolean) => void
   isGateValid: boolean
 }
+
 const TokenGateValidation: React.FC<TokenGateValidationProps> = props => {
   const [chosenGate, setChosenGate] = useState<GateCondition | null>(null)
+  const [userTokens, setUserTokens] = useState<ConditionValidation['tokens']>(
+    []
+  )
+  const [firstToken, setFirstToken] = useState<
+    GateCondition['elements'][number] | undefined
+  >(undefined)
   const [loading, setLoading] = useState<boolean>(true)
-
+  const { openConnection } = useContext(OnboardingModalContext)
+  const { currentAccount } = useLogin()
   const handleGateValidation = async (gateId: string) => {
     if (!gateId || gateId === 'No gate') return
     setLoading(true)
     const chosenGate = await getGateCondition(gateId)
-
     setChosenGate(chosenGate!.definition)
     if (props.userAccount) {
-      const isValid = await isConditionValid(
+      const valid = await isConditionValid(
         chosenGate!.definition!,
         props.userAccount.address!
       )
-      props.setIsGateValid(isValid)
+      props.setIsGateValid(valid.isValid)
+      const userTokens = valid.tokens
+      const token = chosenGate?.definition?.elements.find(val => {
+        const userToken = userTokens.find(
+          userToken => userToken.symbol === val.itemSymbol
+        )
+        if (valid.isValid) {
+          return val.minimumBalance <= (userToken?.balance || 0)
+        } else {
+          return val.minimumBalance > (userToken?.balance || 0)
+        }
+      })
+      setFirstToken(token)
     } else {
+      setFirstToken(chosenGate?.definition?.elements[0])
       props.setIsGateValid(false)
     }
 
@@ -40,18 +70,93 @@ const TokenGateValidation: React.FC<TokenGateValidationProps> = props => {
 
   useEffect(() => {
     handleGateValidation(props.gate)
-  }, [props.gate])
-
-  return (
-    <Box textAlign="center" mt={10}>
-      {!loading && <HumanReadableGate gateCondition={chosenGate!} center />}
-      {!loading && props.isGateValid && (
-        <Text color="green.500">Your wallet has the necessary tokens</Text>
+  }, [props.gate, currentAccount])
+  const bgColor = !props.userAccount
+    ? '#FFC700'
+    : props.isGateValid
+    ? 'neutral.900'
+    : '#F3B5B4'
+  return loading ? (
+    <Box
+      mt={10}
+      position="absolute"
+      top={-48}
+      mx={'auto'}
+      w={'fit-content'}
+      insetX={'0'}
+    >
+      <Loading />
+    </Box>
+  ) : (
+    <Box
+      textAlign="left"
+      mt={10}
+      position="absolute"
+      top={-36}
+      mx={'auto'}
+      w={'fit-content'}
+      insetX={'0'}
+      bg={bgColor}
+      maxW={'500px'}
+      p={4}
+      rounded={'lg'}
+      fontWeight={'500'}
+    >
+      {!props.userAccount && firstToken && (
+        <HStack gap={4} color="neutral.900">
+          <Image
+            src={getTokenIcon(firstToken.itemSymbol as AcceptedToken)}
+            alt={firstToken.itemSymbol}
+            w={30}
+            h={30}
+          />
+          <Text>
+            You need to hold{' '}
+            {formatUnits(firstToken.minimumBalance, firstToken.decimals || 0)}{' '}
+            {firstToken.itemSymbol} to be eligible to schedule this meeting.
+            Please sign in to check.
+          </Text>
+          <Text
+            cursor="pointer"
+            whiteSpace="nowrap"
+            onClick={() => {
+              openConnection(false)
+            }}
+          >
+            Sign In
+          </Text>
+        </HStack>
       )}
-      {!loading && !props.isGateValid && (
-        <Text color="red.500">
-          Your wallet does not have the necessary tokens
-        </Text>
+      {props.isGateValid && props.userAccount && firstToken && (
+        <HStack gap={4}>
+          <Image
+            src={getTokenIcon(firstToken.itemSymbol as AcceptedToken)}
+            alt={firstToken.itemSymbol}
+            w={30}
+            h={30}
+          />
+          <Text>
+            Congratulations. You are eligible to schedule this meeting because
+            you hold{' '}
+            {formatUnits(firstToken.minimumBalance, firstToken.decimals || 0)}{' '}
+            {firstToken.itemSymbol}
+          </Text>
+        </HStack>
+      )}
+      {!props.isGateValid && props.userAccount && firstToken && (
+        <HStack gap={4}>
+          <Image
+            src={getTokenIcon(firstToken.itemSymbol as AcceptedToken)}
+            alt={firstToken.itemSymbol}
+            w={30}
+            h={30}
+          />
+          <Text color="neutral.900">
+            You need to hold{' '}
+            {formatUnits(firstToken.minimumBalance, firstToken.decimals || 0)}{' '}
+            {firstToken.itemSymbol} to be eligible to schedule this meeting.
+          </Text>
+        </HStack>
       )}
     </Box>
   )
