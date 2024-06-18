5495e6180bf5852b15bace14e95ff7b329ace352
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-06-19 00:02:51 +0100

    improvements: implemented new change to validator

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -705,7 +705,7 @@ const saveMeeting = async (
         schedulerAccount.account_address!
       )
 
-      if (!valid) {
+      if (!valid.isValid) {
         throw new GateConditionNotValidError()
       }
     }
