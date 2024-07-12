54869e120521de5314bab19a7d6da80687ef7d66
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-07-12 16:12:15 +0100

    improvements: added modal for when only admin intends to become member

--- src/components/group/GroupCard.tsx
+++ src/components/group/GroupCard.tsx
@@ -53,6 +53,8 @@ export interface IGroupCard extends GetGroupsResponse {
   currentAccount: Account
   onAddNewMember: (groupId: string, groupName: string) => void
   mt: number
+  setToggleAdminChange: React.Dispatch<React.SetStateAction<boolean>>
+  setToggleAdminLeave: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const GroupCard: React.FC<IGroupCard> = props => {
@@ -223,7 +225,8 @@ const GroupCard: React.FC<IGroupCard> = props => {
                     bgColor={itemsBgColor}
                     shadow="lg"
                   >
-                    At least 1 calendar connected to MeetWithWallet platform.
+                    At least 1 eternal calendar connected to Meet With Wallet
+                    platform.
                   </Text>
                   <Tooltip.Arrow />
                 </Tooltip.Content>
@@ -242,6 +245,8 @@ const GroupCard: React.FC<IGroupCard> = props => {
               setGroupRoles={setGroupRoles}
               updateRole={updateRole}
               groupSlug={props.slug}
+              toggleAdminChange={() => props.setToggleAdminChange(true)}
+              toggleAdminLeave={() => props.setToggleAdminLeave(true)}
               {...member}
             />
           ))}
