7815d37fa85ec09d46808dc6eed16dbe2a249fa4
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-10-22 22:18:27 +0100

    improvements: updated base components with new design

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -17,6 +17,7 @@ import {
 import router, { useRouter } from 'next/router'
 import { useContext, useEffect, useState } from 'react'
 import { BiMenuAltRight, BiWallet } from 'react-icons/bi'
+import { FaWallet } from 'react-icons/fa'
 import { useActiveWallet } from 'thirdweb/react'
 
 import { OnboardingModalContext } from '@/providers/OnboardingModalProvider'
@@ -39,7 +40,8 @@ export const Navbar = () => {
 
   const [backdropFilterValue, setBackdropFilterValue] = useState<string>('0')
   const [activeLink, setActiveLink] = useState('')
-
+  const bgColor = useColorModeValue('transparent', 'neutral.1000')
+  const borderColor = useColorModeValue('neutral.300', 'neutral.800')
   function handleSetActiveLink(id: string) {
     if (id === '/') {
       setActiveLink('/#home')
@@ -81,11 +83,21 @@ export const Navbar = () => {
       as="header"
       display={pathname.split('/')[1] === 'embed' ? 'none' : 'block'}
       position="fixed"
-      width="100%"
-      top="0"
+      width={{ md: '60%', base: isOpen ? '100%' : '90%' }}
+      mx="auto"
+      insetX={0}
+      top={{
+        base: isOpen ? 0 : 5,
+        md: 4,
+      }}
+      rounded={15}
       zIndex={999}
+      alignSelf={'center'}
       backdropFilter={`blur(${backdropFilterValue}px)`}
-      bg={'transparent'}
+      bg={bgColor}
+      borderWidth={1}
+      borderColor={borderColor}
+      justifyContent={'space-between'}
     >
       <Flex
         color={useColorModeValue('black', 'white')}
@@ -105,14 +117,14 @@ export const Navbar = () => {
                 cursor="pointer"
               >
                 <Image
-                  width="100px"
+                  width={{ md: '100px', base: '75px' }}
                   p={2}
                   src="/assets/logo.svg"
                   alt="Meet with Wallet"
                 />
               </Flex>
             </Flex>
-            <Flex flex={{ base: 1 }}>
+            <Flex flex={{ base: 1 }} justifyContent={'space-between'}>
               <Link
                 href={'/'}
                 display={{ base: 'none', lg: 'flex' }}
@@ -129,20 +141,25 @@ export const Navbar = () => {
                   />
                 </HStack>
               </Link>
-              <Flex display={{ base: 'none', lg: 'flex' }} ml={10}>
+              <Flex
+                display={{ base: 'none', lg: 'flex' }}
+                ml={10}
+                flexBasis={'75%'}
+              >
                 <DesktopNav
                   pathname={pathname}
                   handleSetActiveLink={handleSetActiveLink}
+                  activeLink={activeLink}
                 />
               </Flex>
             </Flex>
-
             <Stack
               flex={{ base: 1, lg: 0 }}
               justify={'flex-end'}
               direction={'row'}
               spacing={4}
             >
+              {!shouldEnforceColorOnPath(pathname) && <ThemeSwitcher />}
               {logged ? (
                 <NavBarLoggedProfile
                   account={currentAccount!}
@@ -153,8 +170,9 @@ export const Navbar = () => {
                   size="md"
                   onClick={handleConnectionOpen}
                   isLoading={loginIn}
-                  colorScheme="primary"
-                  leftIcon={<BiWallet />}
+                  colorScheme="orangeButton"
+                  color={'white'}
+                  leftIcon={<FaWallet color="white" />}
                 >
                   Sign in
                   <Box display={{ base: 'none', md: 'flex' }} as="span">
@@ -168,10 +186,15 @@ export const Navbar = () => {
                 height={10}
                 display={{ base: 'flex', lg: 'none' }}
                 zIndex="0"
+                bg="neutral.100"
               >
-                <Icon as={BiMenuAltRight} width={6} height={6} />
+                <Icon
+                  as={BiMenuAltRight}
+                  width={6}
+                  height={6}
+                  color="neutral.800"
+                />
               </Button>
-              {!shouldEnforceColorOnPath(pathname) && <ThemeSwitcher />}
             </Stack>
           </Flex>
         </Container>
@@ -184,6 +207,7 @@ export const Navbar = () => {
           handleSetActiveLink={handleSetActiveLink}
           isOpen={isOpen}
           pathname={pathname}
+          activeLink={activeLink}
         />
       </Collapse>
       <ConnectWalletDialog isOpen={loginIn} />
@@ -193,12 +217,17 @@ export const Navbar = () => {
 
 interface DesktopNavProps {
   pathname: string
+  activeLink: string
   handleSetActiveLink: (id: string) => void
 }
 
-const DesktopNav = ({ pathname, handleSetActiveLink }: DesktopNavProps) => {
+const DesktopNav = ({
+  pathname,
+  handleSetActiveLink,
+  activeLink,
+}: DesktopNavProps) => {
   const { logged } = useContext(AccountContext)
-  const linkHoverColor = 'primary.400'
+  const linkHoverColor = 'primary.500'
   const linkColor = useColorModeValue('neutral.800', 'neutral.0')
 
   return (
@@ -213,12 +242,17 @@ const DesktopNav = ({ pathname, handleSetActiveLink }: DesktopNavProps) => {
           <Box key={navItem.label}>
             <Link
               href={navItem.href ?? '#'}
-              onClick={() => handleSetActiveLink(navItem.href)}
               p={2}
-              fontSize={'sm'}
-              fontWeight={500}
+              fontWeight={activeLink === navItem.href ? 700 : 500}
+              onClick={() => handleSetActiveLink(navItem.href)}
               color={
-                shouldEnforceColorOnPath(pathname) ? 'neutral.0' : linkColor
+                shouldEnforceColorOnPath(pathname)
+                  ? activeLink === navItem.href
+                    ? linkHoverColor
+                    : 'neutral.0'
+                  : pathname.includes(navItem.href)
+                  ? linkHoverColor
+                  : linkColor
               }
               _hover={{
                 textDecoration: 'none',
@@ -240,6 +274,7 @@ interface MobileNavProps {
   handleSetActiveLink: (id: string) => void
   isOpen: boolean
   pathname: string
+  activeLink: string
 }
 
 const MobileNav = ({
@@ -248,13 +283,14 @@ const MobileNav = ({
   handleSetActiveLink,
   isOpen,
   pathname,
+  activeLink,
 }: MobileNavProps) => {
   const { currentAccount, logged, loginIn } = useLogin()
   const { logout } = useContext(AccountContext)
   const wallet = useActiveWallet()
 
   const doLogout = async () => {
-    await logout(wallet!)
+    logout(wallet!)
     await router.push('/')
     onToggle()
   }
@@ -266,14 +302,16 @@ const MobileNav = ({
       pos="fixed"
       top="0"
       width="100%"
+      left={0}
+      right={0}
       height="100vh"
       bg={useColorModeValue('white', 'gray.800')}
-      p={4}
+      p={6}
       borderBottom={1}
       borderStyle={'solid'}
       borderColor={useColorModeValue('gray.200', 'gray.900')}
     >
-      <Flex alignItems="center" justify="space-between" mb={8}>
+      <Flex alignItems="center" justify="space-between" mb={2} width="100%">
         <Flex alignItems="center" cursor="pointer">
           <Image
             width="100px"
@@ -283,16 +321,34 @@ const MobileNav = ({
           />
         </Flex>
 
-        <Button onClick={onToggle} width={10} height={10} bg="transparent">
-          <Icon as={CloseIcon} width={3} height={3} />
+        <Button
+          width={10}
+          height={10}
+          bg="transparent"
+          role="group"
+          _hover={{ bgColor: 'transparent' }}
+        >
+          <Icon
+            onClick={onToggle}
+            as={CloseIcon}
+            width={6}
+            height={6}
+            color="neutral.100"
+            _groupHover={{
+              color: 'neutral.100',
+            }}
+          />
         </Button>
       </Flex>
-      <Stack>
+      <Stack width="100%" spacing={4}>
         {NAV_ITEMS.filter(item => !item.logged || (logged && item.logged)).map(
           navItem => (
             <MobileNavItem
               onToggle={onToggle}
               key={navItem.label}
+              handleSetActiveLink={handleSetActiveLink}
+              pathname={pathname}
+              activeLink={activeLink}
               {...navItem}
             />
           )
@@ -307,17 +363,18 @@ const MobileNav = ({
               handleSetActiveLink={handleSetActiveLink}
             />
             <Button onClick={doLogout} variant="link">
-              Sign out
+              Logout
             </Button>
             {!shouldEnforceColorOnPath(pathname) && <ThemeSwitcher />}
           </Flex>
         ) : (
           <Button
-            colorScheme="primary"
+            colorScheme="orangeButton"
             size="md"
             onClick={() => onOpenModal()}
             isLoading={loginIn}
-            leftIcon={<BiWallet />}
+            color={'white'}
+            leftIcon={<FaWallet color="white" />}
           >
             Sign in
             <Box display={{ base: 'none', md: 'flex' }} as="span">
@@ -330,24 +387,45 @@ const MobileNav = ({
   )
 }
 
-const MobileNavItem = ({ label, href, onToggle }: NavItem) => {
+const MobileNavItem = ({
+  label,
+  href,
+  onToggle,
+  handleSetActiveLink,
+  pathname,
+  activeLink,
+}: NavItem & {
+  pathname: string
+  handleSetActiveLink: (id: string) => void
+  activeLink: string
+}) => {
+  const linkHoverColor = 'primary.500'
+
   return (
     <Stack spacing={4}>
       <Flex
-        onClick={onToggle}
         bg="neutral.100"
-        py={2}
+        py={3}
         as={Link}
         href={href ?? '#'}
         justify={'center'}
+        rounded={8}
         align={'center'}
         _hover={{
           textDecoration: 'none',
         }}
+        onClick={() => {
+          handleSetActiveLink(href)
+          onToggle?.()
+        }}
+        color={
+          (shouldEnforceColorOnPath(pathname) && activeLink === href) ||
+          (!shouldEnforceColorOnPath(pathname) && pathname.includes(href))
+            ? linkHoverColor
+            : 'neutral.800'
+        }
       >
-        <Text fontWeight={600} color="neutral.800">
-          {label}
-        </Text>
+        <Text fontWeight={700}>{label}</Text>
       </Flex>
     </Stack>
   )
