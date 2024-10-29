aa69162c7046276d2e909e55707f8633420b9c90
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-10-29 21:12:36 +0100

    feat: added helper for sending message to telegram

---
+++ src/utils/services/telegram.helper.ts
@@ -0,0 +1,15 @@
+const apiUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`
+const sendMessage = async (chat_id: string, text: string) => {
+  const response = await fetch(`${apiUrl}/sendMessage`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      chat_id,
+      text,
+    }),
+  })
+
+  return response.json()
+}
