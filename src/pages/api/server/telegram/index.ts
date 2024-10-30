966fd9acf3f5ee5c3ebfb65070141b38e187730d
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-10-30 22:46:28 +0100

    improvements: updated the logic for telegram based notifications

--- src/pages/api/server/telegram/index.ts
+++ src/pages/api/server/telegram/index.ts
@@ -3,7 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { NotificationChannel } from '@/types/AccountNotifications'
 import { SetTelegramNotificationOptionRequest } from '@/types/Requests'
 import {
+  deleteAllTgConnections,
   getAccountNotificationSubscriptions,
+  getTgConnectionByTgId,
   setAccountNotificationSubscriptions,
 } from '@/utils/database'
 
@@ -12,20 +14,40 @@ export default async function discordInfo(
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
-    const { account_address, chat_id } =
-      req.body as SetTelegramNotificationOptionRequest
+    const { chat_id, tg_id } = req.body as SetTelegramNotificationOptionRequest
+    const tgConnection = await getTgConnectionByTgId(tg_id)
+    if (!tgConnection) {
+      return res
+        .status(404)
+        .send({ success: false, message: 'Telegram connection not found' })
+    }
     const subscriptions = await getAccountNotificationSubscriptions(
-      account_address
+      tgConnection.account_address
     )
+    if (
+      subscriptions.notification_types.find(
+        n => n.channel === NotificationChannel.TELEGRAM
+      )
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: 'Telegram notification already added',
+      })
+    }
     subscriptions.notification_types.push({
       channel: NotificationChannel.TELEGRAM,
       destination: chat_id,
       disabled: false,
     })
-    await setAccountNotificationSubscriptions(account_address, subscriptions)
+    await setAccountNotificationSubscriptions(
+      tgConnection.account_address,
+      subscriptions
+    )
+    await deleteAllTgConnections(tgConnection.account_address)
     return res.status(200).json({
       success: true,
       message: 'Telegram notification added successfully',
+      account_address: tgConnection.account_address,
     })
   }
 
