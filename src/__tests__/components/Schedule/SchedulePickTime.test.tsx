966e9fc4cbe78f0b8bef12db6c72928f9efa7621
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-11-06 00:23:03 +0100

    feat: created tests for scheduling components

---
+++ src/__tests__/components/Schedule/SchedulePickTime.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { SchedulePickTime } from '@/components/schedule/schedule-time-discover/SchedulePickTime'
+
+describe('<SchedulePickTime />', () => {
+  ;<SchedulePickTime />
+})
