a41cf617648cc465e01d0797ee85c05a489bc9b6
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-11-06 00:23:03 +0100

    feat: created tests for scheduling components

---
+++ src/__tests__/components/Schedule/ScheduleParticipants.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { ScheduleParticipants } from '@/components/schedule/schedule-time-discover/ScheduleParticipants'
+
+describe('<ScheduleParticipants />', () => {
+  ;<ScheduleParticipants prop="default value" />
+})
