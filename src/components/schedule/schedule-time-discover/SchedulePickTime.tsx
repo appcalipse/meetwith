3c8be0d275cef17df42c51c74bf89e078a5203fc
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-11-07 00:11:24 +0100

    feat: added logic for scheduling

--- src/components/schedule/schedule-time-discover/SchedulePickTime.tsx
+++ src/components/schedule/schedule-time-discover/SchedulePickTime.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import {
   Box,
+  Flex,
+  Grid,
   Heading,
   HStack,
   Icon,
   IconButton,
+  SlideFade,
   Text,
   VStack,
 } from '@chakra-ui/react'
@@ -16,23 +19,47 @@ import {
 } from 'chakra-react-select'
 import * as ct from 'countries-and-timezones'
 import {
+  add,
   addDays,
   endOfMonth,
+  format,
+  isBefore,
   isSameDay,
   isSameMonth,
   startOfMonth,
+  sub,
 } from 'date-fns'
 import React, { useContext, useEffect, useMemo, useState } from 'react'
 import { FaChevronDown, FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import { SelectComponentsGeneric } from 'react-select/dist/declarations/src/components'
-import { ActionMeta } from 'react-select/dist/declarations/src/types'
 
+import Loading from '@/components/Loading'
 import InfoTooltip from '@/components/profile/components/Tooltip'
-import { IGroupParticipant, ScheduleContext } from '@/pages/dashboard/schedule'
-import { ParticipantInfo } from '@/types/ParticipantInfo'
+import { Page, ScheduleContext } from '@/pages/dashboard/schedule'
+import { DayAvailability, TimeRange } from '@/types/Account'
+import { TimeSlot } from '@/types/Meeting'
 import { fetchBusySlotsRawForMultipleAccounts } from '@/utils/api_helper'
 import { handleApiError } from '@/utils/error_helper'
 
+import { ScheduleTimeSlot } from './ScheduleTimeSlot'
+const GUIDES = [
+  {
+    color: 'green.400',
+    description: 'Everyone is available',
+  },
+  {
+    color: 'green.200',
+    description: 'Some are available',
+  },
+  {
+    color: 'green.300',
+    description: 'Most are available',
+  },
+  {
+    color: 'neutral.0',
+    description: 'No one is available',
+  },
+]
 const timezonesObj = ct.getAllTimezones()
 const timezonesKeys = Object.keys(timezonesObj) as Array<
   keyof typeof timezonesObj
@@ -57,38 +84,43 @@ const CLOCK = [
     value: 'PM',
   },
 ]
-const getEmptySlots = (date: Date) => {
-  const slots = []
-  for (let i = 0; i < 12; i++) {
-    const start = new Date(date)
-    start.setHours(i)
-    start.setMinutes(0)
-    start.setSeconds(0)
-    const end = new Date(date)
-    end.setHours(i + 1)
-    end.setMinutes(0)
-    end.setSeconds(0)
-    slots.push({
-      start,
-      end,
-    })
-  }
-  return slots
+
+type Dates = {
+  date: Date
+  slots: Array<{
+    start: Date
+    end: Date
+  }>
+  busySlots: Array<Array<TimeSlot>>
+  availabilities: Array<Record<string, Array<TimeRange>>>
 }
-export function SchedulePickTime() {
+interface SchedulePickTimeProps {
+  accountAvailabilities: Record<string, Array<DayAvailability>>
+}
+export function SchedulePickTime({
+  accountAvailabilities,
+}: SchedulePickTimeProps) {
   const {
     groupAvailability,
     currentMonth,
     setCurrentMonth,
     timezone,
     setTimezone,
-    participants,
     currentSelectedDate,
+    handleTimePick,
     setCurrentSelectedDate,
+    handlePageSwitch,
+    pickedTime,
   } = useContext(ScheduleContext)
 
-  const [accountSlots, setAccountSlots] = useState<Array<Interval[]>>([])
+  const [accountSlots, setAccountSlots] = useState<Array<TimeSlot[]>>([])
   const [isLoading, setIsLoading] = useState(false)
+  const [clockValue, setClockValue] = useState<
+    SingleValue<{ label: string; value: string }>
+  >({
+    label: format(pickedTime || new Date(), 'a').toUpperCase(),
+    value: format(pickedTime || new Date(), 'a').toUpperCase(),
+  })
 
   const getMonthsForYear = () => {
     const year = currentMonth.getFullYear()
@@ -100,49 +132,49 @@ export function SchedulePickTime() {
       const date = new Date(year, month, 1)
       months.push({
         value: String(month),
-        label: new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date),
+        label: `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(
+          date
+        )} ${year}`,
       })
     }
     return months
   }
-
-  const months = useMemo(() => getMonthsForYear(), [currentMonth])
-  const getDates = () => {
-    const days = Array.from({ length: 7 }, (v, k) => k)
-      .map(k => addDays(currentSelectedDate, k))
-      .filter(val => isSameMonth(val, currentMonth))
-    return days.map((date, index) => {
-      const slots = getEmptySlots(date)
-      const busySlots = accountSlots.map(val => {
-        return val.filter(slot => {
-          return (
-            isSameDay(slot.start, date) &&
-            new Date(slot.start).getHours() === index
-          )
-        })
+  const getEmptySlots = (date: Date) => {
+    const slots = []
+    const startRange = clockValue?.value === 'AM' ? 0 : 12
+    const endRange = clockValue?.value === 'AM' ? 12 : 24
+    for (let i = startRange; i < endRange; i++) {
+      const start = new Date(date)
+      start.setHours(i)
+      start.setMinutes(0)
+      start.setSeconds(0)
+      const end = new Date(date)
+      end.setHours(i + 1)
+      end.setMinutes(0)
+      end.setSeconds(0)
+      slots.push({
+        start,
+        end,
       })
-      return {
-        date,
-        slots,
-        busySlots,
-      }
-    })
+    }
+    return slots
   }
-  const [dates, setDates] = useState(getDates())
-  console.log({ dates })
+  const months = useMemo(() => getMonthsForYear(), [currentMonth])
+  const [dates, setDates] = useState<Array<Dates>>([])
   const [monthValue, setMonthValue] = useState<
     SingleValue<{ label: string; value: string }>
   >({
-    label: new Intl.DateTimeFormat('en-US', { month: 'long' }).format(
-      new Date()
-    ),
-    value: String(new Date().getMonth()),
+    label: `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(
+      currentMonth
+    )} ${currentMonth.getFullYear()}`,
+    value: String(currentMonth.getMonth()),
   })
+
   const _onChangeMonth = (
     newValue:
       | SingleValue<{ label: string; value: string }>
       | MultiValue<{ label: string; value: string }>,
-    actionMeta: ActionMeta<any>
+    newMonth?: Date
   ) => {
     if (Array.isArray(newValue)) {
       return
@@ -150,7 +182,23 @@ export function SchedulePickTime() {
     const month = newValue as SingleValue<{ label: string; value: string }>
     setMonthValue(month)
     if (!month?.value) return
+    const year = newMonth ? newMonth?.getFullYear() : currentMonth.getFullYear()
     setCurrentMonth(new Date(year, Number(month.value), 1))
+    if (!newMonth) {
+      setCurrentSelectedDate(new Date(year, Number(month.value), 1))
+    }
+  }
+
+  const _onChangeClock = (
+    newValue:
+      | SingleValue<{ label: string; value: string }>
+      | MultiValue<{ label: string; value: string }>
+  ) => {
+    if (Array.isArray(newValue)) {
+      return
+    }
+    const clock = newValue as SingleValue<{ label: string; value: string }>
+    setClockValue(clock)
   }
 
   const tzs = timezones.map(tz => {
@@ -167,8 +215,7 @@ export function SchedulePickTime() {
   const _onChange = (
     newValue:
       | SingleValue<{ label: string; value: string }>
-      | MultiValue<{ label: string; value: string }>,
-    actionMeta: ActionMeta<any>
+      | MultiValue<{ label: string; value: string }>
   ) => {
     if (Array.isArray(newValue)) {
       return
@@ -191,31 +238,47 @@ export function SchedulePickTime() {
       </chakraComponents.DropdownIndicator>
     ),
   }
+  const getDates = (accountSlots: Array<TimeSlot[]>) => {
+    const days = Array.from({ length: 7 }, (v, k) => k)
+      .map(k => addDays(currentSelectedDate, k))
+      .filter(val => isSameMonth(val, currentMonth))
+    return days.map(date => {
+      const slots = getEmptySlots(date)
+      const busySlots = accountSlots.map(val => {
+        return val.filter(slot => {
+          return isSameDay(slot.start, date)
+        })
+      })
+      const availabilities: Array<Record<string, Array<TimeRange>>> = []
+      const accounts = Object.values(groupAvailability).flat()
+      for (const [key, entry] of Object.entries(accountAvailabilities)) {
+        for (const availability of entry) {
+          const day = new Date(date)
+          if (day.getDay() !== availability.weekday) {
+            continue
+          }
+          if (accounts.includes(key)) {
+            availabilities.push({
+              [key]: availability.ranges,
+            })
+          }
+        }
+      }
+
+      return {
+        date,
+        slots,
+        busySlots,
+        availabilities,
+      }
+    })
+  }
   async function handleSlotLoad() {
     setIsLoading(true)
     try {
       const monthStart = startOfMonth(currentMonth)
       const monthEnd = endOfMonth(currentMonth)
-      const currentParticipant = participants
-        .filter(val => {
-          const groupData = val as IGroupParticipant
-          const isGroup = groupData.isGroup
-          return !isGroup
-        })
-        .filter(val => {
-          const groupData = val as ParticipantInfo
-          return !!groupData.account_address
-        })
-        .map(val => {
-          const groupData = val as ParticipantInfo
-          return groupData.account_address as string
-        })
-      const accounts = [
-        ...new Set([
-          ...Object.values(groupAvailability).flat(),
-          ...currentParticipant,
-        ]),
-      ]
+      const accounts = [...new Set(Object.values(groupAvailability).flat())]
       const availableSlots = await fetchBusySlotsRawForMultipleAccounts(
         accounts,
         monthStart,
@@ -225,8 +288,9 @@ export function SchedulePickTime() {
         return availableSlots.filter(slot => slot.account_address === account)
       })
       setAccountSlots(accountSlots)
-    } catch (error: any) {
-      handleApiError('Error merging availabilities', error)
+      setDates(getDates(accountSlots))
+    } catch (error: unknown) {
+      handleApiError('Error merging availabilities', error as Error)
     }
     setIsLoading(false)
   }
@@ -234,12 +298,73 @@ export function SchedulePickTime() {
   useEffect(() => {
     handleSlotLoad()
   }, [groupAvailability, currentMonth])
-  const handleScheduledTimeBack = () => {}
-  const handleScheduledTimeNext = () => {}
+  useEffect(() => {
+    setDates(getDates(accountSlots))
+  }, [currentSelectedDate, clockValue])
+  const handleScheduledTimeBack = () => {
+    const currentDay = currentSelectedDate.getDate()
+    if (currentDay === 1) {
+      const newMonth = startOfMonth(sub(currentSelectedDate, { months: 1 }))
+
+      setCurrentSelectedDate(addDays(endOfMonth(newMonth), -6))
+      _onChangeMonth(
+        {
+          label: `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(
+            newMonth
+          )} ${newMonth.getFullYear()}`,
+          value: String(newMonth.getMonth()),
+        },
+        newMonth
+      )
+    } else if (currentDay - 7 < 1) {
+      setCurrentSelectedDate(startOfMonth(currentSelectedDate))
+    } else {
+      setCurrentSelectedDate(addDays(currentSelectedDate, -7))
+    }
+  }
+  const handleScheduledTimeNext = () => {
+    const currentDay = currentSelectedDate.getDate()
+    const lastDateOfMonth = endOfMonth(currentSelectedDate).getDate()
+    if (currentDay === lastDateOfMonth - 6) {
+      const newMonth = startOfMonth(add(currentSelectedDate, { months: 1 }))
+      setCurrentSelectedDate(newMonth)
+      _onChangeMonth(
+        {
+          label: `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(
+            newMonth
+          )} ${newMonth.getFullYear()}`,
+          value: String(newMonth.getMonth()),
+        },
+        newMonth
+      )
+    } else if (currentDay + 7 > lastDateOfMonth - 7) {
+      setCurrentSelectedDate(addDays(endOfMonth(currentSelectedDate), -6))
+    } else {
+      setCurrentSelectedDate(addDays(currentSelectedDate, 7))
+    }
+  }
+  const SLOTS = useMemo(
+    () => getEmptySlots(new Date()).map(val => format(val.start, 'HH:mm a')),
+    [clockValue]
+  )
+
   return (
-    <VStack gap={10} w="100%">
-      <HStack w="100%">
-        <VStack gap={2} alignItems={'flex-start'}>
+    <VStack gap={4} w="100%" p={{ base: 4, md: 0 }}>
+      <Flex
+        w="100%"
+        alignItems={{ md: 'flex-end' }}
+        flexDir={{
+          base: 'column',
+          md: 'row',
+        }}
+        gap={4}
+      >
+        <VStack
+          gap={2}
+          alignItems={'flex-start'}
+          width="fit-content"
+          minW={'300px'}
+        >
           <HStack width="fit-content" gap={0}>
             <Heading fontSize="16px">Show times in</Heading>
             <InfoTooltip text="the default timezone is based on your availability settings" />
@@ -253,19 +378,39 @@ export function SchedulePickTime() {
             components={customComponents}
           />
         </VStack>
-        <VStack gap={2} alignItems={'flex-start'}>
+        <VStack
+          gap={2}
+          alignItems={'flex-start'}
+          width="fit-content"
+          minW={'300px'}
+        >
           <Heading fontSize="16px">Month</Heading>
 
           <Select
             value={monthValue}
             colorScheme="primary"
-            onChange={_onChangeMonth}
+            onChange={newValue => _onChangeMonth(newValue)}
             className="noLeftBorder timezone-select"
             options={months}
             components={customComponents}
           />
         </VStack>
-      </HStack>
+        <Grid
+          gridTemplateColumns={'1fr 1fr'}
+          justifyContent={'space-between'}
+          w="100%"
+          gap={2}
+        >
+          {GUIDES.map((guide, index) => {
+            return (
+              <HStack key={index} gap={2}>
+                <Box w={5} h={5} bg={guide.color} borderRadius={4} />
+                <Text>{guide.description}</Text>
+              </HStack>
+            )
+          })}
+        </Grid>
+      </Flex>
 
       <VStack
         gap={6}
@@ -281,6 +426,7 @@ export function SchedulePickTime() {
             aria-label={'left-icon'}
             icon={<FaChevronLeft />}
             onClick={handleScheduledTimeBack}
+            isDisabled={isBefore(currentSelectedDate, new Date()) || isLoading}
           />
           <Box maxW="350px" textAlign="center">
             <Heading fontSize="16px">Available times</Heading>
@@ -295,18 +441,74 @@ export function SchedulePickTime() {
             onClick={handleScheduledTimeNext}
           />
         </HStack>
-        <HStack>
-          <VStack>
-            <Select
-              value={monthValue}
-              colorScheme="primary"
-              onChange={_onChangeMonth}
-              className="noLeftBorder timezone-select"
-              options={CLOCK}
-              components={customComponents}
-            />
-          </VStack>
-        </HStack>
+        {isLoading ? (
+          <HStack>
+            <Loading />
+          </HStack>
+        ) : (
+          <HStack w="100%" justify={{ md: 'space-between' }} gap={0}>
+            <VStack align={'flex-start'} flex={1} justify={'flex-start'}>
+              <Select
+                value={clockValue}
+                colorScheme="primary"
+                onChange={_onChangeClock}
+                className="noLeftBorder date-select"
+                options={CLOCK}
+                components={customComponents}
+              />
+              <VStack align={'flex-start'} p={1}>
+                {SLOTS.map((slot, index) => {
+                  return (
+                    <HStack
+                      key={index}
+                      w="100%"
+                      justify={'center'}
+                      align={'center'}
+                      h={12}
+                    >
+                      <Text fontWeight={'500'}>{slot}</Text>
+                    </HStack>
+                  )
+                })}
+              </VStack>
+            </VStack>
+            {dates.map((date, index) => {
+              return (
+                <SlideFade
+                  in={true}
+                  key={index + date.date.toDateString()}
+                  transition={{ exit: { delay: 0 }, enter: { duration: 1 } }}
+                  style={{ flex: 1 }}
+                >
+                  <VStack flex={1} align={'flex-start'} gap={0}>
+                    <VStack align={'center'} w="100%" h={12} gap={0}>
+                      <Text fontWeight={'700'}>{format(date.date, 'dd')}</Text>
+                      <Text fontWeight={'500'}>{format(date.date, 'EE')}</Text>
+                    </VStack>
+                    <VStack width="100%" align={'flex-start'} p={1}>
+                      {date.slots.map(slot => {
+                        return (
+                          <ScheduleTimeSlot
+                            key={format(slot.start, 'DDDD,MMMM,yyyy, hh:mm,a')}
+                            slot={slot}
+                            date={date.date}
+                            busySlots={date.busySlots}
+                            availabilities={date.availabilities}
+                            pickedTime={pickedTime}
+                            handleTimePick={time => {
+                              handleTimePick(time)
+                              handlePageSwitch(Page.SCHEDULE_DETAILS)
+                            }}
+                          />
+                        )
+                      })}
+                    </VStack>
+                  </VStack>
+                </SlideFade>
+              )
+            })}
+          </HStack>
+        )}
       </VStack>
     </VStack>
   )
