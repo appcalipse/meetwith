fffd123b306a61829e93cca95c944a6cbd7e7d54
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-11-07 15:04:01 +0100

    feat: mobile cleanups

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,7 +1,19 @@
+import { TimeRange } from './Account'
+
 export enum ConditionRelation {
   AND = 'and',
   OR = 'or',
 }
 export interface SuccessResponse {
   success: boolean
+}
+
+export interface CustomTimeRange extends TimeRange {
+  timezone: string
+  weekday: number
+}
+
+export interface CustomDayAvailability {
+  weekday: number
+  ranges: CustomTimeRange[]
 }
