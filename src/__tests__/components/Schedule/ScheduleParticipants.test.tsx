51317a093e063680efbe8fe5bcd5357c2ebc13a6
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-11-14 16:07:41 +0100

    feat: removed tests

--- src/__tests__/components/Schedule/ScheduleParticipants.test.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-import { ScheduleParticipants } from '@/components/schedule/schedule-time-discover/ScheduleParticipants'
-
-describe('<ScheduleParticipants />', () => {})
