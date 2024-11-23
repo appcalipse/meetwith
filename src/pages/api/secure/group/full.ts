41b6e0e72fb3dc908593e0093447bd4b6f57dffc
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2024-11-23 18:11:45 +0100

    Revert "Revert "Feat/feature flag""

---
+++ src/pages/api/secure/group/full.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { withSessionRoute } from '@/ironAuth/withSessionApiRoute'
+import { GetGroupsResponse } from '@/types/Group'
+import {
+  getAccountNotificationSubscriptionEmail,
+  getGroupsAndMembers,
+  getUserGroups,
+  initDB,
+} from '@/utils/database'
+
+const handle = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    initDB()
+    const account_address = req.session.account!.address
+    if (!account_address) {
+      return res.status(401).send('Unauthorized')
+    }
+    try {
+      const groups = await getGroupsAndMembers(
+        account_address,
+        Number(req.query.limit as string),
+        Number(req.query.offset as string)
+      )
+      return res.status(200).json(groups)
+    } catch (e) {
+      return res.status(500).send(e)
+    }
+  }
+  return res.status(405).send('Method not allowed')
+}
+export default withSessionRoute(handle)
