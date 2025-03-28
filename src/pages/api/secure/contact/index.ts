349c9cd768450d24363ea078806841752b387687
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-03-28 23:05:47 +0100

    Add API route to retrieve user contacts with pagination support

---
+++ src/pages/api/secure/contact/index.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { withSessionRoute } from '@/ironAuth/withSessionApiRoute'
+import { NotificationChannel } from '@/types/AccountNotifications'
+import { Contacts } from '@/types/Contacts'
+import { getContacts, initDB } from '@/utils/database'
+
+const handle = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    initDB()
+    const account_address = req.session.account!.address
+    if (!account_address) {
+      return res.status(401).send('Unauthorized')
+    }
+    try {
+      const dbResults = await getContacts(
+        account_address,
+        req.query.limit ? Number(req.query.limit as string) : undefined,
+        req.query.offset ? Number(req.query.offset as string) : undefined
+      )
+      const results: Array<Contacts> = dbResults.map(result => ({
+        id: result.id,
+        contact_address: result.contact_address,
+        status: result.status,
+        ...result.account.preferences,
+        calendar_exists: result.account.calendars_exist.length > 0,
+        email_address:
+          result.account.account_notifications?.notification_types?.find(
+            n => n.channel === NotificationChannel.EMAIL && !n.disabled
+          )?.destination,
+      }))
+      return res.status(200).json(results)
+    } catch (e) {
+      return res.status(500).send(e)
+    }
+  }
+  return res.status(405).send('Method not allowed')
+}
+export default withSessionRoute(handle)
