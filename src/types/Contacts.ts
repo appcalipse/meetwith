b125acb7b5077ad038830283f90fd3a49ec5159a
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-03-28 23:06:27 +0100

    Enhance contact types with additional properties and interfaces for better data management

--- src/types/Contacts.ts
+++ src/types/Contacts.ts
@@ -1,15 +1,82 @@
-interface LeanAccount {
-  name: string
-  address: string
-  avatar_url?: string
+import { NotificationType } from './AccountNotifications'
+
+export interface LeanAccount {
+  address?: string
   email?: string
+}
+
+export interface SearchAccount extends LeanAccount {
+  is_invited: boolean
+  name?: string
+  avatar_url?: string
 }
 export interface ContactSearch {
   total_count: number
-  result?: Array<LeanAccount> | null
+  result?: Array<SearchAccount> | null
 }
 
 export interface InviteContact {
   address?: string
   email?: string
+}
+
+export interface DBContact {
+  id: string
+  contact_address: string
+  account_owner_address: string
+  status: 'active'
+  account: {
+    preferences: {
+      name: string
+      avatar_url: string
+      description: string
+    }
+    calendars_exist: Array<{
+      id: number
+    }>
+    account_notifications: {
+      notification_types: Array<NotificationType>
+    }
+  }
+}
+
+export interface DBContactInvite {
+  id: string
+  destination: string
+  account_owner_address: string
+  channel: string
+  account: {
+    preferences: {
+      name: string
+      avatar_url: string
+      description: string
+    }
+    calendars_exist: Array<{
+      id: number
+    }>
+    account_notifications: {
+      notification_types: Array<NotificationType>
+    }
+  }
+}
+
+export interface Contacts {
+  id: string
+  contact_address: string
+  status: string
+  name: string
+  avatar_url: string
+  description: string
+  calendar_exists: boolean
+  email_address?: string
+}
+
+export interface ContactInvite {
+  id: string
+  account_owner_address: string
+  name: string
+  avatar_url: string
+  description: string
+  calendar_exists: boolean
+  email_address?: string
 }
