e335a3d55dfe17896d5194def9cb0b3ae78f480e
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-04-02 21:31:15 +0100

    Refactor Contact component to use ContactCountStateContext and integrate ContactRejectInviteModal

--- src/components/profile/Contact.tsx
+++ src/components/profile/Contact.tsx
@@ -30,24 +30,31 @@ import { FaPlus } from 'react-icons/fa'
 import { RiSearch2Line } from 'react-icons/ri'
 
 import { useDebounceValue } from '@/hooks/useDebounceValue'
-import { ContactStateContext } from '@/providers/ContactInvitesProvider'
+import { ContactCountStateContext } from '@/providers/ContactInvitesCountProvider'
+import ContactStateProvider from '@/providers/ContactInvitesProvider'
 import { Account } from '@/types/Account'
 import { type Contact, ContactInvite } from '@/types/Contacts'
 import { getContactInviteRequests, getContacts } from '@/utils/api_helper'
 
 import ContactListItem from '../contact/ContactListItem'
+import ContactRejectInviteModal from '../contact/ContactRejectInviteModal'
 import ContactRequestItem from '../contact/ContactRequestItem'
 import ContactSearchModal from '../contact/ContactSearchModal'
 
 const Contact: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const {
+    isOpen: isRejectOpenModal,
+    onOpen: openRejectModal,
+    onClose: closeRejectModal,
+  } = useDisclosure()
   const [debouncedValue, setValue] = useDebounceValue('', 500)
   const [contacts, setContacts] = useState<Array<Contact>>([])
   const [loading, setIsLoading] = useState(true)
   const [noMoreFetch, setNoMoreFetch] = useState(false)
   const [noMoreFetchRequests, setNoMoreFetchRequests] = useState(false)
   const [loadingRequests, setIsLoadingRequests] = useState(true)
-  const { requestCount } = useContext(ContactStateContext)
+  const { requestCount } = useContext(ContactCountStateContext)
   const [firstFetch, setFirstFetch] = useState(true)
   const [requests, setRequests] = useState<Array<ContactInvite>>([])
   const fetchContacts = async (reset?: boolean) => {
@@ -203,6 +210,7 @@ const Contact: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
               setRequests(requests.filter(r => r.address !== account.address))
             }
             index={index}
+            openRejectModal={openRejectModal}
           />
         ))}
         <Tr color="white">
@@ -228,133 +236,142 @@ const Contact: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
     )
   }
   return (
-    <Flex direction={'column'} maxWidth="100%">
-      <ContactSearchModal isOpen={isOpen} onClose={onClose} />
-      <HStack
-        justifyContent="space-between"
-        alignItems="flex-start"
-        mb={4}
-        gap={6}
-      >
-        <Heading fontSize="2xl">
-          My Contact
-          <Text fontSize="sm" fontWeight={500} mt={1} lineHeight={1.5}>
-            A contact is an entity or person you regularly would want to
-            schedule with so you add them to your list
-          </Text>
-        </Heading>
-      </HStack>
-
-      <Tabs variant="unstyled" bg="neutral.900">
-        <HStack justifyContent="space-between" mb={4} p={5}>
-          <Box w="fit-content" pos="relative" h="fit-content">
-            <FormLabel
-              display="flex"
-              htmlFor="search"
-              pos="absolute"
-              left={2}
-              insetY={0}
-              h="full"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <RiSearch2Line color="#7B8794" />
-            </FormLabel>
-            <Input
-              pl={8}
-              w="fit-content"
-              placeholder="Search contact"
-              id="search"
-              defaultValue={debouncedValue}
-              onChange={e => setValue(e.target.value)}
-            />
-          </Box>
-
-          <TabList
-            w="auto"
-            bg="neutral.850"
-            p={1}
-            borderWidth={1}
-            borderColor="neutral.400"
-            rounded={1.5}
-          >
-            <Tab
-              rounded={4}
-              fontWeight={700}
-              _selected={{
-                color: 'neutral.900',
-                bg: 'primary.200',
-              }}
-            >
-              Contact list
-            </Tab>
-            <Tab
-              rounded={4}
-              fontWeight={700}
-              _selected={{
-                color: 'neutral.900',
-                bg: 'primary.200',
-              }}
-            >
-              Requests received
-              {requestCount > 0 && (
-                <Badge
-                  colorScheme="primary"
-                  color="neutral.900"
-                  bg="primary.200"
-                  ml={2}
-                  px={1.5}
-                >
-                  {requestCount}
-                </Badge>
-              )}
-            </Tab>
-          </TabList>
-          <Button
-            onClick={onOpen}
-            flexShrink={0}
-            colorScheme="primary"
-            display={{ base: 'none', md: 'flex' }}
-            mt={{ base: 4, md: 0 }}
-            mb={4}
-            leftIcon={<FaPlus />}
-          >
-            Add new contact
-          </Button>
+    <ContactStateProvider>
+      <Flex direction={'column'} maxWidth="100%">
+        <ContactSearchModal isOpen={isOpen} onClose={onClose} />
+        <ContactRejectInviteModal
+          isOpen={isRejectOpenModal}
+          onClose={closeRejectModal}
+          syncReject={address =>
+            setRequests(requests.filter(r => r.address !== address))
+          }
+        />
+        <HStack
+          justifyContent="space-between"
+          alignItems="flex-start"
+          mb={4}
+          gap={6}
+        >
+          <Heading fontSize="2xl">
+            My Contact
+            <Text fontSize="sm" fontWeight={500} mt={1} lineHeight={1.5}>
+              A contact is an entity or person you regularly would want to
+              schedule with so you add them to your list
+            </Text>
+          </Heading>
         </HStack>
-        <TableContainer>
-          <TabPanels>
-            <TabPanel p={0}>
-              <Table variant="unstyled" colorScheme="whiteAlpha">
-                <Thead bg="neutral.900">
-                  <Tr color="white">
-                    <Th>User</Th>
-                    <Th>Description</Th>
-                    <Th>Account ID</Th>
-                    <Th>Schedule</Th>
-                    <Th>Action</Th>
-                  </Tr>
-                </Thead>
-                {contactContent}
-              </Table>
-            </TabPanel>
-            <TabPanel p={0}>
-              <Table variant="unstyled" colorScheme="whiteAlpha">
-                <Thead bg="neutral.900">
-                  <Tr color="white">
-                    <Th>User</Th>
-                    <Th>Description</Th>
-                    <Th>Account ID</Th>
-                    <Th>Action</Th>
-                  </Tr>
-                </Thead>
-                {requestContent}
-              </Table>
-            </TabPanel>
-          </TabPanels>
-        </TableContainer>
-      </Tabs>
-    </Flex>
+
+        <Tabs variant="unstyled" bg="neutral.900">
+          <HStack justifyContent="space-between" mb={4} p={5}>
+            <Box w="fit-content" pos="relative" h="fit-content">
+              <FormLabel
+                display="flex"
+                htmlFor="search"
+                pos="absolute"
+                left={2}
+                insetY={0}
+                h="full"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <RiSearch2Line color="#7B8794" />
+              </FormLabel>
+              <Input
+                pl={8}
+                w="fit-content"
+                placeholder="Search contact"
+                id="search"
+                defaultValue={debouncedValue}
+                onChange={e => setValue(e.target.value)}
+              />
+            </Box>
+
+            <TabList
+              w="auto"
+              bg="neutral.850"
+              p={1}
+              borderWidth={1}
+              borderColor="neutral.400"
+              rounded={1.5}
+            >
+              <Tab
+                rounded={4}
+                fontWeight={700}
+                _selected={{
+                  color: 'neutral.900',
+                  bg: 'primary.200',
+                }}
+              >
+                Contact list
+              </Tab>
+              <Tab
+                rounded={4}
+                fontWeight={700}
+                _selected={{
+                  color: 'neutral.900',
+                  bg: 'primary.200',
+                }}
+              >
+                Requests received
+                {requestCount > 0 && (
+                  <Badge
+                    colorScheme="primary"
+                    color="neutral.900"
+                    bg="primary.200"
+                    ml={2}
+                    px={1.5}
+                  >
+                    {requestCount}
+                  </Badge>
+                )}
+              </Tab>
+            </TabList>
+            <Button
+              onClick={onOpen}
+              flexShrink={0}
+              colorScheme="primary"
+              display={{ base: 'none', md: 'flex' }}
+              mt={{ base: 4, md: 0 }}
+              mb={4}
+              leftIcon={<FaPlus />}
+            >
+              Add new contact
+            </Button>
+          </HStack>
+          <TableContainer>
+            <TabPanels>
+              <TabPanel p={0}>
+                <Table variant="unstyled" colorScheme="whiteAlpha">
+                  <Thead bg="neutral.900">
+                    <Tr color="white">
+                      <Th>User</Th>
+                      <Th>Description</Th>
+                      <Th>Account ID</Th>
+                      <Th>Schedule</Th>
+                      <Th>Action</Th>
+                    </Tr>
+                  </Thead>
+                  {contactContent}
+                </Table>
+              </TabPanel>
+              <TabPanel p={0}>
+                <Table variant="unstyled" colorScheme="whiteAlpha">
+                  <Thead bg="neutral.900">
+                    <Tr color="white">
+                      <Th>User</Th>
+                      <Th>Description</Th>
+                      <Th>Account ID</Th>
+                      <Th>Action</Th>
+                    </Tr>
+                  </Thead>
+                  {requestContent}
+                </Table>
+              </TabPanel>
+            </TabPanels>
+          </TableContainer>
+        </Tabs>
+      </Flex>
+    </ContactStateProvider>
   )
 }
 
