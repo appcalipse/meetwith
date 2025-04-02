c64f03ac1bb8301d8cd46643fd5c635e30bade56
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-04-02 21:29:56 +0100

    Update NavMenu to use ContactCountStateContext for request count

--- src/components/profile/components/NavMenu.tsx
+++ src/components/profile/components/NavMenu.tsx
@@ -26,7 +26,7 @@ import { FaUserGroup } from 'react-icons/fa6'
 import DashboardOnboardingGauge from '@/components/onboarding/DashboardOnboardingGauge'
 import ActionToast from '@/components/toasts/ActionToast'
 import { AccountContext } from '@/providers/AccountProvider'
-import { ContactStateContext } from '@/providers/ContactInvitesProvider'
+import { ContactCountStateContext } from '@/providers/ContactInvitesCountProvider'
 import { OnboardingContext } from '@/providers/OnboardingProvider'
 import { EditMode } from '@/types/Dashboard'
 import { logEvent } from '@/utils/analytics'
@@ -57,7 +57,7 @@ export const NavMenu: React.FC<{
   const router = useRouter()
   const toast = useToast()
   const [noOfInvitedGroups, setNoOfInvitedGroups] = React.useState<number>(0)
-  const { requestCount } = useContext(ContactStateContext)
+  const { requestCount } = useContext(ContactCountStateContext)
 
   const { calendarResult } = router.query
   const menuBg = useColorModeValue('white', 'neutral.900')
