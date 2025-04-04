3ef06707b597aa08c12e7f4a2d3875634d33ad90
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-04-04 19:16:13 +0100

    refactor: improve error handling in error classes for consistency and clarity

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -224,3 +224,23 @@ export class SubscriptionNotCustom extends Error {
     this.name = 'SubscriptionNotCustom'
   }
 }
+
+export class ContactAlreadyExists extends Error {
+  constructor() {
+    super(`Contact already exists`)
+    this.name = 'ContactAlreadyExists'
+  }
+}
+export class ContactNotFound extends Error {
+  constructor() {
+    super(`Contact not found`)
+    this.name = 'ContactNotFound'
+  }
+}
+
+export class ContactInviteNotFound extends Error {
+  constructor() {
+    super(`Contact invite not found`)
+    this.name = 'ContactInviteNotFound'
+  }
+}
