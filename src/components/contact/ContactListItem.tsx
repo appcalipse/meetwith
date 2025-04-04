39ca352faff2aa9461dc91fd045ca282ee8789e7
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-04-04 14:11:31 +0100

    feat: add contact invite functionality and improve invite rejection handling

--- src/components/contact/ContactListItem.tsx
+++ src/components/contact/ContactListItem.tsx
@@ -1,29 +1,78 @@
-import { Button, HStack, Text, Th, Tr } from '@chakra-ui/react'
+import { Button, HStack, Text, Th, Tr, useToast } from '@chakra-ui/react'
+import { useMutation } from '@tanstack/react-query'
 import { Jazzicon } from '@ukstv/jazzicon-react'
 import { useRouter } from 'next/router'
 import React, { FC } from 'react'
 
 import { Contact } from '@/types/Contacts'
-import { removeContact } from '@/utils/api_helper'
+import { removeContact, sendContactListInvite } from '@/utils/api_helper'
 import { ContactStatus } from '@/utils/constants/contact'
 import { ellipsizeAddress } from '@/utils/user_manager'
 
 type Props = {
   account: Contact
   index: number
+  sync: (id: string) => void
+  refetch: () => void
 }
 
-const ContactListItem: FC<Props> = ({ account, index }) => {
+const ContactListItem: FC<Props> = ({ account, index, sync, refetch }) => {
   const [isRemoving, setIsRemoving] = React.useState(false)
   const { push } = useRouter()
+  const toast = useToast()
+  const {
+    isLoading: isInviteLoading,
+    mutateAsync: sendInviteAsync,
+    isSuccess,
+  } = useMutation({
+    mutationFn: (address?: string) => sendContactListInvite(address),
+  })
   const handleRemove = async () => {
     setIsRemoving(true)
     try {
       await removeContact(account.address)
+      sync(account.id)
+      toast({
+        title: 'Success',
+        description: 'Contact removed successfully',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      })
     } catch (e) {
-      console.error(e)
+      const error = e as Error
+      toast({
+        title: 'Error',
+        description: error.message || 'Could not remove contact',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      })
+      refetch()
     } finally {
       setIsRemoving(false)
+    }
+  }
+  const handleSendInvite = async () => {
+    if (!account.address) return
+    try {
+      await sendInviteAsync(account.address)
+      toast({
+        title: 'Success',
+        description: 'Contact invite sent successfully',
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+      })
+    } catch (e) {
+      const error = e as Error
+      toast({
+        title: 'Error',
+        description: error.message || 'Could not send contact invite',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      })
     }
   }
   return (
@@ -58,7 +107,18 @@ const ContactListItem: FC<Props> = ({ account, index }) => {
             Schedule
           </Button>
         ) : (
-          <Button colorScheme="primary" onClick={() => {}}>
+          <Button
+            colorScheme="primary"
+            onClick={handleSendInvite}
+            isLoading={isInviteLoading}
+            isDisabled={isSuccess}
+            _disabled={{
+              bg: isSuccess ? 'neutral.400' : '',
+            }}
+            _hover={{
+              bg: isSuccess ? 'neutral.400' : '',
+            }}
+          >
             Send request
           </Button>
         )}
