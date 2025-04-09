02c29c7c58ac89df3857fe44374f2a8f677c3045
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-04-09 22:38:33 +0100

    fix: handle undefined pickedTime in ScheduleCompleted component

--- src/components/schedule/ScheduleCompleted.tsx
+++ src/components/schedule/ScheduleCompleted.tsx
@@ -46,10 +46,12 @@ const ScheduleCompleted = () => {
         fontFamily="'DM Sans', sans-serif"
       >
         Your meeting <b>{title}</b> on{' '}
-        <b>{formatInTimeZone(pickedTime as Date, timezone, 'MMM d, yyyy')}</b>{' '}
+        <b>
+          {formatInTimeZone(pickedTime || new Date(), timezone, 'MMM d, yyyy')}
+        </b>{' '}
         at{' '}
         <b>
-          {formatInTimeZone(pickedTime as Date, timezone, 'hh:mm a')} (
+          {formatInTimeZone(pickedTime || new Date(), timezone, 'hh:mm a')} (
           {timezone})
         </b>{' '}
         has been {intent === Intents.UPDATE_MEETING ? 'updated' : 'scheduled'}.
