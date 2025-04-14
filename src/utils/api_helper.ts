77c026e050a6ebba725ed1fa3e2a122ef1d98ba0
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-04-14 18:57:35 +0100

    fix: exclude account not found error from Sentry logging in internalFetch

--- src/utils/api_helper.ts
+++ src/utils/api_helper.ts
@@ -97,7 +97,8 @@ export const internalFetch = async <T>(
 
     throw new ApiFetchError(response.status, await response.text())
   } catch (e: any) {
-    if (!path.includes('accounts')) {
+    // Exclude account not found error on sentry
+    if (!path.includes('accounts') && e.status === 404) {
       Sentry.captureException(e)
     }
     throw e
