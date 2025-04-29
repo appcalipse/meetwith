ece5c26b9abe7047b420d5e62299acf50dd763b9
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-04-29 21:34:15 +0100

    refactor: streamline reminder handling in Discord and Telegram webhooks; enhance message formatting and logging

--- src/pages/api/server/webhook/tg-reminder.ts
+++ src/pages/api/server/webhook/tg-reminder.ts
@@ -1,12 +1,13 @@
 import * as Sentry from '@sentry/node'
-import { add, format, isWithinInterval } from 'date-fns'
+import { add, isWithinInterval } from 'date-fns'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import { dateToLocalizedRange } from '@/utils/calendar_manager'
 import { MeetingRepeatIntervals } from '@/utils/constants/schedule'
 import {
   getAccountsWithTgConnected,
   getConferenceDataBySlotId,
-  getSlotsForAccount,
+  getSlotsForAccountMinimal,
 } from '@/utils/database'
 import { sendDm } from '@/utils/services/telegram.helper'
 
@@ -15,40 +16,56 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       const tgConnectedAccounts = await getAccountsWithTgConnected()
       const currentTime = new Date()
+
       for (const account of tgConnectedAccounts) {
-        const slots = await getSlotsForAccount(
+        const slots = await getSlotsForAccountMinimal(
           account.account_address,
           currentTime
         )
         for (const slot of slots) {
-          if (!slot.id) continue
-          const meeting = await getConferenceDataBySlotId(slot.id)
-          const reminders = (meeting?.reminders || []).sort((a, b) => b - a)
-          for (const reminder of reminders) {
-            const interval = MeetingRepeatIntervals.find(
-              i => i.value === reminder
-            )
-            const intervalInMinutes = interval?.interval
-            if (!intervalInMinutes) continue
-            const reminderTime = add(new Date(slot.start), {
-              minutes: -intervalInMinutes,
-            })
-            const reminderTimeInterval = add(reminderTime, {
-              minutes: -4,
-            })
-            const startInterval: Interval = {
-              start: reminderTimeInterval,
-              end: reminderTime,
+          try {
+            if (!slot.id) continue
+            const meeting = await getConferenceDataBySlotId(slot.id)
+            const reminders = (meeting?.reminders || []).sort((a, b) => b - a)
+            for (const reminder of reminders) {
+              const interval = MeetingRepeatIntervals.find(
+                i => i.value === reminder
+              )
+              const intervalInMinutes = interval?.interval
+              if (!intervalInMinutes) continue
+              const reminderTime = add(new Date(slot.start), {
+                minutes: -intervalInMinutes,
+              })
+              const reminderTimeEnd = add(reminderTime, {
+                seconds: 60,
+              })
+              const startInterval: Interval = {
+                start: reminderTime,
+                end: reminderTimeEnd,
+              }
+              if (!isWithinInterval(currentTime, startInterval)) continue
+              const message = `You have a meeting (${
+                meeting.title || 'No Title'
+              })(staging) \n Starting in ${
+                interval.label
+              }. \n Meeting Time: ${dateToLocalizedRange(
+                new Date(slot.start),
+                new Date(slot.end),
+                account.timezone,
+                true
+              )} \n Meeting Link: ${meeting.meeting_url}`
+              // eslint-disable-next-line no-restricted-syntax
+              console.info(
+                `
+              Sending Tg message: ${message} to ${account.account_address}
+              `
+              )
+
+              await sendDm(account.telegram_id, message)
+              break
             }
-            if (!isWithinInterval(currentTime, startInterval)) continue
-            const message = `You have a meeting (${
-              meeting.title || 'No Title'
-            }) starting in ${interval.label} \n Start time: ${format(
-              new Date(slot.start),
-              'HH:mm a'
-            )}\n Meeting Link: ${meeting.meeting_url}`
-            await sendDm(account.telegram_id, message)
-            break
+          } catch (e) {
+            console.error(e)
           }
         }
       }
