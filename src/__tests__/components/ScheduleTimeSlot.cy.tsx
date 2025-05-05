fd5fa004c76a515fdf273d6dafada32232708ce0
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-05-05 19:39:30 +0100

    test: add initial Cypress test for ScheduleTimeSlot component rendering

---
+++ src/__tests__/components/ScheduleTimeSlot.cy.tsx
@@ -0,0 +1,25 @@
+import { add } from 'date-fns'
+import React from 'react'
+
+import { ScheduleTimeSlot } from './ScheduleTimeSlot'
+
+describe('<ScheduleTimeSlot />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <ScheduleTimeSlot
+        availabilities={[]}
+        busySlots={[]}
+        date={new Date()}
+        meetingMembers={[]}
+        pickedTime={null}
+        slot={{
+          start: new Date(),
+          end: add(new Date(), { minutes: 30 }),
+        }}
+        timezone={Intl.DateTimeFormat().resolvedOptions().timeZone}
+        handleTimePick={() => {}}
+      />
+    )
+  })
+})
