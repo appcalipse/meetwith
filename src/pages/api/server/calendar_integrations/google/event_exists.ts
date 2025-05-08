9655a3d7fbf05dbef3cc3cbe55e546158ea209cc
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-05-08 10:51:59 +0100

    feat: implement event existence check API endpoint for Google Calendar integration

---
+++ src/pages/api/server/calendar_integrations/google/event_exists.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { eventExists } from '@/utils/test-utils'
+
+const handle = async (req: NextApiRequest, res: NextApiResponse) => {
+  const account_address = req.body.scheduler_address
+  const meeting_id = req.body.meeting_id
+  return res.status(200).json(eventExists(meeting_id, account_address))
+}
+
+export default handle
