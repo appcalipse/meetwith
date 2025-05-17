5bb0b0cab9835721f042692f0e62280e7aa54e75
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-05-17 18:13:31 +0100

    feat: add ts-node dependency and update event_exists to use slot_id

--- src/pages/api/server/calendar_integrations/google/event_exists.ts
+++ src/pages/api/server/calendar_integrations/google/event_exists.ts
@@ -3,9 +3,15 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { eventExists } from '@/utils/test-utils'
 
 const handle = async (req: NextApiRequest, res: NextApiResponse) => {
-  const account_address = req.body.scheduler_address
-  const meeting_id = req.body.meeting_id
-  return res.status(200).json(await eventExists(meeting_id, account_address))
+  try {
+    const account_address = req.body.scheduler_address
+    const slot_id = req.body.slot_id
+    const exists = await eventExists(slot_id, account_address)
+    return res.status(200).json(exists)
+  } catch (e) {
+    console.error({ e })
+    return res.status(500).send(e)
+  }
 }
 
 export default handle
