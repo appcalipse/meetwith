4b48f140b94777977c14ecfd467cfa597ee2c5c7
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-06-05 12:53:00 +0100

    feat: enhance ScheduleBase component with participant ownership feature and improved participant handling

--- src/components/schedule/ScheduleBase.tsx
+++ src/components/schedule/ScheduleBase.tsx
@@ -1,4 +1,4 @@
-import { AddIcon, InfoIcon } from '@chakra-ui/icons'
+import { AddIcon, ChevronDownIcon, InfoIcon } from '@chakra-ui/icons'
 import {
   Box,
   Button,
@@ -10,6 +10,7 @@ import {
   FormLabel,
   Heading,
   HStack,
+  Icon,
   Input,
   Radio,
   RadioGroup,
@@ -17,12 +18,14 @@ import {
   Text,
   useColorModeValue,
   useDisclosure,
+  useToast,
   VStack,
 } from '@chakra-ui/react'
 import { Select as ChakraSelect } from 'chakra-react-select'
 import { format } from 'date-fns'
 import { useRouter } from 'next/router'
 import React, { useContext, useEffect, useMemo, useState } from 'react'
+import { FaChevronDown } from 'react-icons/fa'
 
 import { ChipInput } from '@/components/chip-input'
 import { SingleDatepicker } from '@/components/input-date-picker'
@@ -31,16 +34,17 @@ import RichTextEditor from '@/components/profile/components/RichTextEditor'
 import InfoTooltip from '@/components/profile/components/Tooltip'
 import DiscoverATimeInfoModal from '@/components/schedule/DiscoverATimeInfoModal'
 import ScheduleGroupModal from '@/components/schedule/ScheduleGroupModal'
-import {
-  IGroupParticipant,
-  Page,
-  ScheduleContext,
-} from '@/pages/dashboard/schedule'
+import { Page, ScheduleContext } from '@/pages/dashboard/schedule'
 import { AccountContext } from '@/providers/AccountProvider'
 import { MeetingReminders } from '@/types/common'
 import { Intents } from '@/types/Dashboard'
 import { MeetingProvider, MeetingRepeat } from '@/types/Meeting'
-import { ParticipantInfo } from '@/types/ParticipantInfo'
+import {
+  ParticipantInfo,
+  ParticipantType,
+  ParticipationStatus,
+} from '@/types/ParticipantInfo'
+import { IGroupParticipant, isGroupParticipant } from '@/types/schedule'
 import { durationToHumanReadable } from '@/utils/calendar_manager'
 import {
   DEFAULT_GROUP_SCHEDULING_DURATION,
@@ -51,10 +55,12 @@ import {
 } from '@/utils/constants/schedule'
 import {
   customSelectComponents,
-  MeetingRemindersComponent,
+  noClearCustomSelectComponent,
 } from '@/utils/constants/select'
 import { renderProviderName } from '@/utils/generic_utils'
 import { ellipsizeAddress } from '@/utils/user_manager'
+
+import ScheduleParticipantsOwners from './ScheduleParticipantsOwners'
 
 const ScheduleBase = () => {
   const { query } = useRouter()
@@ -62,6 +68,8 @@ const ScheduleBase = () => {
   const [isTitleValid, setIsTitleValid] = useState(true)
   const [isDurationValid, setIsDurationValid] = useState(true)
   const [isParticipantsValid, setIsParticipantsValid] = useState(true)
+  const toast = useToast()
+  const { onOpen, isOpen, onClose } = useDisclosure()
   const {
     participants,
     setParticipants,
@@ -93,6 +101,8 @@ const ScheduleBase = () => {
     isScheduler,
     selectedPermissions,
     setSelectedPermissions,
+    groups,
+    groupParticipants,
   } = useContext(ScheduleContext)
   const handleSubmit = () => {
     if (!title) {
@@ -127,10 +137,9 @@ const ScheduleBase = () => {
   const iconColor = useColorModeValue('gray.800', 'white')
   const onParticipantsChange = (_participants: Array<ParticipantInfo>) => {
     setParticipants(_prev => {
-      const oldGroups = _prev.filter(_participantOld => {
-        const participant = _participantOld as IGroupParticipant
-        return participant.isGroup
-      }) as Array<IGroupParticipant>
+      const oldGroups = _prev.filter(_participantOld =>
+        isGroupParticipant(_participantOld)
+      ) as Array<IGroupParticipant>
 
       oldGroups.forEach(oldGroup => {
         const participants =
@@ -173,6 +182,44 @@ const ScheduleBase = () => {
       ),
     [duration]
   )
+
+  const mergedParticipants = useMemo(() => {
+    const allParticipants: Array<ParticipantInfo> = []
+    for (const participant of participants) {
+      if (isGroupParticipant(participant)) {
+        const group = groups.find(g => g.id === participant.id)
+        if (group) {
+          const groupMembers = groupParticipants?.[participant.id] || []
+          const membersSanitized = groupMembers
+            .map(member => {
+              const groupMember = group.members.find(m => m.address === member)
+              if (
+                groupMember &&
+                groupMember.address &&
+                allParticipants.every(
+                  val => val.account_address !== groupMember.address
+                )
+              ) {
+                return {
+                  account_address: groupMember.address,
+                  name: groupMember.displayName,
+                  type: ParticipantType.Invitee,
+                  status: ParticipationStatus.Accepted,
+                  meeting_id: '',
+                }
+              }
+              return undefined
+            })
+            .filter(val => val !== undefined)
+          allParticipants.push(...membersSanitized)
+        }
+      } else {
+        allParticipants.push(participant)
+      }
+    }
+    return allParticipants
+  }, [participants, groups, groupParticipants])
+
   useEffect(() => {
     const type = currentAccount?.preferences.availableTypes.find(
       type => type.duration === duration
@@ -194,6 +241,11 @@ const ScheduleBase = () => {
       <DiscoverATimeInfoModal
         isOpen={openWhatIsThis}
         onClose={() => setOpenWhatIsThis(false)}
+      />
+      <ScheduleParticipantsOwners
+        isOpen={isOpen}
+        onClose={onClose}
+        participants={mergedParticipants}
       />
       <ScheduleGroupModal onClose={closeGroupModal} isOpen={isGroupModalOpen} />
       <VStack
@@ -370,6 +422,30 @@ const ScheduleBase = () => {
                 <Text marginInlineStart={-2}>{permission.label}</Text>
               </Checkbox>
             ))}
+            <FormControl>
+              <FormLabel display="flex" alignItems="center" fontSize="medium">
+                Make other participants meeting owners
+                <InfoTooltip text="Granting ownership will allow them to manage the meeting" />
+              </FormLabel>
+              <Button
+                onClick={onOpen}
+                borderColor="inherit"
+                borderWidth={1}
+                cursor="pointer"
+                color="neutral.400"
+                justifyContent="space-between"
+                borderRadius="0.375rem"
+                height={10}
+                fontSize="16"
+                px={4}
+                width="100%"
+                bg="transparent"
+                variant="link"
+              >
+                <Text userSelect="none">Add Participants</Text>
+                <Icon as={FaChevronDown} w={4} h={4} />
+              </Button>
+            </FormControl>
           </VStack>
           <VStack w="100%">
             <HStack width="fit-content" ml={'auto'}>
@@ -498,6 +574,13 @@ const ScheduleBase = () => {
               }>
               // can't select more than 5 notifications
               if (meetingNotification.length > 5) {
+                toast({
+                  title: 'Limit reached',
+                  description: 'You can select up to 5 notifications only.',
+                  status: 'warning',
+                  duration: 3000,
+                  isClosable: true,
+                })
                 return
               }
               setMeetingNotification(meetingNotification)
@@ -507,7 +590,7 @@ const ScheduleBase = () => {
             isMulti
             tagVariant={'solid'}
             options={MeetingNotificationOptions}
-            components={MeetingRemindersComponent}
+            components={noClearCustomSelectComponent}
             chakraStyles={{
               container: provided => ({
                 ...provided,
@@ -545,8 +628,15 @@ const ScheduleBase = () => {
             // eslint-disable-next-line tailwindcss/no-custom-classname
             className="noLeftBorder timezone-select"
             options={MeetingRepeatOptions}
-            components={customSelectComponents}
+            components={noClearCustomSelectComponent}
             chakraStyles={{
+              container: provided => ({
+                ...provided,
+                borderColor: 'inherit',
+                borderRadius: 'md',
+                maxW: '100%',
+                display: 'block',
+              }),
               placeholder: provided => ({
                 ...provided,
                 textAlign: 'left',
