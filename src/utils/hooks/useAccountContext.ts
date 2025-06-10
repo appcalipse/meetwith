3a7eac0c0a6318eb7c5d624423d82c0eddef5e53
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-06-10 23:35:59 +0100

    feat: enhance meeting type management with new session types and UI improvements

---
+++ src/utils/hooks/useAccountContext.ts
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react'
+
+import { AccountContext } from '@/providers/AccountProvider'
+
+const useAccountContext = () => {
+  const { currentAccount } = useContext(AccountContext)
+  return currentAccount
+}
+
+export default useAccountContext
