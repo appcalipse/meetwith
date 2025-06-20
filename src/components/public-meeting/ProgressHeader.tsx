363a1541e98bf414be85c1f43001045553d3efda
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-06-20 13:10:56 +0100

    refactor: replace fragment shorthand with Fragment component in ProgressHeader

--- src/components/public-meeting/ProgressHeader.tsx
+++ src/components/public-meeting/ProgressHeader.tsx
@@ -2,7 +2,7 @@ import { Box, HStack } from '@chakra-ui/react'
 import { PublicScheduleContext } from '@components/public-meeting/index'
 import ProgressHeaderItem from '@components/public-meeting/ProgressHeaderItem'
 import { PublicSchedulingSteps } from '@utils/constants/meeting-types'
-import React, { useContext } from 'react'
+import React, { Fragment, useContext } from 'react'
 const items = [
   {
     label: 'Pay for Session',
@@ -22,16 +22,12 @@ const ProgressHeader = () => {
   return (
     <HStack w={'100%'} gap={0} alignItems="center">
       {items.map((item, index) => (
-        <>
-          <ProgressHeaderItem
-            key={item.label}
-            currentStep={currentStep}
-            {...item}
-          />
+        <Fragment key={item.label}>
+          <ProgressHeaderItem currentStep={currentStep} {...item} />
           {index < items.length - 1 && (
             <Box flex={0.6} borderBottomWidth={3} borderStyle="dotted" />
           )}
-        </>
+        </Fragment>
       ))}
     </HStack>
   )
