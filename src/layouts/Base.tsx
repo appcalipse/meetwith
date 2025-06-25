f54ada492d141d7777970007edab79baf5ec57a6
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-06-25 23:07:49 +0100

    refactor: remove unnecessary ColorModeProvider wrapping in BaseLayout

--- src/layouts/Base.tsx
+++ src/layouts/Base.tsx
@@ -15,20 +15,18 @@ export const BaseLayout: React.FC<{
 }> = ({ children, consentCookie }) => {
   return (
     <ChakraProvider theme={customTheme}>
-      <ColorModeProvider>
-        <ChakraMDXProvider>
-          <CookieConsent consentCookie={consentCookie} />
-          <CookiesProvider>
-            <Box minH="100vh" display="flex" flexDir="column">
-              <Navbar />
-              {children}
-              <Footer />
-            </Box>
-            <OnboardingModal />
-            <DiscordOnboardingModal />
-          </CookiesProvider>
-        </ChakraMDXProvider>
-      </ColorModeProvider>
+      <ChakraMDXProvider>
+        <CookieConsent consentCookie={consentCookie} />
+        <CookiesProvider>
+          <Box minH="100vh" display="flex" flexDir="column">
+            <Navbar />
+            {children}
+            <Footer />
+          </Box>
+          <OnboardingModal />
+          <DiscordOnboardingModal />
+        </CookiesProvider>
+      </ChakraMDXProvider>
     </ChakraProvider>
   )
 }
