0142385327af5034852b0077543e3382b0c91569
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-06-26 14:59:38 +0100

    Revert "Merge pull request #621 from appcalipse/feat/contact-list"
    This reverts commit 0809345179dce052c3b829397c59bb80306b82aa, reversing
    changes made to 7e07b113ddb7c76ea786c27fb1570012c8ce9f17.

--- src/components/contact/ContactListItem.tsx
+++ src/components/contact/ContactListItem.tsx
@@ -110,12 +110,7 @@ const ContactListItem: FC<Props> = ({ account, index, sync, refetch }) => {
   return (
     <Tr bg={index % 2 === 0 ? 'neutral.825' : 'none'} color="white">
       <Th w="fit-content" py={8}>
-        <HStack w="fit-content" pos={'relative'}>
-          {account.status === ContactStatus.INACTIVE && (
-            <Text pos="absolute" top={-6} left={1} color="primary.200">
-              Removed you as a contact
-            </Text>
-          )}
+        <HStack w="fit-content">
           <Jazzicon
             address={account.address || ''}
             className="contact-avatar"
