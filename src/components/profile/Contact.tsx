f7ad9497293039e5bb678999896b27c1dec3b4c3
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-06-26 14:59:32 +0100

    Revert "Merge pull request #624 from appcalipse/feat/contact-list"
    This reverts commit 70a7bfa18370982e8fe9027c2fb263be5605fe84, reversing
    changes made to eb9f52c694dd988af8eed82f5ebad804036ab254.

--- src/components/profile/Contact.tsx
+++ src/components/profile/Contact.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@chakra-ui/next-js'
 import {
   Badge,
   Box,
@@ -16,18 +15,13 @@ import {
   Tabs,
   Text,
   useDisclosure,
-  VStack,
 } from '@chakra-ui/react'
-import ConnectCalendarModal from '@components/ConnectedCalendars/ConnectCalendarModal'
-import { EditMode } from '@meta/Dashboard'
-import { useRouter } from 'next/router'
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useContext, useRef } from 'react'
 import { FaPlus } from 'react-icons/fa'
 import { RiSearch2Line } from 'react-icons/ri'
 
 import { useDebounceValue } from '@/hooks/useDebounceValue'
 import { ContactStateContext } from '@/providers/ContactInvitesProvider'
-import { OnboardingContext } from '@/providers/OnboardingProvider'
 import { Account } from '@/types/Account'
 
 import ContactRequests from '../contact/ContactRequests'
@@ -36,41 +30,14 @@ import ContactsList, { ContactLisRef } from '../contact/ContactsList'
 
 const Contact: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const {
-    isOpen: isCalendarConnectionOpen,
-    onOpen: OpenCalendarConnection,
-    onClose: closeCalendarConnection,
-  } = useDisclosure()
-  const { asPath } = useRouter()
   const { requestCount } = useContext(ContactStateContext)
   const contactListRef = useRef<ContactLisRef>(null)
   const [debouncedValue, setValue] = useDebounceValue('', 500)
-  const [calendarsConnected, setCalendarsConnected] = useState(true)
-  const onboardingContext = useContext(OnboardingContext)
-  async function defineCalendarsConnected() {
-    setCalendarsConnected(await onboardingContext.connectedCalendarsComplete())
-  }
-  useEffect(() => {
-    void defineCalendarsConnected()
-  }, [currentAccount])
-  const state = useMemo(
-    () =>
-      Buffer.from(
-        JSON.stringify({
-          redirectTo: asPath,
-          ignoreState: true,
-        })
-      ).toString('base64'),
-    []
-  )
+
   return (
     <Flex direction={'column'} maxWidth="100%">
       <ContactSearchModal isOpen={isOpen} onClose={onClose} />
-      <ConnectCalendarModal
-        isOpen={isCalendarConnectionOpen}
-        onClose={closeCalendarConnection}
-        state={state}
-      />
+
       <HStack
         justifyContent="space-between"
         alignItems="flex-start"
@@ -93,125 +60,79 @@ const Contact: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
         mb={10}
         pb={0}
       >
-        <VStack w={'100%'} p={5}>
-          <HStack
-            justifyContent="space-between"
-            mb={4}
-            w={'100%'}
-            alignItems="flex-start"
-          >
-            <Box w="fit-content" pos="relative" h="100%">
-              <FormLabel
-                display="flex"
-                htmlFor="search"
-                pos="absolute"
-                left={2}
-                insetY={0}
-                h="100%"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <RiSearch2Line color="neutral.400" />
-              </FormLabel>
-              <Input
-                pl={8}
-                w="fit-content"
-                h={12}
-                type="search"
-                placeholder="Search contact"
-                id="search"
-                defaultValue={debouncedValue}
-                rounded={6}
-                onChange={e => setValue(e.target.value)}
-                autoComplete="off"
-                _placeholder={{
-                  color: 'neutral.400',
-                }}
-              />
-            </Box>
-
-            <TabList
-              w="auto"
-              bg="neutral.850"
-              p={1}
-              borderWidth={1}
+        <HStack justifyContent="space-between" mb={4} p={5}>
+          <Box w="fit-content" pos="relative" h="fit-content">
+            <FormLabel
+              display="flex"
+              htmlFor="search"
+              pos="absolute"
+              left={2}
+              insetY={0}
+              h="full"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <RiSearch2Line color="neutral.400" />
+            </FormLabel>
+            <Input
+              pl={8}
+              w="fit-content"
+              placeholder="Search contact"
+              id="search"
+              defaultValue={debouncedValue}
               rounded={6}
+              onChange={e => setValue(e.target.value)}
+              _placeholder={{
+                color: 'neutral.400',
+              }}
+            />
+          </Box>
+
+          <TabList w="auto" bg="neutral.850" p={1} borderWidth={1} rounded={6}>
+            <Tab
+              rounded={4}
+              fontWeight={700}
+              _selected={{
+                color: 'neutral.900',
+                bg: 'primary.200',
+              }}
             >
-              <Tab
-                rounded={4}
-                fontWeight={700}
-                _selected={{
-                  color: 'neutral.900',
-                  bg: 'primary.200',
-                }}
-              >
-                Contact list
-              </Tab>
-              <Tab
-                rounded={4}
-                fontWeight={700}
-                _selected={{
-                  color: 'neutral.900',
-                  bg: 'primary.200',
-                }}
-              >
-                Requests received
-                {requestCount > 0 && (
-                  <Badge
-                    colorScheme="primary"
-                    color="neutral.900"
-                    bg="primary.200"
-                    ml={2}
-                    px={1.5}
-                  >
-                    {requestCount}
-                  </Badge>
-                )}
-              </Tab>
-            </TabList>
-            <Button
-              onClick={onOpen}
-              flexShrink={0}
-              colorScheme="primary"
-              display={{ base: 'none', md: 'flex' }}
-              mt={{ base: 4, md: 0 }}
-              mb={4}
-              leftIcon={<FaPlus />}
+              Contact list
+            </Tab>
+            <Tab
+              rounded={4}
+              fontWeight={700}
+              _selected={{
+                color: 'neutral.900',
+                bg: 'primary.200',
+              }}
             >
-              Add new contact
-            </Button>
-          </HStack>
-          {!calendarsConnected && (
-            <Text w={'100%'}>
-              Your calendar is not connected, this is preventing your contact
-              from scheduling with you.{' '}
-              <Box
-                as={'span'}
-                color={'primary.200'}
-                onClick={() => OpenCalendarConnection()}
-                fontWeight={700}
-                cursor="pointer"
-                _hover={{
-                  textDecoration: 'underline',
-                }}
-              >
-                Please connect your calendar.
-              </Box>
-            </Text>
-          )}
+              Requests received
+              {requestCount > 0 && (
+                <Badge
+                  colorScheme="primary"
+                  color="neutral.900"
+                  bg="primary.200"
+                  ml={2}
+                  px={1.5}
+                >
+                  {requestCount}
+                </Badge>
+              )}
+            </Tab>
+          </TabList>
           <Button
             onClick={onOpen}
             flexShrink={0}
             colorScheme="primary"
-            display={{ base: 'flex', md: 'none' }}
+            display={{ base: 'none', md: 'flex' }}
             mt={{ base: 4, md: 0 }}
             mb={4}
             leftIcon={<FaPlus />}
-            w={'100%'}
           >
             Add new contact
           </Button>
-        </VStack>
+        </HStack>
         <TableContainer>
           <TabPanels p={0}>
             <TabPanel p={0}>
--- src/utils/api_helper.ts
+++ src/utils/api_helper.ts
@@ -692,7 +692,7 @@ export const getGoogleAuthConnectUrl = async (state?: string | null) => {
   )
 }
 
-export const getOffice365ConnectUrl = async (state?: string | null) => {
+export const getOffice365ConnectUrl = async (state?: string) => {
   return await internalFetch<ConnectResponse>(
     `/secure/calendar_integrations/office365/connect${
       state ? `?state=${state}` : ''
