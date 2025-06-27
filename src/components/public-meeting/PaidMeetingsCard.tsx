349fcb7c88ff112c38342fb4910146c95ca7f23b
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2025-06-27 13:45:25 +0100

    fix: update session remaining display to use plan's no_of_slot for accuracy

--- src/components/public-meeting/PaidMeetingsCard.tsx
+++ src/components/public-meeting/PaidMeetingsCard.tsx
@@ -61,7 +61,7 @@ const PaidMeetingsCard: FC<IProps> = props => {
           Schedule Now
         </Button>
         <Text color="primary.400" fontWeight={500}>
-          {props.session_used}/{props.session_total} Sessions remaining
+          {props.session_used}/{props.plan?.no_of_slot} Sessions remaining
         </Text>
       </HStack>
     </VStack>
