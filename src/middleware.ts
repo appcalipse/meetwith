e2af7d90ebd75f425048cb09b95ea2bf64e9cf38
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-07-09 09:01:28 +0100

    fix: add logging for request and headers in handleServerRoute function

--- src/middleware.ts
+++ src/middleware.ts
@@ -62,6 +62,10 @@ const handleSecureRoute = async (req: NextRequest) => {
 }
 
 const handleServerRoute = async (req: NextRequest) => {
+  // eslint-disable-next-line no-restricted-syntax
+  console.log(req)
+  // eslint-disable-next-line no-restricted-syntax
+  console.log(req.headers)
   const serverSecret = process.env.SERVER_SECRET
   const authHeader =
     req.headers.get('X-Server-Secret') ||
