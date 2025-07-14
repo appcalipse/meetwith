9acf5677e4bc47b6525efc1e67b3e677aecf3ecc
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-07-15 00:44:53 +0100

    feat: enhance avatar component to support optional URL prop for image rendering

--- src/components/profile/components/Avatar.tsx
+++ src/components/profile/components/Avatar.tsx
@@ -1,10 +1,13 @@
+import { Avatar as ChakraAvatar, Image } from '@chakra-ui/react'
 import { Jazzicon } from '@ukstv/jazzicon-react'
+import { getAccountDisplayName } from '@utils/user_manager'
 import { useEffect, useState } from 'react'
 
 import { Account } from '@/types/Account'
 
 interface AvatarProps {
   account: Account
+  url?: string
 }
 
 export const Avatar = (props: AvatarProps) => {
@@ -12,5 +15,17 @@ export const Avatar = (props: AvatarProps) => {
   useEffect(() => {
     setRendered(true)
   }, [])
-  return !rendered ? <div /> : <Jazzicon address={props.account.address} />
+  const avatar_url = props?.url || props?.account?.preferences?.avatar_url
+  return !rendered ? (
+    <div />
+  ) : avatar_url ? (
+    <ChakraAvatar
+      w={'100%'}
+      h={'100%'}
+      name={getAccountDisplayName(props.account)}
+      src={avatar_url}
+    />
+  ) : (
+    <Jazzicon address={props.account.address} />
+  )
 }
