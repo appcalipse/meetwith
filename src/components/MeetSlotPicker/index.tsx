86f102360907576ad75b313dabdbb94de97ece29
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-08-02 19:32:33 +0100

    refactor: remove existingMeetingData and isReschedule props from MeetSlotPicker

--- src/components/MeetSlotPicker/index.tsx
+++ src/components/MeetSlotPicker/index.tsx
@@ -390,8 +390,6 @@ const MeetSlotPicker: React.FC<MeetSlotPickerProps> = ({
             preferences={preferences}
             meetingProviders={preferences?.meetingProviders}
             selectedType={selectedType}
-            existingMeetingData={existingMeetingData}
-            isReschedule={isReschedule}
           />
         </Popup>
       )}
