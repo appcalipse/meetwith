c64491d953879f136b60d5110b5df08523eeca8b
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-08-07 18:28:13 +0100

    chore: Add responsive top margin to VStack in SchedulerPicker
    Introduced a top margin to the VStack component in SchedulerPicker for base screen sizes, improving spacing and layout responsiveness.

--- src/components/public-meeting/SchedulerPicker.tsx
+++ src/components/public-meeting/SchedulerPicker.tsx
@@ -671,7 +671,14 @@ const SchedulerPicker = () => {
             </Text>
           </HStack>
         </VStack>
-        <VStack align="flex-start" flexBasis={{ base: '100%', '2xl': '50%' }}>
+        <VStack
+          align="flex-start"
+          flexBasis={{ base: '100%', '2xl': '50%' }}
+          mt={{
+            base: 4,
+            '2xl': 0,
+          }}
+        >
           <HStack mb={0} cursor="pointer" onClick={() => setShowConfirm(false)}>
             <Icon as={FaArrowLeft} size="1.5em" color={color} />
             <Heading size="md" color={color}>
