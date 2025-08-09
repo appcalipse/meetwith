a6d213fff622b7678a899519cc151f15973b82e5
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-08-09 15:34:16 +0100

    chore: Remove unused props from SlotTypeSelector
    Eliminated passing of 'existingMeetingData' and 'isReschedule' props to SlotTypeSelector in MeetSlotPicker as they are no longer used.

--- src/components/MeetSlotPicker/index.tsx
+++ src/components/MeetSlotPicker/index.tsx
@@ -391,8 +391,6 @@ const MeetSlotPicker: React.FC<MeetSlotPickerProps> = ({
             preferences={preferences}
             meetingProviders={preferences?.meetingProviders}
             selectedType={selectedType}
-            existingMeetingData={existingMeetingData}
-            isReschedule={isReschedule}
           />
         </Popup>
       )}
