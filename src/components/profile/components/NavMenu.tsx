888d0e60d81e43aa9d3311cbb824832382cd7c96
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-08-27 23:17:32 +0100

    fix: update handleGroupInvites to call getGroupsInvites without address parameter

--- src/components/profile/components/NavMenu.tsx
+++ src/components/profile/components/NavMenu.tsx
@@ -139,7 +139,7 @@ export const NavMenu: React.FC<{
   }
   const handleGroupInvites = async () => {
     if (!currentAccount?.address) return
-    const invitedGroups = await getGroupsInvites(currentAccount?.address)
+    const invitedGroups = await getGroupsInvites()
     setNoOfInvitedGroups(invitedGroups?.length || 0)
     invitedGroups?.forEach((data, index) => {
       if (!toast.isActive(data.id)) {
