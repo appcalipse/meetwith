666b7fbf59aebd429976a01d237b8a1c3abed27c
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-08-28 20:18:10 +0100

    refactor: update NavMenu to use MetricStateContext for group invites and contacts request counts

--- src/components/profile/components/NavMenu.tsx
+++ src/components/profile/components/NavMenu.tsx
@@ -25,7 +25,7 @@ import { FaUserGroup } from 'react-icons/fa6'
 import DashboardOnboardingGauge from '@/components/onboarding/DashboardOnboardingGauge'
 import ActionToast from '@/components/toasts/ActionToast'
 import { AccountContext } from '@/providers/AccountProvider'
-import { ContactStateContext } from '@/providers/ContactInvitesProvider'
+import { MetricStateContext } from '@/providers/MetricStateProvider'
 import { OnboardingContext } from '@/providers/OnboardingProvider'
 import { EditMode } from '@/types/Dashboard'
 import { logEvent } from '@/utils/analytics'
@@ -56,7 +56,8 @@ export const NavMenu: React.FC<{
   const router = useRouter()
   const toast = useToast()
   const [noOfInvitedGroups, setNoOfInvitedGroups] = React.useState<number>(0)
-  const { requestCount } = useContext(ContactStateContext)
+  const { contactsRequestCount, groupInvitesCount } =
+    useContext(MetricStateContext)
 
   const { calendarResult } = router.query
   const menuBg = useColorModeValue('white', 'neutral.900')
@@ -67,13 +68,13 @@ export const NavMenu: React.FC<{
         name: 'My Groups',
         icon: FaUserGroup,
         mode: EditMode.GROUPS,
-        badge: noOfInvitedGroups,
+        badge: groupInvitesCount,
       },
       {
         name: 'My Contacts',
         icon: FaUserGroup,
         mode: EditMode.CONTACTS,
-        badge: requestCount,
+        badge: contactsRequestCount,
       },
       {
         name: 'Session Settings',
@@ -102,7 +103,7 @@ export const NavMenu: React.FC<{
         mode: EditMode.SIGNOUT,
       },
     ],
-    [noOfInvitedGroups, requestCount]
+    [groupInvitesCount, contactsRequestCount]
   )
   const handleEmptyGroupCheck = async () => {
     const emptyGroups = await getGroupsEmpty()
