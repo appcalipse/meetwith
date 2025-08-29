f253b4fca0e67a9eeafcac3e84c991660316b1e2
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-08-30 00:52:10 +0100

    feat: update GroupInviteCard to handle resetState as a Promise and improve layout responsiveness

--- src/components/group/GroupInviteCard.tsx
+++ src/components/group/GroupInviteCard.tsx
@@ -16,17 +16,27 @@ import { joinGroup } from '@/utils/api_helper'
 import { handleApiError } from '@/utils/error_helper'
 
 export interface IGroupInviteCard extends EmptyGroupsResponse {
-  resetState: () => void
+  resetState: () => Promise<unknown>
 }
 
 const GroupInviteCard: React.FC<IGroupInviteCard> = props => {
   const bgColor = useColorModeValue('white', 'neutral.900')
   const [accepting, setAccepting] = React.useState(false)
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
   const handleAccept = async () => {
     setAccepting(true)
     try {
       await joinGroup(props.id)
+      await props.resetState()
+      toast({
+        title: `You have joined the group "${props.name}"`,
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+        position: 'top',
+      })
+
       logEvent('Accepted invite', {
         group: props,
       })
@@ -34,46 +44,73 @@ const GroupInviteCard: React.FC<IGroupInviteCard> = props => {
       handleApiError('Error accepting invite', error)
     }
     setAccepting(false)
-    props.resetState()
   }
   return (
     <HStack
       width="100%"
-      p={8}
+      pr={8}
+      py={{ base: 8, lg: 4 }}
+      pl={{ base: 8, lg: 4 }}
       border={0}
       borderRadius="lg"
-      mt={6}
+      mt={2}
       bgColor={bgColor}
+      gap={{
+        base: 7,
+        lg: 4,
+      }}
       justifyContent="space-between"
       position="relative"
+      alignItems={{
+        base: 'flex-start',
+        md: 'center',
+      }}
+      flexDirection={{
+        base: 'column',
+        md: 'row',
+      }}
     >
-      <Box
-        width={2.5}
-        height={2.5}
-        rounded={'100%'}
-        position="absolute"
-        left={3}
-        bg="primary.500"
-      />
-      <Heading
-        size={'lg'}
-        maxW="500px"
-        w="fit-content"
-        whiteSpace="nowrap"
-        overflow="hidden"
-        textOverflow="ellipsis"
+      <HStack position="relative" pl={6}>
+        <Box
+          width={2.5}
+          height={2.5}
+          rounded={'100%'}
+          position="absolute"
+          left={0}
+          bg="primary.500"
+        />
+        <Heading
+          size={'lg'}
+          maxW="500px"
+          w="fit-content"
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+        >
+          {props.name}
+        </Heading>
+      </HStack>
+      <HStack
+        gap="16px"
+        w={{
+          base: '100%',
+          md: 'fit-content',
+        }}
       >
-        {props.name}
-      </Heading>
-      <HStack gap="16px">
         <Button
           isLoading={accepting}
           colorScheme="primary"
           onClick={handleAccept}
+          flex={1}
         >
           Join Group
         </Button>
-        <Button colorScheme="primary" variant="outline" onClick={onOpen}>
+        <Button
+          colorScheme="primary"
+          variant="outline"
+          onClick={onOpen}
+          flex={1}
+        >
           Decline
         </Button>
       </HStack>
