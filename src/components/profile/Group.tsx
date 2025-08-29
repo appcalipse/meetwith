f9f019b48dc7ba5ef06b30d4b287c30c66ed5f58
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-08-30 00:18:48 +0100

    feat: enhance Group component with group invites functionality and UI improvements

--- src/components/profile/Group.tsx
+++ src/components/profile/Group.tsx
@@ -31,7 +31,7 @@ import { Intents, InviteType } from '@/types/Dashboard'
 import { Group as GroupResponse, GroupMember } from '@/types/Group'
 import { getGroupExternal, listConnectedCalendars } from '@/utils/api_helper'
 
-import GroupInvites from '../group/GroupInvites'
+import GroupInvites, { GroupInvitesRef } from '../group/GroupInvites'
 import Groups, { GroupRef } from '../group/Groups'
 interface IGroupModal {
   openLeaveModal: () => void
@@ -68,9 +68,11 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
   const [inviteGroupData, setInviteGroupData] = useState<
     GroupResponse | undefined
   >(undefined)
-  const { groupInvitesCount } = useContext(MetricStateContext)
+  const { groupInvitesCount, fetchGroupInvitesCount } =
+    useContext(MetricStateContext)
   const [debouncedValue, setValue] = useDebounceValue('', 500)
   const groupRef = useRef<GroupRef>(null)
+  const groupInviteRef = useRef<GroupInvitesRef>(null)
 
   const [inviteDataIsLoading, setInviteDataIsLoading] = useState(false)
   const router = useRouter()
@@ -130,18 +132,23 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
   }, [join])
   useEffect(() => {
     if (intent === Intents.JOIN) {
-      checkAccount()
+      void checkAccount()
     }
   }, [intent, groupId])
   return (
-    <Flex direction={'column'} maxWidth="100%">
+    <Flex direction={'column'} maxWidth="100%" mb={4}>
       <ModalLoading isOpen={inviteDataIsLoading} />
       <GroupJoinModal
         group={inviteGroupData}
         onClose={() => setInviteGroupData(undefined)}
-        shouldOpen={isOnboardingOpened}
-        // resetState={resetState}
-        resetState={() => {}}
+        shouldOpen={!isOnboardingOpened}
+        resetState={() =>
+          Promise.all([
+            groupRef.current?.resetState(),
+            groupInviteRef?.current?.resetState(),
+            fetchGroupInvitesCount(),
+          ])
+        }
         inviteEmail={email as string}
         type={type as InviteType}
       />
@@ -157,7 +164,7 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
         gap={6}
       >
         <Heading fontSize="2xl">
-          My Groups21
+          My Groups
           <Text fontSize="sm" fontWeight={500} mt={1} lineHeight={1.5}>
             A group allows you to add multiple members and schedule meetings by
             automatically finding a suitable time based on each member’s
@@ -165,95 +172,46 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
           </Text>
         </Heading>
       </HStack>
-      <Tabs variant="unstyled" pb={0} mb={10}>
-        <VStack w={'100%'} p={5}>
-          <HStack
-            justifyContent="space-between"
-            mb={4}
-            w={'100%'}
-            alignItems="flex-start"
-          >
-            <Box w="fit-content" pos="relative" h="100%">
-              <FormLabel
-                display="flex"
-                htmlFor="search"
-                pos="absolute"
-                left={2}
-                insetY={0}
-                h="100%"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <RiSearch2Line color="neutral.400" />
-              </FormLabel>
-              <Input
-                pl={8}
-                w="fit-content"
-                h={12}
-                type="search"
-                placeholder="Search for group"
-                id="search"
-                defaultValue={debouncedValue}
-                rounded={6}
-                onChange={e => setValue(e.target.value)}
-                autoComplete="off"
-                _placeholder={{
-                  color: 'neutral.400',
-                }}
-              />
-            </Box>
-
-            <TabList
-              w="auto"
-              bg="neutral.850"
-              p={1}
-              borderWidth={1}
+      <Tabs variant="unstyled" pb={0}>
+        <HStack
+          justifyContent="space-between"
+          w={'100%'}
+          alignItems="flex-start"
+          mb={2}
+          flexDirection={{
+            base: 'column-reverse',
+            md: 'row',
+          }}
+        >
+          <Box w={{ base: '100%', md: 'fit-content' }} pos="relative" h="100%">
+            <FormLabel
+              display="flex"
+              htmlFor="search"
+              pos="absolute"
+              left={3}
+              insetY={0}
+              h="100%"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <RiSearch2Line color="neutral.400" />
+            </FormLabel>
+            <Input
+              pl={10}
+              w={{ base: '100%', md: 'fit-content' }}
+              h={12}
+              type="search"
+              placeholder="Search for group"
+              id="search"
+              defaultValue={debouncedValue}
               rounded={6}
-            >
-              <Tab
-                rounded={4}
-                fontWeight={700}
-                _selected={{
-                  color: 'neutral.900',
-                  bg: 'primary.200',
-                }}
-              >
-                Groups
-              </Tab>
-              <Tab
-                rounded={4}
-                fontWeight={700}
-                _selected={{
-                  color: 'neutral.900',
-                  bg: 'primary.200',
-                }}
-              >
-                Group invites
-                {groupInvitesCount > 0 && (
-                  <Badge
-                    colorScheme="primary"
-                    color="neutral.900"
-                    bg="primary.600"
-                    ml={2}
-                    px={1.5}
-                  >
-                    {groupInvitesCount}
-                  </Badge>
-                )}
-              </Tab>
-            </TabList>
-            <Button
-              onClick={() => router.push('/dashboard/create-group')}
-              flexShrink={0}
-              colorScheme="primary"
-              display={{ base: 'none', md: 'flex' }}
-              mt={{ base: 4, md: 0 }}
-              mb={4}
-              leftIcon={<FaPlus />}
-            >
-              Create new group
-            </Button>
-          </HStack>
+              onChange={e => setValue(e.target.value)}
+              autoComplete="off"
+              _placeholder={{
+                color: 'neutral.400',
+              }}
+            />
+          </Box>
           <Button
             onClick={() => router.push('/dashboard/create-group')}
             flexShrink={0}
@@ -266,7 +224,64 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
           >
             Create new group
           </Button>
-        </VStack>
+          <TabList
+            w={{ base: '100%', md: 'auto' }}
+            bg="neutral.850"
+            p={1}
+            borderWidth={1}
+            rounded={6}
+          >
+            <Tab
+              rounded={4}
+              fontWeight={700}
+              flexGrow={{
+                base: 1,
+                md: 0,
+              }}
+              _selected={{
+                color: 'neutral.900',
+                bg: 'primary.200',
+              }}
+            >
+              Groups
+            </Tab>
+            <Tab
+              rounded={4}
+              fontWeight={700}
+              flexGrow={{
+                base: 1,
+                md: 0,
+              }}
+              _selected={{
+                color: 'neutral.900',
+                bg: 'primary.200',
+              }}
+            >
+              Group invites
+              {groupInvitesCount > 0 && (
+                <Badge
+                  colorScheme="primary"
+                  color="white"
+                  bg="primary.600"
+                  ml={2}
+                  px={1.5}
+                >
+                  {groupInvitesCount}
+                </Badge>
+              )}
+            </Tab>
+          </TabList>
+          <Button
+            onClick={() => router.push('/dashboard/create-group')}
+            flexShrink={0}
+            colorScheme="primary"
+            display={{ base: 'none', md: 'flex' }}
+            leftIcon={<FaPlus />}
+          >
+            Create new group
+          </Button>
+        </HStack>
+
         <TabPanels p={0}>
           <TabPanel p={0}>
             <Groups
@@ -279,6 +294,7 @@ const Group: React.FC<{ currentAccount: Account }> = ({ currentAccount }) => {
             <GroupInvites
               currentAccount={currentAccount}
               search={debouncedValue}
+              ref={groupInviteRef}
               reloadGroups={() => groupRef.current?.resetState()}
             />
           </TabPanel>
