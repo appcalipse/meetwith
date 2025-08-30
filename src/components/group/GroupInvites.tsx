4f9748bb6605c70b8fdf28ae709375f12326a3ab
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-08-30 01:00:50 +0100

    feat: refactor GroupInvites to use forwardRef and improve resetState handling

--- src/components/group/GroupInvites.tsx
+++ src/components/group/GroupInvites.tsx
@@ -1,7 +1,9 @@
 import { HStack, Image, Spinner, Text, VStack } from '@chakra-ui/react'
+import { GroupRef } from '@components/group/Groups'
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import React, { forwardRef, useContext, useImperativeHandle } from 'react'
 
+import { MetricStateContext } from '@/providers/MetricStateProvider'
 import { Account } from '@/types/Account'
 import { getGroupsInvites } from '@/utils/api_helper'
 
@@ -10,55 +12,69 @@ import GroupInviteCard from './GroupInviteCard'
 type Props = {
   currentAccount: Account
   search: string
-  reloadGroups: () => void
+  reloadGroups: () => Promise<void> | undefined
 }
-
-const GroupInvites = ({ currentAccount, search, reloadGroups }: Props) => {
-  const { data, isLoading, refetch } = useQuery({
-    queryKey: ['groupInvites', currentAccount?.address, search],
-    queryFn: () => {
-      return getGroupsInvites(search)
-    },
-    enabled: !!currentAccount?.address,
-    staleTime: 0,
-    refetchOnMount: true,
-  })
-  const groups = data ?? []
-  const firstFetch = isLoading
-
-  if (firstFetch) {
-    return (
-      <VStack alignItems="center" mb={6}>
-        <Image src="/assets/schedule.svg" height="200px" alt="Loading..." />
-        <HStack pt={8}>
-          <Spinner />
-          <Text fontSize="lg">Checking for your groups...</Text>
-        </HStack>
-      </VStack>
-    )
-  } else if (groups.length === 0) {
-    return (
-      <VStack alignItems="center" gap={4}>
-        <Image src="/assets/no_group.svg" height="200px" alt="Loading..." />
-        <Text fontSize="lg">
-          No group invitations yet. You&apos;ll see pending invites here when
-          someone adds you to a group.
-        </Text>
-      </VStack>
-    )
-  } else {
-    return (
-      <VStack>
-        {groups.map(group => (
-          <GroupInviteCard
-            key={group.id}
-            {...group}
-            resetState={() => Promise.all([refetch(), reloadGroups()])}
-          />
-        ))}
-      </VStack>
-    )
+export interface GroupInvitesRef {
+  resetState: () => Promise<void>
+}
+const GroupInvites = forwardRef<GroupInvitesRef, Props>(
+  ({ currentAccount, search, reloadGroups }: Props, ref) => {
+    const { fetchGroupInvitesCount } = useContext(MetricStateContext)
+    const { data, isLoading, refetch } = useQuery({
+      queryKey: ['groupInvites', currentAccount?.address, search],
+      queryFn: () => {
+        return getGroupsInvites(search)
+      },
+      enabled: !!currentAccount?.address,
+      staleTime: 0,
+      refetchOnMount: true,
+    })
+    const groups = data ?? []
+    useImperativeHandle(ref, () => ({
+      resetState: async () => {
+        await refetch()
+      },
+    }))
+    if (isLoading) {
+      return (
+        <VStack alignItems="center" mb={6}>
+          <Image src="/assets/schedule.svg" height="200px" alt="Loading..." />
+          <HStack pt={8}>
+            <Spinner />
+            <Text fontSize="lg">Checking for your groups...</Text>
+          </HStack>
+        </VStack>
+      )
+    } else if (groups.length === 0) {
+      return (
+        <VStack alignItems="center" gap={4}>
+          <Image src="/assets/no_group.svg" height="200px" alt="Loading..." />
+          <Text fontSize="lg">
+            No group invitations yet. You&apos;ll see pending invites here when
+            someone adds you to a group.
+          </Text>
+        </VStack>
+      )
+    } else {
+      return (
+        <VStack gap={2}>
+          {groups.map(group => (
+            <GroupInviteCard
+              key={group.id}
+              {...group}
+              resetState={() =>
+                Promise.all([
+                  refetch(),
+                  reloadGroups(),
+                  fetchGroupInvitesCount(),
+                ])
+              }
+            />
+          ))}
+        </VStack>
+      )
+    }
   }
-}
-
+)
+GroupInvites.displayName = 'GroupInvites'
 export default GroupInvites
