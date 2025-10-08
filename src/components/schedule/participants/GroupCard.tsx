a09fc9d890f444062cfe892a04625833d939dc69
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-10-08 22:33:53 +0100

    refactor: update key prop for GroupParticipantsItem to ensure uniqueness

--- src/components/schedule/participants/GroupCard.tsx
+++ src/components/schedule/participants/GroupCard.tsx
@@ -130,7 +130,7 @@ const GroupCard: FC<IGroupCard> = props => {
               {members.length > 0 ? (
                 members.map(member => (
                   <GroupParticipantsItem
-                    key={member.address}
+                    key={member.address + `-${props.id}-${id}`}
                     currentAccount={props.currentAccount!}
                     groupId={props.id}
                     groupName={props.name}
