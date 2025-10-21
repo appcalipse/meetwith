bfb59e8a852c04b625a645eb7e70b3d08ba154e5
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-10-21 22:29:20 +0100

    feat: add enums for PaymentProvider and PaymentAccountStatus

---
+++ src/types/PaymentAccount.ts
@@ -0,0 +1,10 @@
+export enum PaymentProvider {
+  STRIPE = 'stripe',
+}
+
+export enum PaymentAccountStatus {
+  PENDING = 'pending',
+  CONNECTED = 'connected',
+  DISCONNECTED = 'disconnected',
+  FAILED = 'failed',
+}
