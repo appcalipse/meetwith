d699638da828076b1e4dea1e041d767254b5b083
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-10-21 14:35:48 +0100

    fix: ensure Stripe API key defaults to an empty string if not provided

--- src/utils/services/stripe.service.ts
+++ src/utils/services/stripe.service.ts
@@ -2,6 +2,6 @@ import Stripe from 'stripe'
 
 export class StripeService extends Stripe {
   constructor() {
-    super(process.env.STRIPE_API_KEY)
+    super(process.env.STRIPE_API_KEY || '')
   }
 }
