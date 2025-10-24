ca4e6ba964dd2d5da4640558ec841c55ecb5512e
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-10-24 22:04:40 +0100

    feat: update payment account retrieval logic in Stripe login handler

--- src/pages/api/secure/stripe/login.ts
+++ src/pages/api/secure/stripe/login.ts
@@ -4,18 +4,19 @@ import { StripeService } from '@utils/services/stripe.service'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 import { withSessionRoute } from '@/ironAuth/withSessionApiRoute'
-import { getOrCreatePaymentAccount } from '@/utils/database'
+import { getActivePaymentAccountDB } from '@/utils/database'
 
 const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
     try {
       const account_address = req.session.account!.address
       const stripe = new StripeService()
-      const provider = await getOrCreatePaymentAccount(
+      const provider = await getActivePaymentAccountDB(
         account_address,
         PaymentProvider.STRIPE
       )
-      const accountId = provider.provider_account_id
+
+      const accountId = provider?.provider_account_id
       if (!accountId) {
         return res.status(404).send('Stripe account not found.')
       }
