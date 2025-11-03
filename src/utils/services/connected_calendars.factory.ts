60ad399b0720963ba78d528164511be378319c79
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-11-03 11:45:56 +0100

    refactor: enhance getConnectedCalendarIntegration function signatures for better type safety and clarity

--- src/utils/services/connected_calendars.factory.ts
+++ src/utils/services/connected_calendars.factory.ts
@@ -3,13 +3,67 @@ import { Auth } from 'googleapis'
 import { TimeSlotSource } from '@/types/Meeting'
 
 import CaldavCalendarService, { CaldavCredentials } from './caldav.service'
-import { CalendarService } from './calendar.service.types'
+import {
+  BaseCalendarService,
+  IGoogleCalendarService,
+} from './calendar.service.types'
 import GoogleCalendarService from './google.service'
 import Office365CalendarService, {
   O365AuthCredentials,
 } from './office365.service'
 
-export const getConnectedCalendarIntegration = (
+function getConnectedCalendarIntegration(
+  address: string,
+  email: string,
+  provider: TimeSlotSource.GOOGLE,
+  credentials:
+    | string
+    | Auth.Credentials
+    | O365AuthCredentials
+    | CaldavCredentials
+): IGoogleCalendarService
+function getConnectedCalendarIntegration(
+  address: string,
+  email: string,
+  provider: TimeSlotSource.ICLOUD,
+  credentials:
+    | string
+    | Auth.Credentials
+    | O365AuthCredentials
+    | CaldavCredentials
+): BaseCalendarService
+function getConnectedCalendarIntegration(
+  address: string,
+  email: string,
+  provider: TimeSlotSource.OFFICE,
+  credentials:
+    | string
+    | Auth.Credentials
+    | O365AuthCredentials
+    | CaldavCredentials
+): BaseCalendarService
+function getConnectedCalendarIntegration(
+  address: string,
+  email: string,
+  provider: TimeSlotSource.WEBDAV,
+  credentials:
+    | string
+    | Auth.Credentials
+    | O365AuthCredentials
+    | CaldavCredentials
+): BaseCalendarService
+function getConnectedCalendarIntegration(
+  address: string,
+  email: string,
+  provider: TimeSlotSource.MWW,
+  credentials:
+    | string
+    | Auth.Credentials
+    | O365AuthCredentials
+    | CaldavCredentials
+): BaseCalendarService
+
+function getConnectedCalendarIntegration(
   address: string,
   email: string,
   provider: TimeSlotSource,
@@ -18,7 +72,7 @@ export const getConnectedCalendarIntegration = (
     | Auth.Credentials
     | O365AuthCredentials
     | CaldavCredentials
-): CalendarService<typeof provider> => {
+): BaseCalendarService | IGoogleCalendarService {
   switch (provider) {
     case TimeSlotSource.GOOGLE:
       return new GoogleCalendarService(
@@ -43,3 +97,5 @@ export const getConnectedCalendarIntegration = (
       throw new Error(`Unsupported calendar provider: ${provider}`)
   }
 }
+
+export { getConnectedCalendarIntegration }
