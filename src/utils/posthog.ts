580170e6be653b654b812d456d121c31ff998ddd
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-11-04 22:03:35 +0100

    feat: implement PostHogClient as a singleton for improved instance management

--- src/utils/posthog.ts
+++ src/utils/posthog.ts
@@ -1,11 +1,25 @@
 import { PostHog } from 'posthog-node'
 
-// NOTE: This is a Node.js client, so you can use it for sending events from the server side to PostHog.
-export default function PostHogClient() {
-  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    flushAt: 1,
-    flushInterval: 0,
-  })
-  return posthogClient
+class PostHogClientSingleton {
+  private static instance: PostHog | null = null
+
+  private constructor() {}
+
+  public static getInstance(): PostHog {
+    if (PostHogClientSingleton.instance === null) {
+      PostHogClientSingleton.instance = new PostHog(
+        process.env.NEXT_PUBLIC_POSTHOG_KEY!,
+        {
+          host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+          flushAt: 1,
+          flushInterval: 0,
+        }
+      )
+    }
+    return PostHogClientSingleton.instance
+  }
+}
+
+export default function PostHogClient(): PostHog {
+  return PostHogClientSingleton.getInstance()
 }
