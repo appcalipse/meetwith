94d1e906d51fb043f4e6580489efda33eeec79ff
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-12-10 22:36:02 +0100

    feat(calendar): add ActiveCalendarEvent component for rendering calendar events

---
+++ src/components/calendar-view/ActiveCalendarEvent.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+interface ActiveCalendarEventProps {
+  slot: UnifiedEvent<DateTime<boolean>>
+}
+
+const ActiveCalendarEvent: React.FC<ActiveCalendarEventProps> = ({ slot }) => {
+  return <div></div>
+}
+
+export default ActiveCalendarEvent
