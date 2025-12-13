f8dc5fbba83dca6f4ce754c40fd7ce058f300913
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-12-13 23:57:13 +0100

    feat(calendar): refactor Event component to streamline props and improve event handling

--- src/components/calendar-view/Event.tsx
+++ src/components/calendar-view/Event.tsx
@@ -6,13 +6,13 @@ import {
   PopoverCloseButton,
   PopoverContent,
   PopoverTrigger,
-  useDisclosure,
 } from '@chakra-ui/react'
 import { DateTime } from 'luxon'
 import * as React from 'react'
 import { FaExpand } from 'react-icons/fa6'
 
 import useAccountContext from '@/hooks/useAccountContext'
+import { useCalendarContext } from '@/providers/calendar/CalendarContext'
 import {
   AttendeeStatus,
   isCalendarEvent,
@@ -29,44 +29,29 @@ import {
 import EventDetailsPopOver from './EventDetailsPopOver'
 
 interface EventProps {
-  slot: WithInterval<UnifiedEvent<DateTime> | MeetingDecrypted<DateTime>>
+  event: WithInterval<UnifiedEvent<DateTime> | MeetingDecrypted<DateTime>>
   bg: string
-  allSlotsForDay: Array<
+  dayEvents: Array<
     WithInterval<UnifiedEvent<DateTime> | MeetingDecrypted<DateTime>>
   >
   timeSlot: DateTime
+  onClick?: () => void
 }
 
-const Event: React.FC<EventProps> = ({
-  slot,
-  bg,
-  allSlotsForDay,
-  timeSlot,
-}) => {
-  const duration = slot.interval.toDuration('minutes').toObject().minutes || 0
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const height = (duration / 60) * 36
+const Event: React.FC<EventProps> = ({ bg, dayEvents, event, timeSlot }) => {
   const currentAccount = useAccountContext()
-  const top =
-    ((slot.start.diff(timeSlot, 'minutes').toObject().minutes || 0) / 60) * 36
-  const isStartInsideOtherEvent = React.useMemo(() => {
-    return allSlotsForDay.filter(otherEvent => {
-      if (otherEvent.id === slot.id) return false
-      return slot.start > otherEvent.start && slot.start < otherEvent.end
-    })
-  }, [allSlotsForDay, slot])
-  const margin = isStartInsideOtherEvent.length * 3
+  const { setSelectedSlot } = useCalendarContext()
   const actor = React.useMemo(() => {
-    if (isCalendarEvent(slot)) {
-      return slot.attendees?.find(
-        attendee => attendee.email === slot.accountEmail
+    if (isCalendarEvent(event)) {
+      return event.attendees?.find(
+        attendee => attendee.email === event.accountEmail
       )
     } else {
-      return slot.participants.find(
+      return event.participants.find(
         participant => participant.account_address === currentAccount?.address
       )
     }
-  }, [])
+  }, [event, currentAccount])
   const isDeclined =
     actor?.status &&
     [ParticipationStatus.Rejected, AttendeeStatus.DECLINED].includes(
@@ -87,61 +72,76 @@ const Event: React.FC<EventProps> = ({
       AttendeeStatus.ACCEPTED,
       AttendeeStatus.COMPLETED,
     ].includes(actor?.status)
-  return (
-    <Popover isOpen={isOpen} onOpen={onOpen} onClose={onClose}>
-      <PopoverTrigger>
-        <GridItem
-          bg={bg}
-          borderWidth={1}
-          borderLeftWidth={5}
-          rounded={'3px'}
-          px={1.5}
-          borderColor={generateBorderColor(bg)}
-          color={getDesignSystemTextColor(bg)}
-          w="100%"
-          minW={0}
-          height={`${Math.max(height, 18)}px`}
-          marginTop={margin}
-          marginLeft={margin}
-          top={top}
-          zIndex={2}
-          py={0}
-          overflowY="hidden"
-          _hover={{
-            borderWidth: '2px',
-            borderLeftWidth: 5,
-          }}
-        >
-          <VStack gap={0}>
-            <Text
-              w="100%"
-              whiteSpace="nowrap"
-              overflow="hidden"
-              textOverflow="ellipsis"
-              fontSize="xs"
-            >
-              {slot.title}
-            </Text>
-            <Text fontSize="10px">{slot.interval.toFormat('t')}</Text>
-          </VStack>
-        </GridItem>
-      </PopoverTrigger>
-      <PopoverContent zIndex={10} width="600px">
-        <PopoverArrow />
-        <PopoverCloseButton
-          onClick={() => {
-            onClose()
-          }}
-          as={FaExpand}
-          size={'24'}
-          top={4}
-          right={4}
-        />
 
-        <PopoverBody>
-          <EventDetailsPopOver slot={slot} />
-        </PopoverBody>
-      </PopoverContent>
+  const isStartInsideOtherEvent = dayEvents.filter(otherEvent => {
+    if (otherEvent.id === event.id) return false
+    return event.start > otherEvent.start && event.start < otherEvent.end
+  })
+  const duration = event.interval.toDuration('minutes').toObject().minutes || 0
+  const height = (duration / 60) * 36
+  const margin = isStartInsideOtherEvent.length * 3
+  const top =
+    ((event.start.diff(timeSlot, 'minutes').toObject().minutes || 0) / 60) * 36
+  return (
+    <Popover isLazy placement="auto">
+      {({ onClose }) => (
+        <>
+          <PopoverTrigger>
+            <GridItem
+              bg={bg}
+              borderWidth={1}
+              borderLeftWidth={5}
+              rounded={'3px'}
+              px={1.5}
+              borderColor={generateBorderColor(bg)}
+              color={getDesignSystemTextColor(bg)}
+              w="100%"
+              minW={0}
+              height={`${Math.max(height, 18)}px`}
+              marginTop={margin}
+              marginLeft={margin}
+              top={top}
+              zIndex={2}
+              py={0}
+              overflowY="hidden"
+              _hover={{
+                borderWidth: '2px',
+                borderLeftWidth: 5,
+              }}
+            >
+              <VStack gap={0}>
+                <Text
+                  w="100%"
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                  fontSize="xs"
+                >
+                  {event.title}
+                </Text>
+                <Text fontSize="10px">{event.interval.toFormat('t')}</Text>
+              </VStack>
+            </GridItem>
+          </PopoverTrigger>
+          <PopoverContent zIndex={10} width="600px">
+            <PopoverArrow />
+            <PopoverCloseButton
+              onClick={() => {
+                onClose()
+                setSelectedSlot(event)
+              }}
+              as={FaExpand}
+              size={'24'}
+              top={4}
+              right={4}
+            />
+
+            <PopoverBody>
+              <EventDetailsPopOver slot={event} />
+            </PopoverBody>
+          </PopoverContent>
+        </>
+      )}
     </Popover>
   )
 }
