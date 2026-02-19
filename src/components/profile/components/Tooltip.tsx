20a2c37dc5769e58c65afa2eadd1b69e48abe30d
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2026-02-19 16:30:35 +0100

    fix(InfoTooltip): expose mb prop to fix alignment in flex containers

--- src/components/profile/components/Tooltip.tsx
+++ src/components/profile/components/Tooltip.tsx
@@ -1,12 +1,15 @@
 import { InfoIcon } from '@chakra-ui/icons'
 import { Tooltip, useColorModeValue } from '@chakra-ui/react'
 
-const InfoTooltip: React.FC<{ text: string }> = ({ text }) => {
+const InfoTooltip: React.FC<{ text: string; mb?: number | string }> = ({
+  text,
+  mb = 1,
+}) => {
   const color = useColorModeValue('gray.800', 'white')
 
   return (
     <Tooltip label={text}>
-      <InfoIcon cursor="pointer" color={color} ml={2} mb={1} />
+      <InfoIcon cursor="pointer" color={color} ml={2} mb={mb} />
     </Tooltip>
   )
 }
