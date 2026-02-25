37dcd28724cd43eb47fd841f2794568492578de3
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2026-02-25 16:20:51 +0100

    Increase Jest coverage thresholds to 60% for statements, branches, functions, and lines

--- jest.config.js
+++ jest.config.js
@@ -78,10 +78,10 @@ const customJestConfig = {
   },
   coverageThreshold: {
     global: {
-      statements: 40,
-      branches: 40,
-      functions: 40,
-      lines: 40,
+      statements: 60,
+      branches: 60,
+      functions: 60,
+      lines: 60,
     },
   },
 }
