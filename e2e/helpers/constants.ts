36511d7589ddf5f93e31bd8b23584e1dbf195abf
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2026-03-02 23:09:23 +0100

    refactor: remove unused DEFAULT_MESSAGE constant from constants file

--- e2e/helpers/constants.ts
+++ e2e/helpers/constants.ts
@@ -1,6 +1,3 @@
-export const DEFAULT_MESSAGE = (nonce: number) =>
-  `Welcome to Meet with Wallet!\n\nPlease sign this message to prove you are the owner of your account and enable your data to be encrypted and private.\n\nDon't worry, no actual transaction, gas or assets will be used for it.\n\nYour unique number is ${nonce}`
-
 export const AUTH_STATE_PATH = 'e2e/.auth/user.json'
 export const TEST_DATA_PATH = 'e2e/.auth/test-data.json'
 
